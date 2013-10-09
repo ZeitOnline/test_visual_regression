dc1ab751f47a7fbdc8b3cbe49d446031359babd9
Ron Drongowski <ron.drongowski@gmail.com>
2013-10-09 12:52:48 +0200

    ZMO-3: Add static_views for css and js

--- src/zeit/frontend/application.py
+++ src/zeit/frontend/application.py
@@ -3,6 +3,7 @@ import zeit.frontend.model
 import zeit.frontend.navigation
 import pyramid.config
 import pyramid_jinja2
+import pkg_resources
 
 def factory(global_config, **settings):
     config = pyramid.config.Configurator(settings=settings,
@@ -12,5 +13,9 @@ def factory(global_config, **settings):
     utility.globals.update(zeit.frontend.navigation.get_sets())
     config.add_renderer('.html', pyramid_jinja2.renderer_factory)
     config.add_route('json', 'json/*traverse')
+    config.add_static_view(name='css',
+            path=pkg_resources.resource_filename(__name__,'css'))
+    config.add_static_view(name='js',
+            path=pkg_resources.resource_filename(__name__,'js'))
     config.scan(package=zeit.frontend)
     return config.make_wsgi_app()
