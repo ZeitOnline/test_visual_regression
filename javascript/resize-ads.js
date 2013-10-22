6416dd15878444fa5e7ecc7485a40d5e85b11ca1
Anika Szuppa <anika.szuppa@zeit.de>
2013-10-22 15:21:59 +0200

    ZMO-13: integrate leaderboard, mobile banner, fireplace and billboard

--- javascript/resize-ads.js
+++ javascript/resize-ads.js
@@ -3,35 +3,70 @@
         var prepare_slot;
     
         window.ad_slots = {
-          top: {
-            active_class: "",
+          leaderboard: {
+            active_class: null,
+            active_text: '',
             ads: [
               {
-                div_class: "ad__top-banner-s",
-                min_width: 320,
-                min_height: 0
-              }, {
-                div_class: "ad__top-banner-m",
+                div_class: "ad__leaderboard",
                 min_width: 940,
-                min_height: 0
+                min_height: 0,
+                text: "Leaderboard"
+              },
+              {
+                div_class: "ad__leaderboard--mobile",
+                min_width: 320,
+                max_width: 940,
+                min_height: 0,
+                text: "Mobile Banner"
               }
             ]
           },
-          sidebar: {
-            active_class: "",
+          fp_top: {
+            active_class: null,
+            active_text: '',
             ads: [
               {
-                div_class: "ad__sidebar-banner-s",
-                min_width: 320,
-                min_height: 150
-              }, {
-                div_class: "ad__sidebar-banner-m",
-                min_width: 320,
-                min_height: 400
-              }, {
-                div_class: "ad__sidebar-banner-l",
-                min_width: 320,
-                min_height: 700
+                div_class: "ad__fp--top",
+                min_width: 1320,
+                min_height: 0,
+                text: "Fireplace Top"
+              }
+            ]
+          },
+          fp_left: {
+            active_class: null,
+            active_text: '',
+            ads: [
+              {
+                div_class: "ad__fp--left",
+                min_width: 1320,
+                min_height: 600,
+                text: "Fireplace Left"
+              }
+            ]
+          },
+          fp_right: {
+            active_class: null,
+            active_text: '',
+            ads: [
+              {
+                div_class: "ad__fp--right",
+                min_width: 1320,
+                min_height: 600,
+                text: "Fireplace Right"
+              }
+            ]
+          },
+          rec: {
+            active_class: null,
+            active_text: '',
+            ads: [
+              {
+                div_class: "ad__rec",
+                min_width: 720,
+                min_height: 0,
+                text: "Rectangle"
               }
             ]
           }
@@ -43,19 +78,27 @@
           _results = [];
           for (_i = 0, _len = _ref.length; _i < _len; _i++) {
             ad = _ref[_i];
-            if (window.innerWidth >= ad.min_width && window.innerHeight >= ad.min_height) {
-              slot.active_class = ad.div_class;
-              break;
+
+            if( !ad.max_width ){
+              ad.max_width = 5000; 
+            }
+
+            if (window.innerWidth >= ad.min_width && window.innerHeight >= ad.min_height && window.innerWidth <= ad.max_width) {
+                slot.active_class = ad.div_class;
+                slot.active_text = ad.text;
+                break;
             } else {
               _results.push(void 0);
             }
           }
           return _results;
         };
-      
-        prepare_slot(window.ad_slots.top);
-      
-        prepare_slot(window.ad_slots.sidebar);
+     
+        prepare_slot(window.ad_slots.fp_top);
+        prepare_slot(window.ad_slots.fp_left);
+        prepare_slot(window.ad_slots.fp_right);
+        prepare_slot(window.ad_slots.rec);
+        prepare_slot(window.ad_slots.leaderboard);
       
       }).call(this);
       
