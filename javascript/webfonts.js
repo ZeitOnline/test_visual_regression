e94df511f47c8ad40450a6523ebe4275a0570a51
Harry Keller <ich@harry-k.de>
2013-10-30 09:59:56 +0100

    ZMO-88: refactor webfont loader

--- javascript/webfonts.js
+++ javascript/webfonts.js
@@ -1,36 +1,38 @@
-/* global console, alert */
+/* global console */
 (function() {
 	var Fontloader;
 	Fontloader = (function() {
 
 		function Fontloader() {
-			var fonts = "/css/webfonts.css",
+			var font_path = "/css/webfonts.css",
 			ua = window.navigator.userAgent;
 
-			// alternative: 'querySelector' in document && 'localStorage' in window && 'addEventListener' in window
-			var old_browser = ua.indexOf( "Android" ) > -1 && ua.indexOf( "like Gecko" ) > -1 && ua.indexOf( "Chrome" ) === -1 || window.document.documentElement.className.indexOf( "lt-ie9" ) > -1,
-			injectref = window.document.getElementsByTagName( "link" )[0];
+			// detect old browsers that won’t support woff anyways
+			// alternative: ('querySelector' in document && 'localStorage' in window && 'addEventListener' in window)
+			var is_old_browser = ua.indexOf( "Android" ) > -1 && ua.indexOf( "like Gecko" ) > -1 && ua.indexOf( "Chrome" ) === -1 || window.document.documentElement.className.indexOf( "lt-ie9" ) > -1,
+			inject_ref = window.document.getElementsByTagName( "link" )[0];
 
 			function insert_css(style) {
-				var fontstyle = window.document.createElement( "style" );
-				fontstyle.innerHTML = style;
-				if( injectref && injectref.parentNode ) {
-					injectref.parentNode.insertBefore( fontstyle, injectref );
+				// create a style tag in the <head> filled with raw font-face declarations
+				var style_tag = window.document.createElement( "style" );
+				style_tag.innerHTML = style;
+				if( inject_ref && inject_ref.parentNode ) {
+					inject_ref.parentNode.insertBefore( style_tag, inject_ref );
 				}
-				console.timeEnd("localstorage");
 			}
 
 			function load_css() {
-				console.time("localstorage");
+				// first determine whether we have cached fonts in localstorage
 				var local_style = localStorage.getItem('zeit_webfonts');
 				if (local_style && local_style.length > 0) {
-					console.log("throwing in the local style");
+					// yep, we do, so drop them in
 					insert_css(local_style);
 				} else {
-					console.log("ajaxing");
+					// nope, we don’t: let’s fetch them from the server
 					$.ajax({
-						url: '/css/webfonts.css',
+						url: font_path,
 						success: (function (ajaxed_style) {
+							// we’ve got the fonts, store them in localstorage and apply them
 							localStorage.setItem('zeit_webfonts', ajaxed_style);
 							insert_css(ajaxed_style);
 						})
@@ -38,7 +40,8 @@
 				}
 			}
 
-			if (!old_browser) {
+			// only load webfont css when we have a good guesstimate that the browser will support woff
+			if (!is_old_browser) {
 				load_css();
 			}
 		}
