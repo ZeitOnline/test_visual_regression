3339d3508646789ee24519a5825caf0ee9c785e0
Harry Keller <ich@harry-k.de>
2013-10-30 15:05:56 +0100

    ZMO-88: add optional lazy load capabilities to webfont loader

--- javascript/webfonts.js
+++ javascript/webfonts.js
@@ -2,17 +2,30 @@
 (function() {
 	var Fontloader;
 	Fontloader = (function() {
-
 		function Fontloader() {
-			var font_path = "/css/webfonts.css",
-			ua = window.navigator.userAgent;
+			// set up all font packages we need
+			var font_dictionary = [
+				{
+					identifier: 'base',
+					has_priority: true,
+					path: '/css/standalone-fonts/base.css'
+				},
+				{
+					identifier: 'quotes',
+					selectors: ['.quote', '.quote--loud'],
+					has_priority: false,
+					path: '/css/standalone-fonts/quotes.css'
+				}
+			];
+			var ua = window.navigator.userAgent,
+			storage_base_string = 'de.zeit.zmo__webfonts--';
 
 			// detect old browsers that won’t support woff anyways
 			// alternative: ('querySelector' in document && 'localStorage' in window && 'addEventListener' in window)
 			var is_old_browser = ua.indexOf( "Android" ) > -1 && ua.indexOf( "like Gecko" ) > -1 && ua.indexOf( "Chrome" ) === -1 || window.document.documentElement.className.indexOf( "lt-ie9" ) > -1,
 			inject_ref = window.document.getElementsByTagName( "link" )[0];
 
-			function insert_css(style) {
+			function append_css(style) {
 				// create a style tag in the <head> filled with raw font-face declarations
 				var style_tag = window.document.createElement( "style" );
 				style_tag.innerHTML = style;
@@ -21,29 +34,54 @@
 				}
 			}
 
-			function load_css() {
-				// first determine whether we have cached fonts in localstorage
-				var local_style = localStorage.getItem('zeit_webfonts');
-				if (local_style && local_style.length > 0) {
-					// yep, we do, so drop them in
-					insert_css(local_style);
+			function fetch_css(path, identifier, font_data) {
+				// if we already have font_data (from localstorage), make use of it immediately
+				if (font_data && font_data.length > 0) {
+					append_css(font_data);
 				} else {
-					// nope, we don’t: let’s fetch them from the server
+					// nope, we don’t: let’s fetch it asynchronously from the server
 					$.ajax({
-						url: font_path,
-						success: (function (ajaxed_style) {
+						url: path,
+						success: function (ajaxed_style) {
 							// we’ve got the fonts, store them in localstorage and apply them
-							localStorage.setItem('zeit_webfonts', ajaxed_style);
-							insert_css(ajaxed_style);
-						})
+							localStorage.setItem(storage_base_string + identifier, ajaxed_style);
+							append_css(ajaxed_style);
+						}
 					});
 				}
 			}
 
+			function init_fonts() {
+				var scheduled_fonts = [];
+				for (var i in font_dictionary) {
+					if (i) {
+						var pack = font_dictionary[i];
+						pack.data = localStorage.getItem(storage_base_string + pack.identifier);
+						// if pack has priority or data is already in localstorage: apply immediately
+						if (pack.has_priority || pack.data) {
+							// drop it in as fast as possible
+							fetch_css(pack.path, pack.identifier, pack.data);
+						} else {
+							// schedule for later lazy loading
+							scheduled_fonts.push(font_dictionary[i]);
+						}
+					}
+				}
+				// lazy load fonts at last
+				window.onload = function() {
+					for (var i in scheduled_fonts) {
+						if (i) {
+							fetch_css(scheduled_fonts[i].path, scheduled_fonts[i].identifier);
+						}
+					}
+				};
+			}
+
 			// only load webfont css when we have a good guesstimate that the browser will support woff
 			if (!is_old_browser) {
-				load_css();
+				init_fonts();
 			}
+
 		}
 
 		return Fontloader;
