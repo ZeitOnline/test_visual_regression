72e6357cdbad82c8005f54f0ffc03eab0b8bd547
Nico Brünjes <nico.bruenjes@zeit.de>
2013-11-08 13:55:54 +0100

    ZMO-122: test author macro
    add unit test for author macro, delete tests for previously deleted macros

--- src/zeit/frontend/test/test_templates.py
+++ src/zeit/frontend/test/test_templates.py
@@ -16,6 +16,17 @@ def jinja2_env(request):
     return utility
 
 
+def test_macro_p_should_produce_markup(jinja2_env):
+    tpl = jinja2_env.get_template('../templates/block_elements.tpl')
+    html = 'Alles nicht so <em>wichtig</em>, oder?!'
+    lines = tpl.module.p(html, 'test').splitlines()
+    output = ""
+    for line in lines:
+        output += line.strip()
+    markup = '<p class="test">Alles nicht so <em>wichtig</em>, oder?!</p>'
+    assert markup == output
+
+
 def test_macro_intertitle_should_produce_markup(jinja2_env):
     tpl = jinja2_env.get_template('../templates/block_elements.tpl')
     lines = tpl.module.intertitle("xy").splitlines()
@@ -26,15 +37,14 @@ def test_macro_intertitle_should_produce_markup(jinja2_env):
     assert m == output
 
 
-def test_macro_p_should_produce_markup(jinja2_env):
+def test_macro_meta_author_should_produce_markup(jinja2_env):
     tpl = jinja2_env.get_template('../templates/block_elements.tpl')
-    html = 'Alles nicht so <em>wichtig</em>, oder?!'
-    lines = tpl.module.p(html, 'test').splitlines()
-    output = ""
-    for line in lines:
-        output += line.strip()
-    markup = '<p class="test">Alles nicht so <em>wichtig</em>, oder?!</p>'
-    assert markup == output
+    data = {'name': "y", 'prefix': ' von ', 'suffix': ', '}
+    markup = 'von <span class="article__meta__author">y</span>,'
+    assert markup == tpl.module.meta_author(data).strip()
+    data['href'] = 'x'
+    markup = 'von <a href="x" class="article__meta__author meta-link">y</a>,'
+    assert markup == tpl.module.meta_author(data).strip()
 
 
 def test_macro_authorlink_should_produce_valid_markup(jinja2_env):
