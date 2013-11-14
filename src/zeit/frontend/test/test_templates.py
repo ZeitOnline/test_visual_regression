cd0386c953fa770950de7b8f63c13f2173f37de5
Anika Szuppa <anika.szuppa@zeit.de>
2013-11-14 16:45:43 +0100

    ZMO-84: test new subhead anker

--- src/zeit/frontend/test/test_templates.py
+++ src/zeit/frontend/test/test_templates.py
@@ -111,7 +111,7 @@ def test_macro_subpage_head_should_produce_markup(jinja2_env):
 
     # assert normal markup
     markup = '<div class="%s">' \
-        '<a name="kapitel1"></a>1 &mdash; Title</div>' % css_class
+        '<a name="kapitel1"></a><br>1 &mdash; Title</div>' % css_class
     lines = tpl.module.subpage_head(1, 'Title', css_class).splitlines()
     output = ""
     for line in lines:
