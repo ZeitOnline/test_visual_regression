7c3e3771215a163d0a321f8bb1768fc6f5874be6
Nico Brünjes <nico.bruenjes@zeit.de>
2013-12-12 16:18:04 +0100

    add selenium tests for ads

--- src/zeit/frontend/test/ads/test_ad_elements.py
+++ src/zeit/frontend/test/ads/test_ad_elements.py
@@ -3,6 +3,30 @@ import pytest
 screen_sizes = ((320, 480, True), (1024, 768, False))
 
 
+def test_ad_keyword_nofp_nowp(selenium_driver, testserver):
+    driver = selenium_driver
+    driver.set_window_size(729, 600)
+    driver.get('%s/artikel/01' % testserver.url)
+    d_sel = "div[data-place='topbanner']"
+    d_place = driver.find_element_by_css_selector(d_sel)
+    d_ad = d_place.find_element_by_id('iqadtile1')
+    d_script = d_ad.find_element_by_tag_name('script')
+    d_src = d_script.get_attribute("src")
+    assert('noiqdfireplace,noiqdwallpaper' in d_src)
+
+
+def test_ad_no_keyword(selenium_driver, testserver):
+    driver = selenium_driver
+    driver.set_window_size(1200, 600)
+    driver.get('%s/artikel/01' % testserver.url)
+    d_sel = "div[data-place='topbanner']"
+    d_place = driver.find_element_by_css_selector(d_sel)
+    d_ad = d_place.find_element_by_id('iqadtile1')
+    d_script = d_ad.find_element_by_tag_name('script')
+    d_src = d_script.get_attribute("src")
+    assert('noiqdfireplace,noiqdwallpaper' not in d_src)
+
+
 @pytest.fixture(scope='session', params=screen_sizes)
 def screen_size(request):
     return request.param
