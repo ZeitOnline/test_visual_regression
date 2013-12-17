fd8a8814c56e5905a47ea6cca222b2260b9f62ef
Ron Drongowski <ron.drongowski@gmail.com>
2013-12-17 13:03:02 +0100

    ZMO-50: Testing breadcrumbs

--- src/zeit/frontend/test/test_view.py
+++ src/zeit/frontend/test/test_view.py
@@ -4,23 +4,27 @@ from zeit.frontend import view
 
 def test_breadcumb_should_produce_expected_data():
     context = mock.Mock()
-    context.ressort = 'zmo'
+    context.ressort = 'mode'
     context.sub_ressort = 'lebensart'
     context.title = 'This is my title'
 
     view._navigation = {
         'start': ('Start', 'http://www.zeit.de/index', 'myid1'),
         'zmo': ('ZEIT Magazin', 'http://www.zeit.de/magazin/index', 'myid2'),
-        'lebensart': ('ZEIT Magazin',
+        'lebensart': ('lebensart',
                       'http://www.zeit.de/magazin/lebensart/index',
-                      'myid3'), }
+                      'myid3'),
+        'mode': ('mode',
+                      'http://www.zeit.de/magazin/mode/index',
+                      'myid4'), }
 
     article = view.Article(context, '')
 
     l = [
         ('Start', 'http://www.zeit.de/index', 'myid1'),
         ('ZEIT Magazin', 'http://www.zeit.de/magazin/index', 'myid2'),
-        ('ZEIT Magazin', 'http://www.zeit.de/magazin/lebensart/index',
+        ('mode', 'http://www.zeit.de/magazin/mode/index', 'myid4'),
+        ('lebensart', 'http://www.zeit.de/magazin/lebensart/index',
             'myid3'),
         ('This is my title', 'http://localhost'), ]
 
@@ -29,23 +33,26 @@ def test_breadcumb_should_produce_expected_data():
 
 def test_breadcrumb_should_be_shorter_if_ressort_or_sub_ressort_is_unknown():
     context = mock.Mock()
-    context.ressort = 'zmx'
-    context.sub_ressort = 'lebensart'
+    context.ressort = 'modex'
+    context.sub_ressort = 'lebensartx'
     context.title = 'This is my title'
 
     view._navigation = {
         'start': ('Start', 'http://www.zeit.de/index', 'myid1'),
         'zmo': ('ZEIT Magazin', 'http://www.zeit.de/magazin/index', 'myid2'),
-        'lebensart': ('ZEIT Magazin',
+        'lebensart': ('lebensart',
                       'http://www.zeit.de/magazin/lebensart/index',
-                      'myid3'), }
+                      'myid3'),
+        'mode': ('mode',
+                      'http://www.zeit.de/magazin/mode/index',
+                      'myid4'), }
 
     article = view.Article(context, '')
 
     l = [
         ('Start', 'http://www.zeit.de/index', 'myid1'),
-        ('ZEIT Magazin', 'http://www.zeit.de/magazin/lebensart/index',
-            'myid3'),
+        ('ZEIT Magazin', 'http://www.zeit.de/magazin/index',
+            'myid2'),
         ('This is my title', 'http://localhost'), ]
 
     assert article.breadcrumb == l
