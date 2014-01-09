cdcb8b8b5c3086e988b11a9fc50006ece17039df
Ron Drongowski <ron.drongowski@gmail.com>
2014-01-09 10:40:14 +0100

    FRIED-6: Test if repo path is configured.

--- src/zeit/frontend/application.py
+++ src/zeit/frontend/application.py
@@ -87,6 +87,12 @@ class Application(object):
             config=context)
         context.execute_actions()
 
+    def get_repository_path(self):
+        if ('repository_path' not in self.settings.keys() or
+                self.settings['repository_path'] == None):
+            return pkg_resources.resource_filename( __name__, 'data')
+        return self.settings['repository_path']
+
     def configure_product_config(self):
         # XXX make configurable, but see #36
         zope.app.appsetup.product.setProductConfiguration('zeit.cms', {
@@ -108,13 +114,8 @@ class Application(object):
                 'zeit.cms.tagging.tests', 'whitelist.xml'),
         })
 
-        if ('repository_path' not in self.settings.keys() or
-                self.settings['repository_path'] == ''):
-            self.settings['repository_path'] = pkg_resources.resource_filename(
-                __name__, 'data')
-
         zope.app.appsetup.product.setProductConfiguration('zeit.connector', {
-            'repository-path': self.settings['repository_path'],
+            'repository-path': self.get_repository_path(),
         })
 
         zope.app.appsetup.product.setProductConfiguration(
