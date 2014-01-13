f931e9ab81dac7c4abe17963aeec64e4718831aa
Andreas Zeidler <andi@pyfidelity.com>
2014-01-13 14:21:39 +0100

    as a demo, scale all media images down to 80x60

--- javascript/modules/images.js
+++ javascript/modules/images.js
@@ -2,9 +2,22 @@
 
 define(['jquery'], function() {
 
+    var prefix = function(width, height) {
+        var key = width + ':' + height + ':time';
+        var out = sjcl.hash.sha1.hash(key);
+        var digest = sjcl.codec.hex.fromBits(out);
+        return '/bitblt-' + width + 'x' + height + '-' + digest;
+    };
+
+    var scale = function(image, width, height) {
+        var origin = location.origin;
+        image.src = image.src.replace(origin, origin + prefix(width, height));
+    };
+
     var init = function() {
-        // sjcl can now be used here...
-        sjcl.encrypt("password", "data");
+        $('img.figure__media').each(function() {
+            scale(this, 80, 60);
+        });
     };
 
     return {
