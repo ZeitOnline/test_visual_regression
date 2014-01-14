b4ea2424da3c0f08b8d6764e8bc0699f10ff9007
Andreas Zeidler <andi@pyfidelity.com>
2014-01-14 18:35:36 +0100

    don't rescale & reload an image if the ratio between its target and original size is below a given threshold
    this is supposed to avoid reloads on mobile devices, although the threshold value probably needs tuning...

--- javascript/modules/images.js
+++ javascript/modules/images.js
@@ -2,6 +2,8 @@
 
 define(['jquery'], function() {
 
+    var rescale_threshold = 1.5;
+
     var prefix = function(width, height) {
         var key = width + ':' + height + ':time';
         var out = sjcl.hash.sha1.hash(key);
@@ -10,6 +12,9 @@ define(['jquery'], function() {
     };
 
     var rescale = function(image) {
+        if (image.width / image.naturalWidth < rescale_threshold) {
+            return;     // rescaling is not needed...
+        }
         var origin = location.origin;
         var token = prefix(image.width, image.height);
         image.src = image.src.replace(/\/bitblt-\d+x\d+-[a-z0-9]+/, token);
