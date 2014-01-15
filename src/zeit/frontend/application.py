992a298bb0b173422c0f5865352bac7d856f406c
Wolfgang Schnerring <ws@gocept.com>
2014-01-15 09:30:02 +0100

    Re FRIED-14: Take product config settings from the frontend.ini file
    * Put default values (using example files from the eggs) into the conftest settings
    * Call Application in test setup the same way it is called in production (i.e. the
      settings go into the kw params)
    * Remove Application.repository_path property, since it was not used anywhere

--- src/zeit/frontend/application.py
+++ src/zeit/frontend/application.py
@@ -5,6 +5,7 @@ import martian
 import pkg_resources
 import pyramid.config
 import pyramid_jinja2
+import urlparse
 import zeit.frontend
 import zeit.frontend.block
 import zeit.frontend.navigation
@@ -87,64 +88,37 @@ class Application(object):
             config=context)
         context.execute_actions()
 
-    @property
-    def repository_path(self):
-        if ('repository_path' not in self.settings.keys() or
-                self.settings['repository_path'] == None):
-            return pkg_resources.resource_filename( __name__, 'data')
-        return self.settings['repository_path']
-
     def configure_product_config(self):
-        # XXX make configurable, but see #36
-        zope.app.appsetup.product.setProductConfiguration('zeit.cms', {
-            'keyword-configuration': _product_url(
-                'zeit.cms.tagging.tests', 'keywords_config.xml'),
-            'source-badges': _product_url(
-                'zeit.cms.asset', 'badges.xml'),
-            'source-banners': _product_url(
-                'zeit.cms.content', 'banners.xml'),
-            'source-keyword': _product_url(
-                'zeit.cms.content', 'zeit-ontologie-prism.xml'),
-            'source-navigation': _product_url(
-                'zeit.cms.content', 'navigation.xml'),
-            'source-products': _product_url(
-                'zeit.cms.content', 'products.xml'),
-            'source-serie': _product_url(
-                'zeit.cms.content', 'serie.xml'),
-            'whitelist-url': _product_url(
-                'zeit.cms.tagging.tests', 'whitelist.xml'),
-        })
+        """Sets values of Zope Product Config used by vivi for configuration,
+        using settings from the WSGI ini file.
 
-        zope.app.appsetup.product.setProductConfiguration('zeit.connector', {
-            'repository-path': self.repository_path,
-        })
+        Requires the following naming convention in the ini file:
+            vivi_<PACKAGE>_<SETTING> = <VALUE>
+        for example
+            vivi_zeit.connector_repository-path = egg://zeit.frontend/data
 
-        zope.app.appsetup.product.setProductConfiguration(
-            'zeit.content.article', {
-                'genre-url': _product_url(
-                    'zeit.frontend', 'data/config/article-genres.xml'),
-                'image-layout-source': _product_url(
-                    'zeit.frontend',
-                    'data/config/article-image-layouts.xml'),
-                'video-layout-source': _product_url(
-                    'zeit.frontend',
-                    'data/config/article-video-layouts.xml'),
-                'htmlblock-layout-source': _product_url(
-                    'zeit.frontend',
-                    'data/config/article-htmlblock-layouts.xml'),
-            }
-        )
+        (XXX This is based on the assumption that vivi never uses an underscore
+        in a SETTING name.)
 
-        zope.app.appsetup.product.setProductConfiguration(
-            'zeit.magazin', {
-                'article-template-source': _product_url(
-                    'zeit.frontend',
-                    'data/config/article-templates.xml'),
-                'article-related-layout-source': _product_url(
-                    'zeit.frontend',
-                    'data/config/article-related-layouts.xml'),
-            }
-        )
+        For convenience we resolve egg:// URLs using pkg_resources into file://
+        URLs. This functionality should probably move to vivi, see VIV-288.
+
+        """
+        for key, value in self.settings.items():
+            if not key.startswith('vivi_'):
+                continue
+
+            ignored, package, setting = key.split('_')
+            if zope.app.appsetup.product.getProductConfiguration(
+                    package) is None:
+                zope.app.appsetup.product.setProductConfiguration(package, {})
+            config = zope.app.appsetup.product.getProductConfiguration(package)
+            value = maybe_convert_egg_url(value)
+            # XXX Stopgap until FRIED-12, since MockConnector does not
+            # understand file-URLs
+            if key == 'vivi_zeit.connector_repository-path':
+                value = value.replace('file://', '')
+            config[setting] = value
 
     @property
     def pipeline(self):
@@ -166,8 +140,12 @@ class Application(object):
 factory = Application()
 
 
-def _product_url(package, path):
-    return 'file://' + pkg_resources.resource_filename(package, path)
+def maybe_convert_egg_url(url):
+    if not url.startswith('egg://'):
+        return url
+    parts = urlparse.urlparse(url)
+    return 'file://' + pkg_resources.resource_filename(
+        parts.netloc, parts.path[1:])
 
 
 def translate_url(obj):
--- src/zeit/frontend/test/conftest.py
+++ src/zeit/frontend/test/conftest.py
@@ -9,6 +9,34 @@ settings = {
     'pyramid.debug_notfound': 'false',
     'pyramid.debug_routematch': 'false',
     'pyramid.debug_templates': 'false',
+
+    'vivi_zeit.connector_repository-path': 'egg://zeit.frontend/data',
+
+    'vivi_zeit.cms_keyword-configuration': (
+        'egg://zeit.cms.tagging.tests/keywords_config.xml'),
+    'vivi_zeit.cms_source-badges': 'egg://zeit.cms.asset/badges.xml',
+    'vivi_zeit.cms_source-banners': 'egg://zeit.cms.content/banners.xml',
+    'vivi_zeit.cms_source-keyword': (
+        'egg://zeit.cms.content/zeit-ontologie-prism.xml'),
+    'vivi_zeit.cms_source-navigation': (
+        'egg://zeit.cms.content/navigation.xml'),
+    'vivi_zeit.cms_source-products': 'egg://zeit.cms.content/products.xml',
+    'vivi_zeit.cms_source-serie': 'egg://zeit.cms.content/serie.xml',
+    'vivi_zeit.cms_whitelist-url': (
+        'egg://zeit.cms.tagging.tests/whitelist.xml'),
+
+    'vivi_zeit.content.article_genre-url': (
+        'egg://zeit.frontend/data/config/article-genres.xml'),
+    'vivi_zeit.content.article_image-layout-source': (
+        'egg://zeit.frontend/data/config/article-image-layouts.xml'),
+    'vivi_zeit.content.article_video-layout-source': (
+        'egg://zeit.frontend/data/config/article-video-layouts.xml'),
+    'vivi_zeit.content.article_htmlblock-layout-source': (
+        'egg://zeit.frontend/data/config/article-htmlblock-layouts.xml'),
+    'vivi_zeit.magazin_article-template-source': (
+        'egg://zeit.frontend/data/config/article-templates.xml'),
+    'vivi_zeit.magazin_article-related-layout-source': (
+        'egg://zeit.frontend/data/config/article-related-layouts.xml'),
 }
 
 browsers = {
@@ -17,10 +45,9 @@ browsers = {
 }
 
 
-
 @pytest.fixture(scope='session')
 def application():
-    return zeit.frontend.application.Application()(settings)
+    return zeit.frontend.application.Application()({}, **settings)
 
 
 @pytest.fixture(scope='session')
