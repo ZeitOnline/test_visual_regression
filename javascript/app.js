f1c2cb37033d20e274393563c0db2079982c4e04
Nico Brünjes <nico.bruenjes@zeit.de>
2014-01-20 20:33:50 +0100

    ZMO-312: add bxslider to app.js

--- javascript/app.js
+++ javascript/app.js
@@ -5,15 +5,31 @@ require.config({
 		"jquery": "libs/jquery-1.10.2.min",
 		"sjcl": "libs/sjcl"
 	},
-	shim: {
-		'modules/plugins/jquery.switchvideo': {
+	shim: [{
+		'libs/jquery.switchvideo': {
 			deps: [ 'jquery' ],
 			exports: 'jQuery.fn.switchVideo'
 		}
-	}
+	},{
+		'libs/jquery.bxslider': {
+			deps: [ 'jquery' ],
+			exports: 'jQuery.fn.bxSlider'
+		}
+	}]
 });
 
-require(['modules/fontloader', 'modules/breadcrumbs', 'modules/tabs', 'modules/comments', 'modules/main-nav', 'modules/adloader', 'modules/images', 'sjcl', 'modules/plugins/jquery.switchvideo'],
+require([
+	'modules/fontloader',
+	'modules/breadcrumbs',
+	'modules/tabs',
+	'modules/comments',
+	'modules/main-nav',
+	'modules/adloader',
+	'modules/images',
+	'sjcl',
+	'modules/plugins/jquery.switchvideo',
+	'libs/jquery.bxslider'
+	],
   function(fontloader, breadcrumbs, tabs, comments, main_nav, adloader, images) {
     fontloader.init();
     main_nav.init();
@@ -22,5 +38,6 @@ require(['modules/fontloader', 'modules/breadcrumbs', 'modules/tabs', 'modules/c
     comments.init();
     adloader.init();
     images.init();
-		$( "figure[data-video]" ).switchVideo();
+	$( "figure[data-video]" ).switchVideo();
+	$( "figure.gallery__inline").bxSlider();
 });
