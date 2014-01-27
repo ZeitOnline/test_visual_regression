ac17923a5105de471b11024c236821c1ae653d95
Nico Brünjes <nico.bruenjes@zeit.de>
2014-01-27 11:36:39 +0100

    add test for clickable elements

--- src/zeit/frontend/test/galleries/test_inline-gallery.py
+++ src/zeit/frontend/test/galleries/test_inline-gallery.py
@@ -1,4 +1,8 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test_inline_gallery_is_there(selenium_driver, testserver):
     driver = selenium_driver
@@ -6,3 +10,18 @@ def test_inline_gallery_is_there(selenium_driver, testserver):
     selector = ".inline-gallery"
     elem = driver.find_element_by_css_selector(selector)
     assert elem
+
+def test_inline_gallery_buttons(selenium_driver, testserver):
+    driver = selenium_driver
+    driver.get('%s/artikel/01' % testserver.url)
+    try:
+		element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "bx-wrapper")))
+		nextselector = ".bx-next"
+		prevselector = ".bx-prev"
+		nextbutton = driver.find_element_by_css_selector(nextselector)
+		prevbutton = driver.find_element_by_css_selector(prevselector)
+		assert nextbutton
+		assert prevbutton
+    except:
+		print "Timeout Gallery Script"
+		assert False
