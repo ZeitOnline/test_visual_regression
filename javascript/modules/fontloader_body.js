c31428eb436a54467f6bc4aa22ad3c5f3eeb97e1
Harry Keller <ich@harry-k.de>
2014-02-07 15:26:05 +0100

    ZMO-363: re-architect font loader

--- javascript/modules/fontloader_body.js
+++ javascript/modules/fontloader_body.js
@@ -2,66 +2,34 @@
 
 define(['jquery'], function() {
 	var fl = window.FontLoader;
-	var append_css = function(style) {
-		// create a style tag in the <head> filled with raw font-face declarations
-		var style_tag = window.document.createElement( "style" );
-		style_tag.innerHTML = style;
-		if( fl.inject_ref && fl.inject_ref.parentNode ) {
-			fl.inject_ref.parentNode.insertBefore( style_tag, fl.inject_ref );
-		}
+
+	var fetch_css = function(path, identifier, version) {
+		// let’s fetch font data asynchronously from the server and store it in local storage
+		$.ajax({
+			url: path,
+			success: function (ajaxed_style, font_pack_version) {
+				// we’ve got the fonts, store them in localstorage and apply them
+				localStorage.setItem(fl.storage_base_string + identifier, ajaxed_style);
+				localStorage.setItem(fl.storage_base_string + identifier + fl.storage_version_suffix, version);
+				fl.append_css(ajaxed_style);
+			}
+		});
 	};
 
-	var fetch_css = function(path, identifier, font_data) {
-		// if we already have font_data (from localstorage), make use of it immediately
-		if (font_data && font_data.length > 0) {
-			append_css(font_data);
-		} else {
-			// nope, we don’t: let’s fetch it asynchronously from the server
-			$.ajax({
-				url: path,
-				success: function (ajaxed_style) {
-					// we’ve got the fonts, store them in localstorage and apply them
-					localStorage.setItem(fl.storage_base_string + identifier, ajaxed_style);
-					append_css(ajaxed_style);
-				}
-			});
-		}
-	};
-
-	var load_fonts = function() {
-		var scheduled_fonts = [];
-		for (var i in fl.font_dictionary) {
-			if (i) {
-				var pack = fl.font_dictionary[i];
-				// apply fonts if no selector present or selector returns at least one element
-				if (!pack.selector || document.querySelectorAll(pack.selector).length >= 1) {
-					pack.data = localStorage.getItem(fl.storage_base_string + pack.identifier);
-					// if pack has priority or data is already in localstorage: apply immediately
-					if (pack.has_priority || pack.data) {
-						// drop it in as fast as possible
-						fetch_css(pack.path, pack.identifier, pack.data);
-					} else {
-						// schedule for later lazy loading
-						scheduled_fonts.push(fl.font_dictionary[i]);
+	var init = function() {
+		// lazy load queued fonts after document ready
+		$(function() {
+			for (var i in fl.scheduled_fonts) {
+				if (i) {
+					var pack = fl.font_dictionary[i];
+					// apply fonts only if no selector present or selector returns at least one element
+					if (!pack.selector || document.querySelectorAll(pack.selector).length >= 1) {
+						// get font data via ajax and store
+						fetch_css(fl.scheduled_fonts[i].path, fl.scheduled_fonts[i].identifier, fl.scheduled_fonts[i].version);
 					}
 				}
 			}
-		}
-		// lazy load fonts at last
-		window.onload = function() {
-			for (var i in scheduled_fonts) {
-				if (i) {
-					fetch_css(scheduled_fonts[i].path, scheduled_fonts[i].identifier);
-				}
-			}
-		};
-	};
-
-	// only load webfont css when we have a good guesstimate that the browser will support woff
-	var init = function() {
-		if (!fl.is_old_browser && !fl.fonts_loaded) {
-			load_fonts();
-		}
+		});
 	};
 
 	return {
