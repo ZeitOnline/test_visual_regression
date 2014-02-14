b3fc0bc8c443479e6671bce23c7e80306157569f
Anika Szuppa <anika.szuppa@zeit.de>
2014-02-14 15:23:41 +0100

    ZMO-437: add webm video source on opendata.zeit.de

--- src/zeit/frontend/test/video/test_background_video.py
+++ src/zeit/frontend/test/video/test_background_video.py
@@ -3,8 +3,8 @@ def test_background_video_html(selenium_driver, testserver):
     driver.get('%s/artikel/06' % testserver.url)
     cssclass = 'article__main-video--longform'
     for video in driver.find_elements_by_class_name(cssclass):
-        source = video.find_element_by_tag_name("source")
         fallback = video.find_element_by_tag_name("div")
-        assert 'video/mp4' == unicode(source.get_attribute("type"))
+        video.find_element_by_xpath("//source[@type='video/mp4']")
+        video.find_element_by_xpath("//source[@type='video/webm']")
         assert 'article__main-image--longform video--fallback' == unicode(
             fallback.get_attribute("class"))
