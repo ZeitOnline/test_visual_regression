674388d0e4b5cb03cb106fbf033ece2360fc5fd0
Ron Drongowski <ron.drongowski@gmail.com>
2014-03-06 12:41:29 +0100

    ZMO-326: Add tool to download a resource from CMS

---
+++ tools/download_resource.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+# To download a resource from the CMS vivi, call this script with an path as
+# argument. This path should _not_ begin with a '/' and ist a subpath of
+# /cms/work. 
+# To download resource and meta resource of the homepage call:
+# ./download_resource.sh index
+
+dav_host='http://cms-backend.zeit.de:9000';
+
+curl ${dav_host}/cms/work/$1 > resource
+
+outcurl=$(curl -X PROPFIND --header "depth:1" ${dav_host}/cms/work/$1 2> /dev/null |xsltproc meta_dav2xml.xslt -)
+
+echo $outcurl |  xmllint --format - > resource.meta
