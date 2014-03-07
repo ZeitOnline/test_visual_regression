4e7a52b61e8ad73c6b60b7508a45c650f325f318
Ron Drongowski <ron.drongowski@gmail.com>
2014-03-07 15:01:44 +0100

    ZMO-430: Restructure views for a better overview

--- src/zeit/frontend/test/test_view.py
+++ src/zeit/frontend/test/test_view.py
@@ -1,6 +1,7 @@
 from urllib2 import HTTPError
 from zeit.content.article.edit.reference import Gallery
 from zeit.frontend import view
+from zeit.frontend import view_article
 from zeit.frontend.block import InlineGalleryImage
 from zope.testbrowser.browser import Browser
 import mock
@@ -9,14 +10,13 @@ import requests
 import zeit.cms.interfaces
 
 
-
 def test_breadcumb_should_produce_expected_data():
     context = mock.Mock()
     context.ressort = 'mode'
     context.sub_ressort = 'lebensart'
     context.title = 'This is my title'
 
-    view._navigation = {
+    view_article._navigation = {
         'start': ('Start', 'http://www.zeit.de/index', 'myid1'),
         'zmo': ('ZEIT Magazin', 'http://www.zeit.de/magazin/index', 'myid2'),
         'lebensart': ('lebensart',
@@ -24,7 +24,7 @@ def test_breadcumb_should_produce_expected_data():
                       'myid3'),
         'mode': ('mode', 'http://www.zeit.de/magazin/mode/index', 'myid4'), }
 
-    article = view.Article(context, '')
+    article = view_article.Article(context, '')
 
     l = [
         ('Start', 'http://www.zeit.de/index', 'myid1'),
@@ -43,7 +43,7 @@ def test_breadcrumb_should_be_shorter_if_ressort_or_sub_ressort_is_unknown():
     context.sub_ressort = 'lebensartx'
     context.title = 'This is my title'
 
-    view._navigation = {
+    view_article._navigation = {
         'start': ('Start', 'http://www.zeit.de/index', 'myid1'),
         'zmo': ('ZEIT Magazin', 'http://www.zeit.de/magazin/index', 'myid2'),
         'lebensart': ('lebensart',
@@ -51,7 +51,7 @@ def test_breadcrumb_should_be_shorter_if_ressort_or_sub_ressort_is_unknown():
                       'myid3'),
         'mode': ('mode', 'http://www.zeit.de/magazin/mode/index', 'myid4'), }
 
-    article = view.Article(context, '')
+    article = view_article.Article(context, '')
 
     l = [
         ('Start', 'http://www.zeit.de/index', 'myid1'),
@@ -64,21 +64,21 @@ def test_breadcrumb_should_be_shorter_if_ressort_or_sub_ressort_is_unknown():
 
 def test_header_img_should_be_first_image_of_content_blocks(application):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/05')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     url = 'http://xml.zeit.de/exampleimages/artikel/05/01.jpg'
     assert article_view.header_img.src == url
 
 
 def test_header_img_should_be_none_if_we_have_a_wrong_layout(application):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/01')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert article_view.header_img is None
 
 
 def test_header_video_should_be_first_video_of_content_blocks(application):
     vid_url = 'http://xml.zeit.de/artikel/header_video'
     context = zeit.cms.interfaces.ICMSContent(vid_url)
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     url = 'http://brightcove.vo.llnwd.net/pd15/media/18140073001/201401/' \
         '3809/18140073001_3094832002001_Aurora-Borealis--Northern-Lights' \
         '--Time-lapses-in-Norway-Polarlichter-Der-Himmel-brennt.mp4'
@@ -87,19 +87,19 @@ def test_header_video_should_be_first_video_of_content_blocks(application):
 
 def test_header_video_should_be_none_if_we_have_a_wrong_layout(application):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/01')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert article_view.header_video is None
 
 
 def test_header_elem_should_be_img_if_there_is_a_header_img(application):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/05')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert type(article_view.header_elem) == zeit.frontend.block.HeaderImage
 
 
 def test_header_elem_should_be_video_if_there_is_a_header_video(application):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/header_video')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert type(article_view.header_elem) == zeit.frontend.block.HeaderVideo
 
 
@@ -173,44 +173,44 @@ def test_content_should_have_type(testserver):
 
 def test_tracking_type_is_provided(testserver):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/02')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert article_view.tracking_type == 'Artikel'
 
 
 def test_artikel02_has_lebensart_ressort(testserver):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/02')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert article_view.ressort == 'lebensart'
 
 
 def test_artikel02_has_mode_sub_ressort(testserver):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/02')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert article_view.sub_ressort == 'mode'
 
 
 def test_artikel02_has_correct_banner_channel(testserver):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/02')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert article_view.banner_channel == 'lebensart/mode/article'
 
 
 def test_artikel05_has_rankedTagsList(testserver):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/05')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert article_view.rankedTagsList is not None
     assert article_view.rankedTagsList != ''
 
 
 def test_artikel05_has_set_text_length(testserver):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/05')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert article_view.text_length is not None
 
 
 def test_article05_has_date_formats(testserver):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/05')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert article_view.date_last_published_semantic is not None
     assert article_view.date_first_released is not None
     assert article_view.show_article_date is not None
@@ -218,26 +218,26 @@ def test_article05_has_date_formats(testserver):
 
 def test_article01__has_correct_twitter_card_type(testserver):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/01')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert article_view.twitter_card_type == 'summary'
 
 
 def test_article05_has_correct_twitter_card_type(testserver):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/05')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert article_view.twitter_card_type == 'summary_large_image'
 
 
 def test_article01_has_correct_sharing_img_src(testserver):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/01')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert article_view.sharing_img.src == \
         'http://xml.zeit.de/exampleimages/artikel/01/01.jpg'
 
 
 def test_article06_has_correct_sharing_img_video_still(testserver):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/06')
-    article_view = view.Article(context, '')
+    article_view = view_article.Article(context, '')
     assert article_view.sharing_img.video_still == \
         'http://brightcove.vo.llnwd.net/d21/unsecured/media/18140073001/' \
         '201401/3097/18140073001_3094729885001_7x.jpg'
