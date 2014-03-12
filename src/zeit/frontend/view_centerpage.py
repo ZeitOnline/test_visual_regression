e91498cb9ca4d70e5c28ca98154c647c2dc54a5a
Ron Drongowski <ron.drongowski@gmail.com>
2014-03-12 16:53:02 +0100

    ZMO-511: Default template structure for CPs

--- src/zeit/frontend/view_centerpage.py
+++ src/zeit/frontend/view_centerpage.py
@@ -1,19 +1,5 @@
-from babel.dates import get_timezone
-from datetime import date
-from pyramid.renderers import render_to_response
-from pyramid.response import Response
-from pyramid.view import notfound_view_config
 from pyramid.view import view_config
-from zeit.cms.workflow.interfaces import IPublishInfo
-from zeit.content.article.edit.interfaces import IImage
-from zeit.content.article.edit.interfaces import IVideo
-from zeit.content.image.interfaces import IImageMetadata
-from zeit.frontend.log import access_log
-from zeit.magazin.interfaces import IArticleTemplateSettings, INextRead
-from zope.component import providedBy
 import logging
-import os.path
-import pyramid.response
 import zeit.connector.connector
 import zeit.connector.interfaces
 import zeit.content.cp.interfaces
