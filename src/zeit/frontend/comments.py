566421b585a4cac31aa6378ecbd61bbec7ad4ecb
Thomas Baumann <thomas.baumann@zeit.de>
2014-03-20 14:17:54 +0100

    ZMO-66: Bugfix

--- src/zeit/frontend/comments.py
+++ src/zeit/frontend/comments.py
@@ -19,7 +19,7 @@ class Agatho(object):
         #random='?r='+str(randint(2,90000))
         try:
             #return etree.parse('%s%s%s' % (self.entry_point, path_of_article(unique_id),random))
-            return etree.parse('%s%s%s' % (self.entry_point, path_of_article(unique_id)))
+            return etree.parse('%s%s' % (self.entry_point, path_of_article(unique_id)))
         except IOError: # lxml reports a 404 as IOError, 404 code signals that no thread exists for that article
             return None
 
