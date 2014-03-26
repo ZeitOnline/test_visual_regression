a4c6ae1dbaa636f5367e1ece3e0d2a79a24c248e
Thomas Baumann <thomas.baumann@zeit.de>
2014-03-26 16:54:23 +0100

    ZMO-502 script for creating commentsections
    as first step: for files in data/articles folder

---
+++ tools/create_resource_commentsection.sh
@@ -0,0 +1,26 @@
+#_INFILE_=$1
+#for RES in $(cat $_INFILE_); do
+path_to_articles='../src/zeit/frontend/data/artikel'
+service_add_commentsection='http://localhost:8888/agatho/commentsection'
+for RES in `ls $path_to_articles`; do
+echo $path_to_articles/$RES
+
+		if ! grep -q "<article" $path_to_articles/$RES && \
+            ! grep -q "<gallery" $path_to_articles/$RES && \
+            ! grep -q "<video" $path_to_articles/$RES
+		then
+			continue
+		fi
+		if grep -q "show_commentthread.*no" $path_to_articles/$RES
+		then
+			continue
+		fi
+
+
+  #HEADER=$(echo $RES | sed s/^work/http:\\/\\/xml.zeit.de/g) 
+		echo "curl -i -X POST -H \"X-uniqueId: http://localhost:9090/artikel/$RES\" \
+            -H 'Content-Type: text/xml' \
+            --data-binary @$path_to_articles/$RES  $service_add_commentsection"
+    curl -i -X POST -H "X-uniqueId: http://localhost:9090/artikel/$RES" -H 'Content-Type: text/xml' --data-binary @$(echo $path_to_articles/$RES) $service_add_commentsection
+            >>$LOG 2>>/tmp/update_comments.error
+done
