e895feaf0fd96fa61eeaa9b7ae1ee98a8458dda6
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2014-04-09 10:48:31 +0200

    make JS animated scrolling more failsafe and accept IDs

--- javascript/modules/plugins/jquery.animatescroll.js
+++ javascript/modules/plugins/jquery.animatescroll.js
@@ -9,12 +9,23 @@
     /**
      * run through links that jump to anchors
      */
-    $(this).each(function(){
+    $(this).each(function() {
       $(this).click(function(e) {
+        var anchor = $(this).attr('href').slice(1), // remove '#'
+          target;
+
+        if (anchor) {
+          target = document.getElementById(anchor) || document.getElementsByName(anchor)[0];
+        }
+
+        if (!target) {
+          return;
+        }
+
         e.preventDefault();
-        var anchor = $(this).attr('href').replace('#','');
+
         $('html, body').animate({
-          scrollTop: $('a[name='+anchor+']').eq(0).offset().top
+          scrollTop: $(target).offset().top
         }, 500);
       });
     });
