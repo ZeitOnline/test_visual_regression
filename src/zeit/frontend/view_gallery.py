36179973ef125d0061ad16fd36d9c67c976bf016
Dominik Hoppe <dominik.hoppe@zeit.de>
2014-04-09 14:57:54 +0200

    ZMO-508: Add boilerplate block for gally images

--- src/zeit/frontend/view_gallery.py
+++ src/zeit/frontend/view_gallery.py
@@ -3,6 +3,9 @@ from pyramid.view import view_config
 
 
 @view_config(context=zeit.content.gallery.interfaces.IGallery,
-            renderer='templates/gallery.html')
+             renderer='templates/gallery.html')
 class Gallery(zeit.frontend.view.Content):
-    pass
+
+    @property
+    def images(self):
+        return [self.context[i] for i in self.context]
