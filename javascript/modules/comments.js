faedfbbe2dfa2cc7b5ae5f2a9af8a17482304a92
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2014-04-11 18:30:41 +0200

    [ZMO-558] implement reply to comment functions and prepare report comment. Add debounced window resize event handler.

--- javascript/modules/comments.js
+++ javascript/modules/comments.js
@@ -1,5 +1,5 @@
-/* global console, define, alert */
-define(['jquery', 'modules/tabs'], function() {
+/* global console, define, alert, _ */
+define(['jquery', 'underscore', 'modules/tabs'], function() {
 
     var $comments_trigger = $('#js-comments-trigger');
     var $comments = $('#js-comments');
@@ -9,13 +9,13 @@ define(['jquery', 'modules/tabs'], function() {
     var $comments_active_list;
     var $comments_older = $('#js-comments-body-older');
     var $comments_newer = $('#js-comments-body-newer');
-    var comments_body_height = 0;
-    var window_width = 0;
+    var comments_body_height = null;
+    var window_width = null;
 
     /**
      * handles comment pagination
      */
-    var recalculate_pagination = function() {
+    var calculatePagination = function() {
 
         // handle tablet/desktop size with paginated comments
         if (window_width >= 768) {
@@ -38,23 +38,152 @@ define(['jquery', 'modules/tabs'], function() {
 
         // handle mobile widths
         } else {
-            $comments_body.css('height', 'auto');
+            $comments_body.css('height', '');
         }
     };
 
+    /**
+     * Reply to comment
+     */
     var replyToComment = function() {
-        var id = this.getAttribute('data-id');
+        var cid  = this.getAttribute('data-cid'),
+            name = this.getAttribute('data-name'),
+            form = document.forms['js-comments-form'],
+            node = $(form).find('.comments__recipient').first(),
+            html = 'Antwort auf: <a href="#cid-' + cid + '">' + name + '</a>';
 
-        document.forms['comments-form'].elements.pid.value = id;
+        form.elements.pid.value = cid;
+        node.html(html).find("a[href^='#']").animateScroll();
     };
 
     /**
-     * handles comment events
+     * Report comment
+     */
+    var reportComment = function() {
+        var cid  = this.getAttribute('data-cid'),
+            comment = $('#cid-' + cid),
+            form = comment.next('.comment__report__form'),
+            template, templateData;
+
+        if ( ! form.length ) {
+            template = _.template(
+                $('#js-report-comment-template').html()
+            );
+
+            templateData = {
+                commentId: cid
+            };
+
+            form = $(template(templateData)).insertAfter(comment);
+        }
+
+        form.slideToggle();
+    };
+
+    /**
+     * Cancel report
+     */
+    var cancelReport = function(e) {
+        e.preventDefault();
+
+        $(this).closest('.comment__report__form').slideUp();
+    };
+
+    /**
+     * Toggle comments
+     */
+    var toggleComments = function() {
+        $page_wrap_inner.toggleClass('show-comments');
+
+        // this should not be done on every toggle, but we have to do it once *after* comments get visible. refactor
+        calculatePagination();
+    };
+
+    /**
+     * Get window inner width
+     */
+    var getWindowWidth = function() {
+        return document.documentElement.clientWidth || document.body.clientWidth || $(document).width();
+    };
+
+    /**
+     * Initialize layout
+     */
+    var initLayout = function() {
+        window_width = getWindowWidth();
+
+        if (window_width >= 1280) {
+            // on big screens find out how much outside space there is
+            var comments_width = window_width - $page_wrap_inner.outerWidth();
+            // restrict width of comments
+            if (comments_width > 700) {
+                comments_width = 700;
+            }
+            $comments.css('width', comments_width);
+        } else {
+            // mobile case: show full width comments
+            $comments.css('width', '');
+        }
+
+        // calculate if we need pagination
+        calculatePagination();
+    };
+
+    /**
+     * Update layout, debounced
+     */
+    var updateLayout = _.debounce(function(e) {
+
+        initLayout();
+
+    }, 250); // Maximum run of once per 250 milliseconds
+
+    /**
+     * Initialize comment section
      */
     var init = function() {
 
+        initLayout();
+
         // register event handlers
         $comments_body.on('click', '.js-reply-to-comment', replyToComment);
+        $comments_body.on('click', '.js-report-comment', reportComment);
+        $comments_body.on('click', '.js-cancel-report', cancelReport);
+        $comments_trigger.click(toggleComments);
+        $(window).on('resize', updateLayout);
+        // enable animation of dynamically added anchor links
+        $comments_body.on('click', ".js-cancel-report a[href^='#']", function(e){ $(this).animateScroll(); });
+
+        // mimic hover for the sake of grunticons - change later (SVG Sprites FTW)
+        $('.icon-flag').hover(function() {
+            $(this).removeClass('icon-flag').addClass('icon-flag-hover');
+        }, function() {
+            $(this).removeClass('icon-flag-hover').addClass('icon-flag');
+        });
+        $('.icon-reply').hover(function() {
+            $(this).removeClass('icon-reply').addClass('icon-reply-hover');
+        }, function() {
+            $(this).removeClass('icon-reply-hover').addClass('icon-reply');
+        });
+
+        // on document ready: check for url hash to enable anchor links and return urls
+        $(function() {
+            var anchor = window.location.hash.slice(1);
+
+            if (/^cid-\d/.test(anchor)) {
+                toggleComments();
+
+                var target = document.getElementById(anchor) || document.getElementsByName(anchor)[0];
+
+                if (!target) {
+                    return;
+                }
+
+                $('html, body').animate({
+                    scrollTop: $(target).offset().top
+                }, 500);
+            }
+        });
 
         // bind pagination for older comments
         $comments_older.click(function() {
@@ -100,33 +229,9 @@ define(['jquery', 'modules/tabs'], function() {
         // handle tab switch: recalculate comment metrics for new comment list
         $comments_tabs_head.find('.tabs__head__tab').click(function(e) {
             $comments_body.removeClass('show-newer-trigger show-older-trigger');
-            recalculate_pagination();
+            calculatePagination();
         });
 
-        // bind comments trigger to toggle comments
-        $comments_trigger.click(function() {
-
-            window_width = $(window).innerWidth();
-
-            if (window_width >= 1280) {
-                // on big screens find out how much outside space there is
-                var comments_width = window_width - $page_wrap_inner.outerWidth();
-                // restrict width of comments
-                if (comments_width > 700) {
-                    comments_width = 700;
-                }
-                $comments.css('width', comments_width);
-            } else {
-                // mobile case: show full width comments
-                $comments.css('width', '100%');
-            }
-
-            // finally show the comments
-            $page_wrap_inner.toggleClass('show-comments');
-
-            // calculate if we need pagination
-            recalculate_pagination();
-        });
     };
 
     return {
