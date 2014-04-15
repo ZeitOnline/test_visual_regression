29e681adba8e4aef4c8418558f8087bba383af77
Malte Modrow <malte.modrow@zeit.de>
2014-04-15 17:15:18 +0200

    ZMO-508: add gallery description

--- src/zeit/frontend/view_gallery.py
+++ src/zeit/frontend/view_gallery.py
@@ -1,4 +1,5 @@
 import zeit.content.gallery.interfaces
+import zeit.wysiwyg.interfaces
 from pyramid.view import view_config
 
 
@@ -10,5 +11,9 @@ class Gallery(zeit.frontend.view.Content):
     def images(self):
         return [self.context[i] for i in self.context]
 
-    def __call__(self):
-         import pdb;pdb.set_trace()
+    @property
+    def galleryText(self):
+        return zeit.wysiwyg.interfaces.IHTMLContent(self.context).html
+
+    # def __call__(self):
+    #      import pdb;pdb.set_trace()
