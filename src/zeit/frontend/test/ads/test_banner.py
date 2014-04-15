74cbe27c7dc5286ce12d719e06bb64a18b80757a
Nico Brünjes <nico.bruenjes@zeit.de>
2014-04-15 11:21:54 +0200

    ZMO-382: fix tests and change naming scheme of tests

--- src/zeit/frontend/test/ads/test_banner.py
+++ src/zeit/frontend/test/ads/test_banner.py
@@ -5,7 +5,7 @@ from zeit.frontend.banner import Place
 import zeit.cms.interfaces
 
 
-def test_place_should_be_serialized(testserver):
+def test_banner_place_should_be_serialized(testserver):
     place = Place(1, ['728x90'], True, label='')
     assert place.__dict__ == {'dcopt': 'ist', 'diuqilon': True,
                               'label': '', 'min_width': 0, 'name': 'tile_1',
@@ -13,18 +13,23 @@ def test_place_should_be_serialized(testserver):
                               'sizes': ['728x90'], 'tile': 1}
 
 
-def test_place_should_raise_on_index_error(testserver):
+def test_banner_place_should_raise_on_index_error(testserver):
     with pytest.raises(IndexError):
         Place(1, '123x456', True, label='')
 
 
-def test_view_banner_should_return_Place_if_tile_present(application):
+def test_banner_list_should_be_sorted(testserver):
+    tiles = [place.tile for place in zeit.frontend.banner.banner_list]
+    assert sorted(tiles) == tiles
+
+
+def test_banner_view_should_return_Place_if_tile_present(application):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/02')
     article_view = view_article.Article(context, '')
     assert isinstance(article_view.banner(1), zeit.frontend.banner.Place)
 
 
-def test_view_banner_should_return_None_if_tile_is_not_present(application):
+def test_banner_view_should_return_None_if_tile_is_not_present(application):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/02')
     article_view = view_article.Article(context, '')
     assert article_view.banner(999) is None
