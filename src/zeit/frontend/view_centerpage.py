2bdf256b487072477a859028218b2c0898552b05
Thomas Baumann <thomas.baumann@zeit.de>
2014-04-17 17:53:01 +0200

    ZMO-538: return commentscount by view function...
    ...instead of setting it as an attribute on the teaser object

--- src/zeit/frontend/view_centerpage.py
+++ src/zeit/frontend/view_centerpage.py
@@ -22,8 +22,8 @@ class Centerpage(zeit.frontend.view.Base):
     def pagetitle(self):
         #ToDo(T.B.) should be, doesn't work
         #return self.context.html-meta-title
-        return 'Lebensart - Mode, Essen und Trinken, \
-                Partnerschaft | ZEIT ONLINE'
+        return 'Lebensart - Mode, Essen und Trinken, '+ \
+                'Partnerschaft | ZEIT ONLINE'
 
     @property
     def pagedescription(self):
@@ -50,18 +50,18 @@ class Centerpage(zeit.frontend.view.Base):
 
     @property
     def area_lead(self):
-        unique_id_comments = comments.comments_per_unique_id(self)
         teaserblock_list = self.context['lead'].values()
         for teaserblock in teaserblock_list:
-            for teaser in teaserblock:
-                try:
-                    teaser.__setattr__('commentscount', unique_id_comments[
-                    '/'+urlparse.urlparse(teaser.uniqueId).path[1:]])
-                except KeyError:
-                    teaser.__setattr__('commentscount', None)
             if teaserblock.layout.id == 'zmo-leader-fullwidth':
                 teaserblock_list.remove(teaserblock)
         return teaserblock_list
+
+    def teaser_get_commentcount(self, uniqueId):
+        unique_id_comments = comments.comments_per_unique_id(self)
+        try:
+            return unique_id_comments['/'+urlparse.urlparse(uniqueId).path[1:]]
+        except KeyError:
+            return None
 
     @property
     def area_lead_full_teaser(self):
