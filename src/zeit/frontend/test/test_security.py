c3acb37c66a9d8389812302db7807602236b1e35
Tom Lazar <tomster@pyfidelity.com>
2014-04-30 14:42:46 +0200

    if the user id has changed, invalidate the cache.

--- src/zeit/frontend/test/test_security.py
+++ src/zeit/frontend/test/test_security.py
@@ -19,8 +19,24 @@ def test_cookieless_request_clears_session(policy, dummy_request):
 
 def test_session_cache_has_precedence(policy, dummy_request):
 	dummy_request.cookies['drupal-userid'] = 23
-	dummy_request.session[ZMO_USER_KEY] = dict(uid='bar')
-	assert policy.authenticated_userid(dummy_request) == 'bar'
+	dummy_request.session[ZMO_USER_KEY] = dict(uid=23, name='s3crit')
+	assert policy.authenticated_userid(dummy_request) == 23
+	assert dummy_request.session[ZMO_USER_KEY]['name'] == 's3crit'
+
+
+def test_session_cache_cleared_when_id_changes(policy, dummy_request):
+	dummy_request.cookies['drupal-userid'] = 23
+	# session still contains old user id and sensitive information
+	dummy_request.session[ZMO_USER_KEY] = dict(uid=42, name='s3crit')
+	with patch('zeit.frontend.security.Request.get_response') as mocked_getter:
+		mocked_response = MagicMock()
+		mocked_response.body = '''<user><uid>457322</uid><name>test-friedbert</name><mail>test-friedbert@example.com</mail><picture/><roles><role>authenticated user</role></roles><profile/></user>		
+		'''
+		mocked_getter.return_value = mocked_response
+		dummy_request.cookies['drupal-userid'] = 23
+		dummy_request.headers['Cookie'] = ''
+		assert policy.authenticated_userid(dummy_request) == '457322'
+		assert dummy_request.session[ZMO_USER_KEY]['name'] == 'test-friedbert'
 
 
 def test_empty_cache_triggers_backend_fills_cache(policy, dummy_request):
