9619149e4f0866aad9c12188f82654c36624831c
Nico Brünjes <nico.bruenjes@zeit.de>
2014-05-02 17:01:54 +0200

    Bugfix: accept columns w/o a header image

--- src/zeit/frontend/test/test_view.py
+++ src/zeit/frontend/test/test_view.py
@@ -162,6 +162,10 @@ def test_column_should_have_header_image(testserver):
     assert '<div class="scaled-image">' in browser.contents
     assert '<img class=" figure__media"' in browser.contents
 
+def test_column_should_not_have_header_image(testserver):
+    browser = Browser('%s/artikel/standardkolumne-ohne-bild-beispiel' % testserver.url)
+    assert '<div class="article__column__headerimage">' not in browser.contents
+
 
 def test_health_check_should_response_and_have_status_200(testserver):
     browser = Browser('%s/health_check' % testserver.url)
