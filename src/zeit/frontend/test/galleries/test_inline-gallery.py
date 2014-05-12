7a7e4408d9dbe0da77a44647b45b08cb47583a06
Nicolas Drebenstedt <cutoffthetop@me.com>
2014-05-12 09:37:06 +0200

    ZMO-759: PEP8 and pyflakes

--- src/zeit/frontend/test/galleries/test_inline-gallery.py
+++ src/zeit/frontend/test/galleries/test_inline-gallery.py
@@ -1,8 +1,7 @@
-import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 
 def test_inline_gallery_is_there(selenium_driver, testserver):
     driver = selenium_driver
@@ -11,33 +10,36 @@ def test_inline_gallery_is_there(selenium_driver, testserver):
     elem = driver.find_element_by_css_selector(selector)
     assert elem
 
+
 def test_inline_gallery_buttons(selenium_driver, testserver):
     driver = selenium_driver
     driver.get('%s/artikel/01' % testserver.url)
     try:
-      element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "bx-wrapper")))
-      nextselector = ".bx-next"
-      prevselector = ".bx-prev"
-      onextselector = ".bx-overlay-next"
-      oprevselector = ".bx-overlay-prev"
-      # test navigation buttons
-      nextbutton = driver.find_element_by_css_selector(nextselector)
-      prevbutton = driver.find_element_by_css_selector(prevselector)
-      assert nextbutton
-      assert prevbutton
-      nextbutton.click()
-      prevbutton.click()
-      # test overlay buttons
-      overlaynext = driver.find_element_by_css_selector(onextselector)
-      elemOpacity = driver.execute_script('return $(".bx-overlay-next").css("opacity")')
-      overlayprev = driver.find_element_by_css_selector(onextselector)
-      assert overlaynext
-      assert overlayprev
-      overlaynext.click()
-      elemOpacityLater = driver.execute_script('return $(".bx-overlay-next").css("opacity")')
-      overlayprev.click()
-      # opacity should have changed
-      assert elemOpacity != elemOpacityLater
+        cond = EC.presence_of_element_located((By.CLASS_NAME, "bx-wrapper"))
+        WebDriverWait(driver, 10).until(cond)
+        nextselector = ".bx-next"
+        prevselector = ".bx-prev"
+        onextselector = ".bx-overlay-next"
+        # test navigation buttons
+        nextbutton = driver.find_element_by_css_selector(nextselector)
+        prevbutton = driver.find_element_by_css_selector(prevselector)
+        assert nextbutton
+        assert prevbutton
+        nextbutton.click()
+        prevbutton.click()
+        # test overlay buttons
+        overlaynext = driver.find_element_by_css_selector(onextselector)
+        script = 'return $(".bx-overlay-next").css("opacity")'
+        elemOpacity = driver.execute_script(script)
+        overlayprev = driver.find_element_by_css_selector(onextselector)
+        assert overlaynext
+        assert overlayprev
+        overlaynext.click()
+        script = 'return $(".bx-overlay-next").css("opacity")'
+        elemOpacityLater = driver.execute_script(script)
+        overlayprev.click()
+        # opacity should have changed
+        assert elemOpacity != elemOpacityLater
     except:
-      print "Timeout Gallery Script"
-      assert False
+        print "Timeout Gallery Script"
+        assert False
