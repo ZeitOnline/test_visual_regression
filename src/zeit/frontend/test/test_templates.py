de48530cd99dbd18e31966b6a47b9712cd744d6f
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2014-05-28 17:10:22 +0200

    Update test

--- src/zeit/frontend/test/test_templates.py
+++ src/zeit/frontend/test/test_templates.py
@@ -957,10 +957,10 @@ def test_macro_click_tracking_should_produce_correct_js(jinja2_env):
         output += line.strip()
 
     assert 'var clickCount = {' in output
-    assert 'get_channel: function(){' in output
+    assert 'get_channel: function() {' in output
     assert "var channel = 'test' != 'False' ? 'test' : ''" in output
-    assert 'webtrekk: function ( mode ) {' in output
-    assert 'ga: function( mode ) {' in output
+    assert 'webtrekk: function(id) {' in output
+    assert 'ga: function(id) {' in output
     assert 'ivw: function() {' in output
-    assert 'cc: function(){' in output
-    assert 'all: function( mode ){' in output
+    assert 'cc: function() {' in output
+    assert 'all: function(id) {' in output
