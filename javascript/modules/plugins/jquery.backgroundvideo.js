4f7c8f7bb2fb397a54c92c5430c51a9537d332ab
anszu <anika.szuppa@zeit.de>
2014-07-18 15:00:49 +0200

    ZMO-860: set blank spaces

--- javascript/modules/plugins/jquery.backgroundvideo.js
+++ javascript/modules/plugins/jquery.backgroundvideo.js
@@ -35,7 +35,7 @@
 			el.setVideoPosition( $element );
 
 			//start video
-			$(video).get(0).play();
+			$( video ).get(0).play();
 
 			//on video play, hide image and show video
 			$( video ).on( "play", function() {
