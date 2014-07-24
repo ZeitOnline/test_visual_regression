5c687c7f0feb0e2ef93405cdbe0927db564e6469
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2014-07-24 11:38:38 +0200

    Preparing release 0.9.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.frontend',
-    version='0.10.dev0',
+    version='0.9.2',
     author=(
         'Thomas Baumann, Nico Brünjes, Nicolas Drebenstedt, Ron Drongowski, '
         'Dominik Hoppe, Marco Kaiser, Harry Keller, Tom Lazar, Thomas Lotze, '
