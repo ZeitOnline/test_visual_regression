64c41e5ba1a287c50bdfc8682c5b5df1b74eb695
Nicolas Drebenstedt <cutoffthetop@me.com>
2014-08-08 12:52:11 +0200

    Back to development: 0.11.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeit.frontend',
-    version='0.11.5',
+    version='0.11.6.dev0',
     author=(
         'Thomas Baumann, Nico Brünjes, Nicolas Drebenstedt, Ron Drongowski, '
         'Dominik Hoppe, Marco Kaiser, Harry Keller, Tom Lazar, Thomas Lotze, '
