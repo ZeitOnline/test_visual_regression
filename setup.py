1ccb5ddf56ad2aff7b74b036eed313ae18ae1ff5
Nicolas Drebenstedt <cutoffthetop@me.com>
2014-08-29 11:01:44 +0200

    VIV-440: Rollback solr package because of TAL registration errors

--- setup.py
+++ setup.py
@@ -62,7 +62,6 @@ setup(
         'zeit.newsletter >= 1.0.0b2.dev0',
         'zeit.wysiwyg',
         'zeit.seo',
-        'zeit.solr',
         'zope.app.appsetup',
         'zope.component',
         'zope.configuration',
