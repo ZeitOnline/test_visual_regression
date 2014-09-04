17b08a4b2484b2ed686543de975195b4cd907150
Dominik Hoppe <dominik.hoppe@zeit.de>
2014-09-04 10:42:24 +0200

    Preparing release 0.15.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='zeit.frontend',
     url='https://github.com/ZeitOnline/friedbert',
-    version='0.16.dev0',
+    version='0.15.1',
     author=(
         'Thomas Baumann, Nico Brünjes, Nicolas Drebenstedt, Ron Drongowski, '
         'Dominik Hoppe, Marco Kaiser, Harry Keller, Tom Lazar, Thomas Lotze, '
