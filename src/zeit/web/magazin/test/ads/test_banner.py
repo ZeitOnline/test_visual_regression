419a6e0ee159c05f05d65600f709f272f4509c3e
Nicolas Drebenstedt <cutoffthetop@me.com>
2014-09-15 16:40:08 +0200

    ZON-923: More migration clean-up and fixes

--- src/zeit/web/magazin/test/ads/test_banner.py
+++ src/zeit/web/magazin/test/ads/test_banner.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 import mock
-from zeit.web.magazin import view_article
-from zeit.web.core.banner import Place
 import pytest
+
 import zeit.cms.interfaces
+
+import zeit.web.core.banner
+import zeit.web.magazin
 
 
 def test_banner_place_should_be_serialized(testserver, testbrowser):
-    place = Place(1, ['728x90'], True, label='')
+    place = zeit.web.core.banner.Place(1, ['728x90'], True, label='')
     assert place.__dict__ == {'dcopt': 'ist', 'diuqilon': True,
                               'label': '', 'min_width': 0, 'name': 'tile_1',
                               'noscript_width_height': ['728', '90'],
@@ -16,7 +18,7 @@ def test_banner_place_should_be_serialized(testserver, testbrowser):
 
 def test_banner_place_should_raise_on_index_error(testserver, testbrowser):
     with pytest.raises(IndexError):
-        Place(1, '123x456', True, label='')
+        zeit.web.core.banner.Place(1, '123x456', True, label='')
 
 
 def test_banner_list_should_be_sorted(testserver, testbrowser):
@@ -26,13 +28,13 @@ def test_banner_list_should_be_sorted(testserver, testbrowser):
 
 def test_banner_view_should_return_Place_if_tile_present(application):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/02')
-    article_view = view_article.Article(context, mock.Mock())
+    article_view = zeit.web.magazin.view_article.Article(context, mock.Mock())
     assert isinstance(article_view.banner(1), zeit.web.core.banner.Place)
 
 
 def test_banner_view_should_return_None_if_tile_is_not_present(application):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/02')
-    article_view = view_article.Article(context, mock.Mock())
+    article_view = zeit.web.magazin.view_article.Article(context, mock.Mock())
     assert article_view.banner(999) is None
 
 
@@ -200,33 +202,35 @@ def test_inject_banner_code_should_be_inserted_on_all_pages():
     total = 10
     pages = [mock.Mock() for i in xrange(total)]
 
-    with mock.patch.object(
-        zeit.frontend.article, "_place_adtag_by_paragraph") as mock_method:
+    with mock.patch.object(zeit.web.core.article,
+                           "_place_adtag_by_paragraph") as mock_method:
         mock_method.return_value = True
-        zeit.frontend.article._inject_banner_code(pages, True, False)
+        zeit.web.core.article._inject_banner_code(pages, True, False)
         assert mock_method.call_count == total
+
 
 def test_inject_banner_code_should_be_inserted_on_certain_pages():
     total = 10
     pages = [mock.Mock() for i in xrange(total)]
 
-    with mock.patch.object(
-        zeit.frontend.article, "_place_adtag_by_paragraph") as mock_method:
+    with mock.patch.object(zeit.web.core.article,
+                           "_place_adtag_by_paragraph") as mock_method:
         mock_method.return_value = True
-        zeit.frontend.article._inject_banner_code(pages, True, True)
+        zeit.web.core.article._inject_banner_code(pages, True, True)
         assert mock_method.call_count == 1
+
 
 def test_inject_banner_code_should_be_inserted_between_paragraphs(monkeypatch):
     tile_list = [0]
     possible_paragraphs = [1]
     banner_list = [mock.Mock()]
 
-    monkeypatch.setattr(zeit.frontend.banner, "banner_list", banner_list)
+    monkeypatch.setattr(zeit.web.core.banner, "banner_list", banner_list)
     page = mock.Mock()
-    p = zeit.frontend.block.Paragraph
+    p = zeit.web.core.block.Paragraph
     # we need at least three paragraphs to insert AFTER 1st (before 2nd)
     page.blocks = [p(mock.Mock()), p(mock.Mock()), p(mock.Mock())]
-    zeit.frontend.article._place_adtag_by_paragraph(
+    zeit.web.core.article._place_adtag_by_paragraph(
         page, tile_list, possible_paragraphs)
 
     assert isinstance(page.blocks[1], mock.Mock)
--- src/zeit/web/magazin/test/test_adwords.py
+++ src/zeit/web/magazin/test/test_adwords.py
@@ -1,22 +1,24 @@
 # -*- coding: utf-8 -*-
-import babel.dates
 import datetime
+
+import babel.dates
 import mock
 
 import zeit.cms.interfaces
-import zeit.frontend.view_article
-import zeit.frontend.view_centerpage
+
+import zeit.web.magazin.view_article
+import zeit.web.magazin.view_centerpage
 
 
 def test_adwords_for_article(application):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/01')
-    view = zeit.frontend.view_article.Article(context, mock.Mock())
+    view = zeit.web.magazin.view_article.Article(context, mock.Mock())
     adwords = ','.join(view.adwords)
     assert adwords == 'zeitonline,zeitmz'
 
 def test_adwords_for_lead_article(application):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/01')
-    view = zeit.frontend.view_article.Article(context, mock.Mock())
+    view = zeit.web.magazin.view_article.Article(context, mock.Mock())
     tz = babel.dates.get_timezone('Europe/Berlin')
     today = datetime.datetime.now(tz)
     view.leadtime = mock.Mock()
@@ -26,20 +28,20 @@ def test_adwords_for_lead_article(application):
 
 def test_adwords_for_longform(application):
     context = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/05')
-    view = zeit.frontend.view_article.LongformArticle(context, mock.Mock())
+    view = zeit.web.magazin.view_article.LongformArticle(context, mock.Mock())
     adwords = ','.join(view.adwords)
     assert adwords == 'zeitonline,zeitmz,longform,noiqdband'
 
 def test_adwords_for_feature_longform(application):
     context = zeit.cms.interfaces.ICMSContent(
         'http://xml.zeit.de/feature/feature_longform')
-    view = zeit.frontend.view_article.FeatureLongform(context, mock.Mock())
+    view = zeit.web.magazin.view_article.FeatureLongform(context, mock.Mock())
     adwords = ','.join(view.adwords)
     assert adwords == 'zeitonline,longform,noiqdband'
 
 def test_adwords_for_centerpage(application):
     context = zeit.cms.interfaces.ICMSContent(
         'http://xml.zeit.de/centerpage/index')
-    view = zeit.frontend.view_centerpage.Centerpage(context, mock.Mock())
+    view = zeit.web.magazin.view_centerpage.Centerpage(context, mock.Mock())
     adwords = ','.join(view.adwords)
     assert adwords == 'zeitonline,zeitmz'
