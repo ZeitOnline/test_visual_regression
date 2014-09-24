1bf26320ff2b6d4a717c8278fed95808b9c6152a
Ron Drongowski <ron.drongowski@gmail.com>
2014-09-24 13:03:30 +0200

    Fix more tests

--- src/zeit/web/site/test/test_zon_navi.py
+++ src/zeit/web/site/test/test_zon_navi.py
@@ -1,12 +1,5 @@
 # -*- coding: utf-8 -*-
-import re
-
-import mock
-import pyramid.threadlocal
-import pyramid.config
 import lxml
-
-#macro testing
 
 
 def test_nav_markup_should_match_css_selectors(jinja2_env):
@@ -14,14 +7,14 @@ def test_nav_markup_should_match_css_selectors(jinja2_env):
         'zeit.web.site:templates/inc/nav_main.html')
     html_str = tpl.render()
     html = lxml.html.fromstring(html_str).cssselect
-    assert len(html('nav.main_nav')) == 1,\
-        'just one .main_nav should be present'
+    assert len(html('nav.main_nav')) == 1, (
+        'just one .main_nav should be present')
 
-    assert len(html('nav.main_nav > div')) == 9,\
-        'nine divs within .main_nav'
+    assert len(html('nav.main_nav > div')) == 9, (
+        'nine divs within .main_nav')
 
-    assert '</div><div class="main_nav__date"' in html_str,\
-        'don\'t break line here, due to inline-block state'
+    assert '</div><div class="main_nav__date"' in html_str, (
+        'don\'t break line here, due to inline-block state')
 
     assert len(html('nav.main_nav > div.logo_bar >'
         'div.logo_bar__image')) == 1, 'just one .logo_bar__image'
@@ -29,34 +22,34 @@ def test_nav_markup_should_match_css_selectors(jinja2_env):
     assert len(html('nav.main_nav > div.logo_bar >'
         'div.logo_bar__menue')) == 1, 'just one .logo_bar__menue'
 
-    assert len(html('nav.main_nav > div.main_nav__teaser')) == 1,\
-        'just one .main_nav__teaser'
+    assert len(html('nav.main_nav > div.main_nav__teaser')) == 1, (
+        'just one .main_nav__teaser')
 
     assert len(html('nav.main_nav > div.main_nav__community'
-        '[data-dropdown="true"]')) == 1,\
-        'just one .main_nav__community w/ data-dropdown=true'
+        '[data-dropdown="true"]')) == 1, (
+        'just one .main_nav__community w/ data-dropdown=true')
 
     assert len(html('nav.main_nav > div.main_nav__ressorts'
-        '[data-dropdown="true"]')) == 1,\
-        'just one .main_nav__ressorts w/ data-dropdown=true'
+        '[data-dropdown="true"]')) == 1, (
+        'just one .main_nav__ressorts w/ data-dropdown=true')
 
     assert len(html('nav.main_nav > div.main_nav__services'
-        '[data-dropdown="true"]')) == 1,\
-        'just one .main_nav__services w/ data-dropdown=true'
+        '[data-dropdown="true"]')) == 1, (
+        'just one .main_nav__services w/ data-dropdown=true')
 
     assert len(html('nav.main_nav > div.main_nav__classifieds'
-        '[data-dropdown="true"]')) == 1,\
-        'just one .main_nav__classifieds w/ data-dropdown=true'
+        '[data-dropdown="true"]')) == 1, (
+        'just one .main_nav__classifieds w/ data-dropdown=true')
 
     assert len(html('nav.main_nav > div.main_nav__search'
-        '[data-dropdown="true"]')) == 1,\
-        'just one .main_nav__search w/ data-dropdown=true'
+        '[data-dropdown="true"]')) == 1, (
+        'just one .main_nav__search w/ data-dropdown=true')
 
-    assert len(html('nav.main_nav > div.main_nav__tags')) == 1,\
-        'just one .main_nav__tags'
+    assert len(html('nav.main_nav > div.main_nav__tags')) == 1, (
+        'just one .main_nav__tags')
 
-    assert len(html('nav.main_nav > div.main_nav__date')) == 1,\
-        'just one .main_nav__date'
+    assert len(html('nav.main_nav > div.main_nav__date')) == 1, (
+        'just one .main_nav__date')
 
 
 def test_nav_services_macro_should_have_expected_links(jinja2_env):
@@ -64,24 +57,24 @@ def test_nav_services_macro_should_have_expected_links(jinja2_env):
         'zeit.web.site:templates/inc/nav_main.html')
     html_str = tpl.module.main_nav_services()
     html = lxml.html.fromstring(html_str).cssselect
-    assert html('li > a[href="http://www.zeitabo.de/?mcwt=2009_07_0002"]')[0]\
-        is not None, 'No link for zeitabo.de'
+    assert html('li > a[href="http://www.zeitabo.de/'
+        '?mcwt=2009_07_0002"]')[0] is not None, 'No link for zeitabo.de'
     assert html('li > a[href="http://shop.zeit.de?et=l6VVNm&et_cid=42&'
         'et_lid=175&et_sub=Startseite_header"]'
-        '[id="hp.global.topnav.links.shop"]')[0] is not None,\
-        'No link for shop.zeit.de'
+        '[id="hp.global.topnav.links.shop"]')[0] is not None, (
+        'No link for shop.zeit.de')
     assert html('li > a[href="https://premium.zeit.de/?wt_mc=pm.intern.fix.'
         'zeitde.fix.dach.text.epaper"]'
-        '[id="hp.global.topnav.links.epaper"]')[0]\
-        is not None, 'No link for premium.zeit.de'
+        '[id="hp.global.topnav.links.epaper"]')[0] is not None, (
+        'No link for premium.zeit.de')
     assert html('li > a[href="https://premium.zeit.de/abo/digitalpaket5'
         '?wt_mc=pm.intern.fix.zeitde.fix.dach.text.audio"][id="hp.global.'
-        'topnav.links.audio"]')[0] is not None,\
-        'No link for premium.zeit.de AUDIO'
+        'topnav.links.audio"]')[0] is not None, (
+        'No link for premium.zeit.de AUDIO')
     assert html('li > a[href="https://premium.zeit.de/abo/appsios?'
         'wt_mc=pm.intern.fix.zeitde.fix.dach.text.apps"][id="hp.global.topnav'
         '.links.apps"]')[0] is not None, 'No link for premium.zeit.de APPS'
-    assert html('li > a[href="http://www.zeit.de/archiv"][id="hp.global.'\
+    assert html('li > a[href="http://www.zeit.de/archiv"][id="hp.global.'
         'topnav.links.archiv"]')[0] is not None, 'No link for Archiv'
 
 
@@ -91,19 +84,19 @@ def test_nav_classifieds_macro_should_have_expected_structure(jinja2_env):
     html_str = tpl.module.main_nav_classifieds()
     html = lxml.html.fromstring(html_str).cssselect
     assert html('li > a[href="http://jobs.zeit.de/"]'
-        '[id="hp.global.topnav.links.jobs"]')[0]\
-        is not None, 'No link for job.zeit.de'
+        '[id="hp.global.topnav.links.jobs"]')[0] is not None, (
+        'No link for job.zeit.de')
     assert html('li > a[href="http://www.zeit.de/angebote/partnersuche/index?'
         'pscode=01_100_20003_0001_0001_0005_empty_AF00ID_GV00ID"]'
-        '[id="hp.global.topnav.links.partnersuche"]')[0] is not None,\
-        'Link for partnersuche not present'
+        '[id="hp.global.topnav.links.partnersuche"]')[0] is not None, (
+        'Link for partnersuche not present')
     assert len(html('li.main_nav__classifieds__more')) == 1, 'No classifieds'
     assert html('li > a[href="http://zeit.immowelt.de/"]'
-        '[id="hp.global.topnav.links.immobilien"][rel="nofollow"]')[0]\
-        is not None, 'No link for zeit.immowelt.de'
+        '[id="hp.global.topnav.links.immobilien"]'
+        '[rel="nofollow"]')[0] is not None, 'No link for zeit.immowelt.de'
     assert html('li > a[href="http://automarkt.zeit.de/"]'
-        '[id="hp.global.topnav.links.automarkt"]')[0]\
-        is not None, 'No link for Automarkt'
+        '[id="hp.global.topnav.links.automarkt"]')[0] is not None, (
+        'No link for Automarkt')
 
 
 def test_nav_community_macro_should_render_a_login(jinja2_env):
@@ -127,8 +120,8 @@ def test_nav_main_nav_logo_should_create_a_logo_link(jinja2_env):
     assert html('a[href="http://www.zeit.de/index"]'
         '[title="Nachrichten auf ZEIT ONLINE"]'
         '[class="icon-zon-logo-desktop"]'
-        '[id="hp.global.topnav.centerpages.logo"]')[0] is not None,\
-        'Logo link is missing'
+        '[id="hp.global.topnav.centerpages.logo"]')[0] is not None, (
+        'Logo link is missing')
 
 
 def test_nav_main_nav_burger_should_produce_markup(jinja2_env):
@@ -142,8 +135,8 @@ def test_nav_main_nav_burger_should_produce_markup(jinja2_env):
         '.icon-zon-logo-navigation_menu')) == 1,\
         'Logo for bar menu is not present'
     assert len(html('div.logo_bar__menue__image'
-        '.main_nav__icon--hover.icon-zon-logo-navigation_menu-hover')) == 1,\
-        "A div for the burger menu is missing."
+        '.main_nav__icon--hover.icon-zon-logo-navigation_menu-hover')) == 1, (
+        "A div for the burger menu is missing.")
 
 
 def test_nav_macro_main_nav_search_should_produce_markup(jinja2_env):
@@ -156,12 +149,12 @@ def test_nav_macro_main_nav_search_should_produce_markup(jinja2_env):
         '[accept-charset="utf-8"]'
         '[method="get"]'
         '[role="search"]'
-        '[action="http://www.zeit.de/suche/index"]')[0] is not None,\
-        'Form element is not present'
-    assert html('label.hideme[for="q"]')[0] is not None,\
-        'Hide me label is not present'
-    assert html('button.search__button[type="submit"][tabindex="2"]')[0]\
-        is not None, 'No search button present'
+        '[action="http://www.zeit.de/suche/index"]')[0] is not None, (
+        'Form element is not present')
+    assert html('label.hideme[for="q"]')[0] is not None, (
+        'Hide me label is not present')
+    assert html('button.search__button[type="submit"]'
+        '[tabindex="2"]')[0] is not None, 'No search button present'
     assert html('span.icon-zon-logo-navigation_suche'
         '.search__button__image.main_nav__icon--plain')[0] is not None, (
         'No search logo present')
@@ -177,7 +170,7 @@ def test_nav_macro_main_nav_search_should_produce_markup(jinja2_env):
         'search__close__image')[0] is not None, 'No close icon present'
 
 
-def test_macro_main_nav_ressorts_produce_markup(jinja2_env):
+def test_macro_main_nav_ressorts_should_produce_markup(jinja2_env):
     tpl = jinja2_env.get_template(
         'zeit.web.site:templates/inc/nav_main.html')
     html_str = tpl.module.main_nav_ressorts()
@@ -190,28 +183,22 @@ def test_macro_main_nav_ressorts_produce_markup(jinja2_env):
                 'Links must have same length a list items.')
 
 
-def test_macro_main_nav_tags_produce_markup(jinja2_env):
+def test_macro_main_nav_tags_should_produce_markup(jinja2_env):
     tpl = jinja2_env.get_template(
-        'zeit.web.site:templates/macros/layout_macro.tpl')
-    lines = tpl.module.main_nav_tags().splitlines()
-    output = ""
-    for line in lines:
-        output += line.strip()
-    title = '<span class="main_nav__tags__label">Schwerpunkte'
-    ul_list = '<ul>'
-    assert title in output
-    assert ul_list in output
+        'zeit.web.site:templates/inc/nav_main.html')
+    html_str = tpl.module.main_nav_tags()
+    html = lxml.html.fromstring(html_str).cssselect
+    assert html('span.main_nav__tags__label')[0] is not None, (
+    'Label span is not present')
+    assert html('ul')[0] is not None, 'A list for the tags is not present.'
 
 
-def test_macro_main_nav_date_produce_markup(jinja2_env):
+def test_macro_main_nav_date_should_return_what_was_given(jinja2_env):
+    #ToDo: Maybe fill this function with more sense?
     tpl = jinja2_env.get_template(
-        'zeit.web.site:templates/macros/layout_macro.tpl')
-    lines = tpl.module.main_nav_date('Mein Datum').splitlines()
-    output = ""
-    for line in lines:
-        output += line.strip()
-    date = 'Mein Datum'
-    assert date == output
+        'zeit.web.site:templates/inc/nav_main.html')
+    html_str = tpl.module.main_nav_date('Mein Datum')
+    assert html_str == 'Mein Datum'
 
 
 #integration testing
