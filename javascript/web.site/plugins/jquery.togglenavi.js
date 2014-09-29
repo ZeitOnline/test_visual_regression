28bed288c0d92bd55d35b1ff592a573ccb97eff2
Anika Szuppa <anika.szuppa@zeit.de>
2014-09-29 15:48:45 +0200

    ZON-976: add mobile navigation toggle

---
+++ javascript/web.site/plugins/jquery.togglenavi.js
@@ -0,0 +1,56 @@
+/* global console */
+
+/**
+ * @fileOverview jQuery Plugin for toggling the mobile Navigation
+ * @author anika.szuppa@zeit.de
+ * @version  0.1
+ */
+(function( $ ) {
+    /**
+    * See (http://jquery.com/)
+    * @name jQuery
+    * @alias $
+    * @class jQuery Library
+    * See the jQuery Library  (http://jquery.com/) for full details.  This just
+    * documents the function and classes that are added to jQuery by this plug-in.
+    */
+    /**
+    * See (http://jquery.com/)
+    * @name fn
+    * @class jQuery Library
+    * See the jQuery Library  (http://jquery.com/) for full details.  This just
+    * documents the function and classes that are added to jQuery by this plug-in.
+    * @memberOf jQuery
+    */
+    /**
+    * Switches between hidden and viewable mobile navigation
+    * @class toggleNavi
+    * @memberOf jQuery.fn
+    * @return {object} jQuery-Object for chaining
+    */
+    $.fn.toggleNavi = function() {
+
+        var el = {
+            bindNaviEvents: function( that ) {
+                var $buttonMenue = $( that ).find( 'a' ),
+                    $toggleEls = $( '.main_nav' ).find( 'div[data-dropdown="true"]' ),
+                    $icons = $buttonMenue.find( 'div' ),
+                    icons = [ 'icon-zon-logo-navigation_menu', 'icon-zon-logo-navigation_close' ];
+
+                //toggle icon and visibility of dropdown elements
+                $buttonMenue.on( 'click', function( event ) {
+                    $toggleEls.toggle();
+                    $.each( icons, function( index, value ) {
+                        $( $icons[0] ).toggleClass( value );
+                        $( $icons[1] ).toggleClass( value + '-hover' );
+                    });
+                });
+            }
+        };
+
+        //run through search element and return object
+        return this.each( function() {
+            el.bindNaviEvents( this );
+        });
+    };
+})( jQuery );
