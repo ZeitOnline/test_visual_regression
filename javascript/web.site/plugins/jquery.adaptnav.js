5cdb9a762a2c0d15ab58c50f6fdfcdf261a1fdae
Arne Seemann <arne.seemann@zeit.de>
2014-10-05 20:50:06 +0200

    ZON-908: adaptive nav: remove unused code

--- javascript/web.site/plugins/jquery.adaptnav.js
+++ javascript/web.site/plugins/jquery.adaptnav.js
@@ -43,13 +43,6 @@
                 this.$items = el.$nav.children( '.primary-nav__item' ).not( '*[data-id="more-dropdown"]' );
                 this.$feature = el.$nav.find( '.primary-nav__item--featured' );
 
-                /* el.renderAdaptiveNav();
-
-                //make adaption on resize possible too
-                $( window ).on( 'resize', function() {
-                    el.renderAdaptiveNav();
-                }); */
-
                 this.$moreList = el.$nav.find( '.primary-nav__item[data-id="more-dropdown"]');
                 this.$moreListItems = el.$moreList.find( '.dropdown > .dropdown__item');
 
@@ -92,64 +85,12 @@
             isDesktop: function() {
                 return ( $( '.logo_bar__menue, .main_nav' ).is( ':hidden' ) );
             },
-            prepareHTML: function() {
-
-                el.$html = $( '<li data-feature="dropdown" class="primary-nav__item">' +
-                                '   <a class="primary-nav__link" href="#">mehr</a>' +
-                                '   <ul class="dropdown"></ul>' +
-                                '</li>' );
-
-                //reset nav if necessary
-                // el.$nav.find( 'li[data-feature="dropdown"]' ).remove(); // TODO: Arne CHECK
-
-                //set items
-                el.$items = el.$nav.children( '.primary-nav__item' ).not( '.primary-nav__item--featured' );
-
-            },
             getAvailableWidth: function() {
             //get size available in navi
 
                 var featureWidth = el.$feature.outerWidth( true );
                 return ( el.$nav.width() - featureWidth - 70 );
 
-            },
-            buildAppendList: function() {
-            // compute which items need to be hidden and add to temporary dropdown-DOM
-
-                //prepare fresh html to work with
-                el.prepareHTML();
-
-                var navWidth = 0,
-                    threshold = el.getAvailableWidth(),
-                    $appendList = el.$html.find( '.dropdown' );
-
-                //append items to new menue
-                el.$items.each(function() {
-
-                    navWidth += $( this ).outerWidth();
-                    if ( navWidth > threshold ) {
-                        $( this ).detach().appendTo( $appendList );
-                    }
-
-                });
-            },
-            appendListToDom: function() {
-            // add temporary dropdown-DOM to the DOM, check for featured item
-
-                var state = el.$feature ? el.$feature.before( el.$html ) : el.$nav.append( el.$html );
-
-                //refresh items
-                el.$items = el.$nav.children( '.primary-nav__item' );
-
-                // after computation is done, show the glory
-                el.$items.css( 'display', 'block' );
-
-            },
-            renderAdaptiveNav: function() {
-                if ( el.isDesktop() && el.$nav ) {
-                    // el.buildAppendList();
-                    // el.appendListToDom();
-                }
             }
         };
 
