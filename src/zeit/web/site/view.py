2a46c0b59345471534af5d318eed99741dcf341f
Ron Drongowski <ron.drongowski@gmail.com>
2014-10-06 10:30:26 +0200

    ZON-1016: Add custom predicate to identify content

---
+++ src/zeit/web/site/view.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import zeit.web.magazin.view
+
+
+def is_zon_content(context, request):
+    """Custom predicate to verify, if this can be rendered via zeit.frontend
+       : rtype: string
+    """
+    return getattr(context, 'rebrush_website_content', False) and (
+        not(zeit.web.magazin.view.is_zmo_content(context, request)))
