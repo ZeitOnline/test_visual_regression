297be086dd09408b4acc17a9cfffd02b8acb41b6
Nicolas Drebenstedt <cutoffthetop@me.com>
2014-10-06 15:24:14 +0200

    cleanup: Simplification by pythonication

--- src/zeit/web/core/appinfo.py
+++ src/zeit/web/core/appinfo.py
@@ -5,21 +5,18 @@ import pyramid.settings
 
 def assemble_app_info(request):
     settings = request.registry.settings
-    result = dict(debug=pyramid.settings.asbool(settings.get('debug', False)))
-    userid = pyramid.security.authenticated_userid(request)
-    if userid:
-        result['authenticated'] = True
-    else:
-        result['authenticated'] = False
-    result['user'] = request.session.get('zmo-user', dict())
-    result['scripts_url'] = request.registry.settings.scripts_url
-    result['community_host'] = request.registry.settings.community_host + '/'
-    result['community_paths'] = {'login': 'user/login',
-                                 'register': 'user/register',
-                                 'logout': 'logout',
-                                 'leserartikel': 'leserartikel',
-                                 'newsletter': 'newsletter'}
-    return dict(result)
+    return dict(
+        authenticated=bool(pyramid.security.authenticated_userid(request)),
+        community_host=request.registry.settings.community_host + '/',
+        community_paths={'login': 'user/login',
+                         'register': 'user/register',
+                         'logout': 'logout',
+                         'leserartikel': 'leserartikel',
+                         'newsletter': 'newsletter'},
+        debug=pyramid.settings.asbool(settings.get('debug', False)),
+        scripts_url=request.registry.settings.scripts_url,
+        user=request.session.get('zmo-user', {})
+    )
 
 
 app_info = cornice.service.Service(
