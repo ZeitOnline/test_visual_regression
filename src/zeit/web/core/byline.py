dd92cd74960bc599ff014175bd2b2134e51fff99
Ron Drongowski <ron.drongowski@gmail.com>
2014-10-15 18:29:56 +0200

    ZON-1032: Add byline object and tests for it

--- src/zeit/web/core/byline.py
+++ src/zeit/web/core/byline.py
@@ -1,48 +1,106 @@
 # -*- coding: utf-8 -*-
-import logging
 
 import grokcore.component
 import zope.interface
 
+import zeit.content.article.interfaces
+
+
 class IRenderByline(zope.interface.Interface):
 
-    """A string representation of information on
+    '''A string representation of information on
     author, genre, location of a ICMSContent resource
-    """
+    '''
+
 
 @grokcore.component.implementer(IRenderByline)
-@grokcore.component.adapter(zeit.cms.interfaces.ICMSContent)
+@grokcore.component.adapter(zeit.content.article.interfaces.IArticle)
 class RenderByline(object):
 
     genre = ['glosse', 'reportage', 'nachricht', 'analyse']
-    eine_n = lambda x: 'eine' if x in self.gernre else 'ein'
+    display_fe = ['glosse', 'kommentar', 'leserartikel', 'gastbeitrag',
+                  'interview']
+    eine_n = lambda self, x: 'Eine ' if x in self.genre else 'Ein '
     byline = []
 
     def __init__(self, content):
-        self.byline.append(self.eine_n(content.grenre))
-        self.byline.append(content.genre.title())
-        self.byline.append('von')
+        self._genre(content)
+        self._von(content)
+        self._interview_exception(content)
+        self._author_str(content)
 
-        if content.genre == 'interview':
-            self.byline = [].append('Interview:')
+    def __unicode__(self):
+        return ''.join(unicode(x[0]) + x[1] if isinstance(x, tuple)
+                       else str(x) for x in self.byline)
 
+    def __str__(self):
+        return unicode(self.byline).encode('utf-8')
+
+    def __html__(self):
+        return unicode(self.byline)
+
+    def _genre(self, content):
+        if content.genre and content.genre in self.display_fe:
+            self.byline.append(self.eine_n(content.genre))
+            self.byline.append(content.genre.title()+' ')
+
+    def _von(self, content):
+        if self.byline:
+            self.byline.append('von ')
+        else:
+            self.byline.append('Von ')
+
+    def _interview_exception(self, content):
+        if (content.genre and content.genre in self.display_fe and
+                content.genre == 'interview'):
+            self.byline = ['%s: ' % content.genre.title()]
+
+    def _author_str(self, content):
         authors = filter(lambda a: a is not None, content.authorships)
-        for author in authors:
-            byline.append(Author(author))
-            byline.append(',')
-            if last(author):
-                byline = byline[:-1]
-                byline[-2] = 'und'
+
+        if not authors:  # we don't have a byline, if we have no authors for it
+            self.byline = []
+            return
+
+        authors_str, authors_location = self._author_location_list(authors)
+        zipped = zip(authors_str, authors_location)
+
+        # insert punctation and remove last ', '
+        punctation = [', ' for x in authors_str]
+        zipped = list(sum(zip(zipped, punctation), ()))[:-1]
+
+        # insert conjunction
+        if len(zipped) > 1:
+            zipped[-2] = ' und '  # whitespace is relevant
+
+        # append authors list to byline
+        self.byline += zipped
+
+    def _author_location_list(self, authors):
+        authors_str = [Author(a) for a in authors]
+        authors_location = [', ' + author.location if author.location else ''
+                            for author in authors]
+
+        if len(set(authors_location)) == 1:
+            authors_location = ['' for x in range(len(authors_location)-1)] + (
+                [authors_location[0]])
+
+        assert len(authors_str) == len(authors_location)
+        return (authors_str, authors_location)
+
 
 class Author(object):
 
     def __init__(self, obj):
-        self.display_name = obj.display_name
-        self.href = self.uniqueId
+
+        self.name = obj.target.display_name
+        self.href = obj.target.uniqueId
+
+    def __unicode__(self):
+        return self.name
 
     def __str__(self):
-        return self.display_name
+        return unicode(self.name).encode('utf-8')
 
     def render(self, html):
-        return html.format((self.display_name, self.uniqueId)
-
+        return html.format((self.name, self.uniqueId))
---
+++ src/zeit/web/core/test/test_byline.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+
+import zeit.cms.interfaces
+import mock
+
+import zeit.web.core.byline
+import pytest
+
+
+def test_byline_should_be_represented_by_a_string(application):
+    article = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/artikel/08')
+    byline = zeit.web.core.byline.IRenderByline(article)
+    assert unicode(byline) == ('Ein Kommentar von Anne Mustermann, '
+                               'Berlin und Oliver Fritsch, London')
+
+
+@pytest.fixture()
+def patched_byline(monkeypatch):
+    def init(x):
+        return
+    monkeypatch.setattr(zeit.web.core.byline.RenderByline, '__init__', init)
+    return zeit.web.core.byline.RenderByline
+
+
+def test_byline_should_have_genres_if_provided(patched_byline):
+    byline = patched_byline()
+    content = mock.Mock()
+    content.genre = 'glosse'
+    content.title = lambda x: 'glosse'
+    byline._genre(content)
+    assert unicode(byline) == 'Eine Glosse '
+
+
+def test_some_byline_genres_should_not_be_displayed(patched_byline):
+    byline = patched_byline()
+    content = mock.Mock()
+    content.genre = 'nachricht'
+    byline._genre(content)
+    assert unicode(byline) == ''
+
+
+def test_byline_should_have_von_or_Von(patched_byline):
+    byline = patched_byline()
+    content = mock.Mock()
+
+    byline._von(content)
+    assert byline.byline == ['Von ']
+
+    byline.byline = ['not empty']
+    byline._von(content)
+    assert byline.byline[1] == 'von '
+
+
+def test_byline_should_have_interview_exception(patched_byline):
+    byline = patched_byline()
+    content = mock.Mock()
+    content.genre = 'interview'
+    content.title = lambda x: 'Interview'
+    byline._interview_exception(content)
+    assert unicode(byline) == 'Interview: '
+
+
+def test_byline_should_be_empty_if_no_authors_given(patched_byline):
+    byline = patched_byline()
+    content = mock.Mock()
+    content.authorships = [None]
+    byline.byline = ['full']
+    byline._author_str(content)
+    assert not byline.byline
+
+
+def test_one_author_should_be_in_byline(patched_byline):
+    byline = patched_byline()
+    content = mock.Mock()
+    author = mock.Mock()
+    author.target.display_name = 'Max Mustermann'
+    author.target.uniqueId = 'http://author'
+    author.location = 'Berlin'
+    content.authorships = [author]
+    byline._author_str(content)
+    assert unicode(byline) == 'Max Mustermann, Berlin'
+
+
+def test_two_authors_should_be_in_byline(patched_byline):
+    byline = patched_byline()
+    content = mock.Mock()
+    author = mock.Mock()
+    author.target.display_name = 'Max Mustermann'
+    author.target.uniqueId = 'http://author'
+    author.location = 'Berlin'
+    author2 = mock.Mock()
+    author2.target.display_name = 'Anne Mustermann'
+    author2.target.uniqueId = 'http://author2'
+    author2.location = 'Berlin'
+
+    content.authorships = [author, author2]
+    byline._author_str(content)
+    assert unicode(byline) == (u'Max Mustermann und Anne Mustermann, Berlin')
+
+
+def test_no_locations_should_be_in_byline_if_not_provided(patched_byline):
+    byline = patched_byline()
+    content = mock.Mock()
+    author = mock.Mock()
+    author.target.display_name = 'Max Mustermann'
+    author.target.uniqueId = 'http://author'
+    author.location = None
+    author2 = mock.Mock()
+    author2.target.display_name = 'Anne Mustermann'
+    author2.target.uniqueId = 'http://author2'
+    author2.location = None
+
+    content.authorships = [author, author2]
+    byline._author_str(content)
+    assert unicode(byline) == (u'Max Mustermann und Anne Mustermann')
+
+
+def test_three_authors_should_be_in_byline(patched_byline):
+    byline = patched_byline()
+    content = mock.Mock()
+    author = mock.Mock()
+    author.target.display_name = 'Max Mustermann'
+    author.target.uniqueId = 'http://author'
+    author.location = 'Berlin'
+    author2 = mock.Mock()
+    author2.target.display_name = 'Anne Mustermann'
+    author2.target.uniqueId = 'http://author2'
+    author2.location = 'Berlin'
+    author3 = mock.Mock()
+    author3.target.display_name = u'Ernst Ärgerlich'
+    author3.target.uniqueId = 'http://author3'
+    author3.location = 'Berlin'
+
+    content.authorships = [author, author2, author3]
+    byline._author_str(content)
+    assert unicode(byline) == (u'Max Mustermann, Anne Mustermann und '
+                               u'Ernst Ärgerlich, Berlin')
+
+
+def test_locations_none_b_b_authors_should_be_in_byline(patched_byline):
+    byline = patched_byline()
+    content = mock.Mock()
+    author = mock.Mock()
+    author.target.display_name = 'Max Mustermann'
+    author.target.uniqueId = 'http://author'
+    author.location = None
+    author2 = mock.Mock()
+    author2.target.display_name = 'Anne Mustermann'
+    author2.target.uniqueId = 'http://author2'
+    author2.location = 'Berlin'
+    author3 = mock.Mock()
+    author3.target.display_name = u'Ernst Ärgerlich'
+    author3.target.uniqueId = 'http://author3'
+    author3.location = 'Berlin'
+
+    content.authorships = [author, author2, author3]
+    authors = filter(lambda a: a is not None, content.authorships)
+    authors, locations = byline._author_location_list(authors)
+    assert locations == ['', ', Berlin', ', Berlin']
+
+
+def test_locations_a_a_a_should_produce_correct_list(patched_byline):
+    byline = patched_byline()
+    content = mock.Mock()
+    author = mock.Mock()
+    author.target.display_name = 'Max Mustermann'
+    author.target.uniqueId = 'http://author'
+    author.location = 'Hamburg'
+    author2 = mock.Mock()
+    author2.target.display_name = 'Anne Mustermann'
+    author2.target.uniqueId = 'http://author2'
+    author2.location = 'Hamburg'
+    author3 = mock.Mock()
+    author3.target.display_name = 'Ernst Ärgerlich'
+    author3.target.uniqueId = 'http://author3'
+    author3.location = 'Hamburg'
+
+    content.authorships = [author, author2, author3]
+    authors = filter(lambda a: a is not None, content.authorships)
+    authors, locations = byline._author_location_list(authors)
+    assert locations == ['', '', ', Hamburg']
+
+
+def test_locations_a_b_a_should_produce_correct_list(patched_byline):
+    byline = patched_byline()
+    content = mock.Mock()
+    author = mock.Mock()
+    author.target.display_name = 'Max Mustermann'
+    author.target.uniqueId = 'http://author'
+    author.location = 'Hamburg'
+    author2 = mock.Mock()
+    author2.target.display_name = 'Anne Mustermann'
+    author2.target.uniqueId = 'http://author2'
+    author2.location = 'Berlin'
+    author3 = mock.Mock()
+    author3.target.display_name = 'Ernst Ärgerlich'
+    author3.target.uniqueId = 'http://author3'
+    author3.location = 'Hamburg'
+
+    content.authorships = [author, author2, author3]
+    authors = filter(lambda a: a is not None, content.authorships)
+    authors, locations = byline._author_location_list(authors)
+    assert locations == [', Hamburg', ', Berlin', ', Hamburg']
+
+
+def test_locations_b_b_a_should_produce_correct_list(patched_byline):
+    byline = patched_byline()
+    content = mock.Mock()
+    author = mock.Mock()
+    author.target.display_name = 'Max Mustermann'
+    author.target.uniqueId = 'http://author'
+    author.location = 'Berlin'
+    author2 = mock.Mock()
+    author2.target.display_name = 'Anne Mustermann'
+    author2.target.uniqueId = 'http://author2'
+    author2.location = 'Berlin'
+    author3 = mock.Mock()
+    author3.target.display_name = 'Ernst Ärgerlich'
+    author3.target.uniqueId = 'http://author3'
+    author3.location = 'Hamburg'
+
+    content.authorships = [author, author2, author3]
+    authors = filter(lambda a: a is not None, content.authorships)
+    authors, locations = byline._author_location_list(authors)
+    assert locations == [', Berlin', ', Berlin', ', Hamburg']
