51237b69783fcd9531b0d83a8954b8f007e3c2fa
Dominik Hoppe <dominik.hoppe@zeit.de>
2014-10-16 17:38:22 +0200

    ZON-1039: Fix tests for pyramid json renderer

--- src/zeit/web/core/test/test_view.py
+++ src/zeit/web/core/test/test_view.py
@@ -10,7 +10,7 @@ def test_json_delta_time_from_date_should_return_delta_time(testserver,
         '&base_date=2014-10-14T09%3A06%3A45.950590%2B00%3A00'.format(
             testserver.url))
     assert browser.contents == (
-        '"{\\"delta_time\\": {\\"time\\": \\"vor 1 Tag 1 Stunde\\"}}"')
+        '{"delta_time": {"time": "vor 1 Tag 1 Stunde"}}')
 
 
 def test_json_delta_time_from_date_should_fallback_to_now_for_base_date(
@@ -36,11 +36,11 @@ def test_json_delta_time_from_unique_id_should_return_delta_time(testserver,
         'unique_id=http://xml.zeit.de/zeit-online/main-teaser-setup'.format(
             testserver.url))
     assert browser.contents == (
-        '"{\\"delta_time\\": ['
-        '{\\"http://xml.zeit.de/zeit-online/cp-content/article-01\\": '
-        '{\\"time\\": \\"vor 7 Monate 1 Tag\\"}}, '
-        '{\\"http://xml.zeit.de/zeit-online/cp-content/article-02\\": '
-        '{\\"time\\": \\"vor 7 Monate 1 Tag\\"}}]}"')
+        '{"delta_time": ['
+        '{"http://xml.zeit.de/zeit-online/cp-content/article-01": '
+        '{"time": "vor 2 Tagen 23 Stunden"}}, '
+        '{"http://xml.zeit.de/zeit-online/cp-content/article-02": '
+        '{"time": "vor 2 Tagen 23 Stunden"}}]}')
 
 
 def test_json_delta_time_from_unique_id_should_return_http_error_on_false_uid(
