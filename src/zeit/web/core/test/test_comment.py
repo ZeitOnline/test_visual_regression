5bcfc28f069402a78ce48515e8c3c54dd4f4653b
Nicolas Drebenstedt <cutoffthetop@me.com>
2014-10-17 10:50:00 +0200

    ZON-1043: Setup test function stubs

---
+++ src/zeit/web/core/test/test_comment.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+
+def test_comment_count_should_handle_failed_preconditions(
+        testbrowser, testserver):
+    pass
+
+
+def test_comment_count_should_return_expected_json_structure_for_cp_id(
+        testbrowser, testserver):
+    pass
+
+
+def test_comment_count_should_return_expected_json_structure_for_article_id(
+        testbrowser, testserver):
+    pass
+
+
+def test_comment_count_should_fallback_to_zero_if_count_unavailable(
+        testbrowser, testserver):
+    pass
