fc313f88aa49c543c8eaeced893b58492b8919d7
Nico Brünjes <nico.bruenjes@gmail.com>
2014-10-28 17:09:28 +0100

    ZON-1025: Add core centerpage view
    Add a core centerpage view and import it into the local views. Transfer globallly usable attributes to core class.

---
+++ src/zeit/web/core/view_centerpage.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import zeit.web.core.view
+
+
+class Centerpage(zeit.web.core.view.Base):
+
+    advertising_enabled = True
+
+    def __init__(self, *args, **kwargs):
+        super(Centerpage, self).__init__(*args, **kwargs)
+        self._copyrights = {}
+        self.context.advertising_enabled = self.advertising_enabled
+
+    @zeit.web.reify
+    def is_hp(self):
+        return self.request.path == '/' + self.request.registry.settings.hp
+
+    @zeit.web.reify
+    def meta_robots(self):
+        seo = zeit.seo.interfaces.ISEO(self.context)
+        try:
+            if seo.meta_robots:
+                return seo.meta_robots
+        except AttributeError:
+            pass
+        return 'index,follow,noodp,noydir,noarchive'
+
+    @zeit.web.reify
+    def tracking_type(self):
+        return type(self.context).__name__.title()
--- src/zeit/web/magazin/view_centerpage.py
+++ src/zeit/web/magazin/view_centerpage.py
@@ -23,14 +23,7 @@ import zeit.web.magazin.view
 @view_config(context=zeit.content.cp.interfaces.ICenterPage,
              custom_predicates=(zeit.web.magazin.view.is_zmo_content,),
              renderer='templates/centerpage.html')
-class Centerpage(zeit.web.core.view.Base):
-
-    advertising_enabled = True
-
-    def __init__(self, *args, **kwargs):
-        super(Centerpage, self).__init__(*args, **kwargs)
-        self._copyrights = {}
-        self.context.advertising_enabled = self.advertising_enabled
+class Centerpage(zeit.web.core.view_centerpage.Centerpage):
 
     def insert_seperator(self, position, obj):
         teaser_list = obj
@@ -46,24 +39,6 @@ class Centerpage(zeit.web.core.view.Base):
                     return teaser_list
             except:
                 continue
-
-    @zeit.web.reify
-    def is_hp(self):
-        return self.request.path == '/' + self.request.registry.settings.hp
-
-    @zeit.web.reify
-    def meta_robots(self):
-        seo = zeit.seo.interfaces.ISEO(self.context)
-        try:
-            if seo.meta_robots:
-                return seo.meta_robots
-        except AttributeError:
-            pass
-        return 'index,follow,noodp,noydir,noarchive'
-
-    @zeit.web.reify
-    def tracking_type(self):
-        return type(self.context).__name__.title()
 
     @zeit.web.reify
     @zeit.web.register_copyrights
