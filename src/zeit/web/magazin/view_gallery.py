6db62e6e30c49491ca03855b6d8fe12cb514514f
Anika Szuppa <anika.szuppa@zeit.de>
2014-11-12 15:22:59 +0100

    ZMO-842: add new suffix for galleries as well

--- src/zeit/web/magazin/view_gallery.py
+++ src/zeit/web/magazin/view_gallery.py
@@ -11,7 +11,7 @@ import zeit.web.magazin.view
 @view_config(context=zeit.web.core.gallery.IGallery,
              custom_predicates=(zeit.web.magazin.view.is_zmo_content,),
              renderer='templates/gallery.html')
-class Gallery(zeit.web.core.view.Content):
+class Gallery(zeit.web.core.view.Content, zeit.web.magazin.view.Base):
 
     advertising_enabled = True
 
