4fb4c2a1520cdcad93abedeb36d1ab44b78953c0
Anika Szuppa <anika.szuppa@zeit.de>
2014-11-25 16:00:28 +0100

    teaser-cleanup: fix mapping

--- src/zeit/web/core/test/test_template.py
+++ src/zeit/web/core/test/test_template.py
@@ -265,7 +265,7 @@ def test_zon_small_teaser_mapping_is_working_as_expected(testserver):
 
 def test_teaser_fullwidth_mapping_is_working_as_expected(testserver):
     teaser = zeit.web.core.template.get_mapped_teaser('leader-fullwidth')
-    assert teaser == 'teaser-fullwidth'
+    assert teaser == 'zon-fullwidth'
 
 
 def test_hide_teaser_mapping_is_working_as_expected(testserver):
