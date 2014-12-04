6901f073e9ffc037af2540272e43c8850e135a68
Anika Szuppa <anika.szuppa@zeit.de>
2014-12-04 13:58:37 +0100

    ZON-1261: add basic test and macros files for footer and standartite navigation

--- src/zeit/web/site/test/test_navigation.py
+++ src/zeit/web/site/test/test_navigation.py
@@ -9,7 +9,7 @@ import selenium.webdriver
 
 def test_nav_markup_should_match_css_selectors(jinja2_env):
     tpl = jinja2_env.get_template(
-        'zeit.web.site:templates/inc/nav_main.html')
+        'zeit.web.site:templates/inc/navigation.html')
     html_str = tpl.render(view=mock.MagicMock())
     html = lxml.html.fromstring(html_str).cssselect
 
@@ -60,7 +60,7 @@ def test_nav_markup_should_match_css_selectors(jinja2_env):
 
 def test_nav_services_macro_should_have_expected_links(jinja2_env):
     tpl = jinja2_env.get_template(
-        'zeit.web.site:templates/macros/navigation.tpl')
+        'zeit.web.site:templates/macros/navigation_macro.tpl')
     nav = zeit.web.core.navigation.Navigation()
     nav['abo'] = (
         zeit.web.core.navigation.NavigationItem(
@@ -97,7 +97,7 @@ def test_nav_classifieds_macro_should_have_expected_structure(jinja2_env):
             'Partnersuche',
             'http://www.zeit.de/angebote/partnersuche/index?pscode=01_100'))
     tpl = jinja2_env.get_template(
-        'zeit.web.site:templates/macros/navigation.tpl')
+        'zeit.web.site:templates/macros/navigation_macro.tpl')
     html_str = tpl.module.main_nav_classifieds(nav)
     html = lxml.html.fromstring(html_str).cssselect
 
@@ -113,7 +113,7 @@ def test_nav_classifieds_macro_should_have_expected_structure(jinja2_env):
 
 def test_nav_community_macro_should_render_a_login(jinja2_env):
     tpl = jinja2_env.get_template(
-        'zeit.web.site:templates/macros/navigation.tpl')
+        'zeit.web.site:templates/macros/navigation_macro.tpl')
     html_str = tpl.module.main_nav_community()
     html = lxml.html.fromstring(html_str).cssselect
 
@@ -127,7 +127,7 @@ def test_nav_community_macro_should_render_a_login(jinja2_env):
 
 def test_nav_main_nav_logo_should_create_a_logo_link(jinja2_env):
     tpl = jinja2_env.get_template(
-        'zeit.web.site:templates/macros/navigation.tpl')
+        'zeit.web.site:templates/macros/navigation_macro.tpl')
     html_str = tpl.module.main_nav_logo()
     html = lxml.html.fromstring(html_str).cssselect
 
@@ -140,7 +140,7 @@ def test_nav_main_nav_logo_should_create_a_logo_link(jinja2_env):
 
 def test_nav_main_nav_burger_should_produce_markup(jinja2_env):
     tpl = jinja2_env.get_template(
-        'zeit.web.site:templates/macros/navigation.tpl')
+        'zeit.web.site:templates/macros/navigation_macro.tpl')
     html_str = tpl.module.main_nav_burger()
     html = lxml.html.fromstring(html_str).cssselect
 
@@ -156,7 +156,7 @@ def test_nav_main_nav_burger_should_produce_markup(jinja2_env):
 
 def test_nav_macro_main_nav_search_should_produce_markup(jinja2_env):
     tpl = jinja2_env.get_template(
-        'zeit.web.site:templates/macros/navigation.tpl')
+        'zeit.web.site:templates/macros/navigation_macro.tpl')
     html_str = tpl.module.main_nav_search()
     html = lxml.html.fromstring(html_str).cssselect
 
@@ -186,7 +186,7 @@ def test_nav_macro_main_nav_search_should_produce_markup(jinja2_env):
 
 def test_macro_main_nav_ressorts_should_produce_markup(jinja2_env):
     tpl = jinja2_env.get_template(
-        'zeit.web.site:templates/macros/navigation.tpl')
+        'zeit.web.site:templates/macros/navigation_macro.tpl')
     nav = zeit.web.core.navigation.Navigation()
     nav['hp.global.topnav.links.jobs'] = (
         zeit.web.core.navigation.NavigationItem(
@@ -210,7 +210,7 @@ def test_macro_main_nav_ressorts_should_produce_markup(jinja2_env):
 
 def test_macro_main_nav_tags_should_produce_markup(jinja2_env):
     tpl = jinja2_env.get_template(
-        'zeit.web.site:templates/macros/navigation.tpl')
+        'zeit.web.site:templates/macros/navigation_macro.tpl')
     links = [('Label 1', 'http://link_1'),
              ('Label 2', 'http://link_2'),
              ('Label 3', 'http://link_3')]
@@ -230,7 +230,7 @@ def test_macro_main_nav_tags_should_produce_markup(jinja2_env):
 def test_macro_main_nav_date_should_return_what_was_given(jinja2_env):
     # ToDo: Maybe fill this function with more sense?
     tpl = jinja2_env.get_template(
-        'zeit.web.site:templates/macros/navigation.tpl')
+        'zeit.web.site:templates/macros/navigation_macro.tpl')
     html_str = tpl.module.main_nav_date('Mein Datum')
 
     assert html_str == 'Mein Datum'
