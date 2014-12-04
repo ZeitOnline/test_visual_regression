0f9d5989d1d6fa688cd21116243bd5134d148658
Anika Szuppa <anika.szuppa@zeit.de>
2014-12-04 14:41:56 +0100

    ZON-1261: add footer macros and testing

--- src/zeit/web/site/test/test_footer.py
+++ src/zeit/web/site/test/test_footer.py
@@ -8,28 +8,40 @@ import selenium.webdriver
 def test_footer_should_have_basic_structure(jinja2_env):
     tpl = jinja2_env.get_template(
         'zeit.web.site:templates/inc/footer.html')
+    html_str = tpl.render()
     html = lxml.html.fromstring(html_str).cssselect
 
-    assert len(html('.footer')) == 1, (
-        'just one .footer should be present')
-
-    assert len(html('.footer > .footer__logo')) == 1, (
+    assert len(html('.footer__logo')) == 1, (
         'just one .footer__logo')
 
-    assert len(html('.footer > .footer__logo__inner')) == 1, (
+    assert len(html('.footer__logo__inner')) == 1, (
         'just one .footer__logo__inner')
 
-    assert len(html('.footer > .footer__publisher')) == 1, (
+    assert len(html('.footer__publisher')) == 1, (
         'just one .footer__publisher')
 
-    assert len(html('.footer > .footer__publisher__inner')) == 1, (
+    assert len(html('.footer__publisher__inner')) == 1, (
         'just one .footer__publisher__inner')
 
-    assert len(html('.footer > .footer__links')) == 1, (
+    assert len(html('.footer__links')) == 1, (
         'just one .footer__links')
 
-    assert len(html('.footer > .footer__links__inner')) == 1, (
+    assert len(html('.footer__links__inner')) == 1, (
         'just one .footer__links__inner')
 
-    assert len(html('.footer > .footer__links__button')) == 1, (
+    assert len(html('.footer__links__button')) == 1, (
         'just one .footer__links__button')
+
+
+#integration tests
+
+def test_footer_is_displayed(selenium_driver, testserver):
+
+    driver = selenium_driver
+    driver.get('%s/zeitonline/index' % testserver.url)
+
+    footer = driver.find_element_by_class_name(
+        '.footer')
+
+    assert(footer.is_displayed()), (
+        'footer isnt displayed')
