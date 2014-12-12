35d21368d66d7ef25c9f349d1f38ac3536ae91a7
Anika Szuppa <anika.szuppa@zeit.de>
2014-12-12 10:41:43 +0100

    Preparing release 1.8.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(
     name='zeit.frontend',
     url='https://github.com/ZeitOnline/friedbert',
-    version='1.8.6.dev0',
+    version='1.8.6',
     author=(
         'Thomas Baumann, Nico Brünjes, Nicolas Drebenstedt, Ron Drongowski, '
         'Dominik Hoppe, Marco Kaiser, Harry Keller, Tom Lazar, Thomas Lotze, '
