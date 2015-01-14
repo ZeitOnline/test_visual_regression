eea902bcdfd0557754552cf6b96ccb4326614eda
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-01-14 16:50:06 +0100

    ZON-1290: Read out opt_in state and cookie presence

--- src/zeit/web/site/view_beta.py
+++ src/zeit/web/site/view_beta.py
@@ -2,6 +2,10 @@ import logging
 
 import pyramid.view
 
+import zeit.content.article.article
+
+import zeit.web.core.view
+
 
 log = logging.getLogger(__name__)
 
@@ -9,11 +13,24 @@ log = logging.getLogger(__name__)
 @pyramid.view.view_config(
     route_name='beta_toggle',
     renderer='templates/beta.html')
-class Beta(object):
+class Beta(zeit.web.core.view.Base):
 
     def __call__(self):
         return {}
 
     def __init__(self, context, request):
-        self.context = context
+        self.context = zeit.content.article.article.Article()
         self.request = request
+
+    @property
+    def beta_version(self):
+        return 'site-version' in self.request.session
+
+    @property
+    def opted_in(self):
+        site_version = self.request.session.get('site-version')
+
+        if site_version not in ('beta-opt_in', 'beta-opt_out'):
+            return
+        else:
+            return 'opt_in' in site_version
