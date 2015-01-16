ebad03b97954767c3f28cfc85571701ef6a3cc48
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-01-16 14:17:49 +0100

    ZON-1290: Round out missing beta view attributes

--- src/zeit/web/site/view_beta.py
+++ src/zeit/web/site/view_beta.py
@@ -4,6 +4,7 @@ import pyramid.view
 
 import zeit.content.article.article
 
+import zeit.web.core.security
 import zeit.web.core.view
 
 
@@ -16,6 +17,14 @@ log = logging.getLogger(__name__)
 class Beta(zeit.web.core.view.Base):
 
     def __call__(self):
+        beta = self.request.POST.get('opt')
+        if beta in ('in', 'out'):
+            beta = 'beta-opt_{}'.format(beta)
+        elif self.beta_user:
+            beta = 'beta-opt_in'
+        if beta:
+            self.request.session.update({'site_version': beta})
+            return {'site-version': beta}
         return {}
 
     def __init__(self, context, request):
@@ -23,14 +32,24 @@ class Beta(zeit.web.core.view.Base):
         self.request = request
 
     @property
-    def beta_version(self):
-        return 'site-version' in self.request.session
+    def community_user(self):
+        return zeit.web.core.security.get_community_user_info(self.request)
 
     @property
-    def opted_in(self):
-        site_version = self.request.session.get('site-version')
+    def beta_user(self):
+        return 'beta' in self.community_user.get('roles')
 
-        if site_version not in ('beta-opt_in', 'beta-opt_out'):
-            return
-        else:
-            return 'opt_in' in site_version
+    @property
+    def site_version(self):
+        version = self.request.session.get('site-version')
+
+        if version in ('beta-opt_in', 'beta-opt_out'):
+            return version.lstrip('beta-')
+
+    @property
+    def community_host(self):
+        return self.request.registry.settings.get('community_host')
+
+    @property
+    def friedbert_host(self):
+        return self.request.route_url('home')
