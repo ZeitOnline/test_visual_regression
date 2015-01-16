abfdf9c2e45ac7bbf30383d0de542405fb593e46
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-01-16 14:17:12 +0100

    ZON-1290: Use requests instead of mechanize browser

--- src/zeit/web/site/test/test_beta.py
+++ src/zeit/web/site/test/test_beta.py
@@ -1,19 +1,19 @@
 import pytest
+import requests
 
 
 @pytest.mark.parametrize('selector,cookies', [
-    ('.beta-teaser', {}),
-    ('.beta-teaser', {'site-version': 'foo'}),
+    ('.beta-prompt', {}),
+    ('.beta-prompt', {'site-version': 'foo'}),
     ('.beta-toggle', {'site-version': 'opt_in'}),
     ('.beta-toggle', {'site-version': 'opt_out'})
 ])
 def test_beta_view_should_display_info_text_if_missing_cookie(
-        selector, cookies, testbrowser, testserver):
-    browser = testbrowser('%s/beta' % testserver.url)
-    browser.cookies.update(cookies)
-    browser.reload()
-    assert len(browser.cssselect(selector)) == 1
-    browser.cookies.clear()
+        selector, cookies, css_selector, testserver):
+    resp = requests.get('{}/beta'.format(testserver.url),
+                        cookies=cookies)
+    __import__('pdb').set_trace()
+    assert css_selector(selector, resp.content) == 1
 
 
 @pytest.mark.parametrize('cookies,opt_in,opt_out', [
@@ -21,11 +21,10 @@ def test_beta_view_should_display_info_text_if_missing_cookie(
     ({'site-version': 'opt_out'}, 0, 1)
 ])
 def test_beta_view_should_correctly_display_toggled_toggle(
-        cookies, opt_in, opt_out, testbrowser, testserver):
-    browser = testbrowser('%s/beta' % testserver.url)
-    browser.cookies.update(cookies)
-    browser.reload()
-    toggle = browser.cssselect('.beta-toggle__cta')[0]
+        cookies, opt_in, opt_out, css_selector, testserver):
+    resp = requests.get('{}/beta'.format(testserver.url),
+                        cookies=cookies)
+    toggle = css_selector('.beta-toggle__cta', resp.content)[0]
     assert len(toggle.cssselect('input[value="opt_in"][checked]')) == opt_in
     assert len(toggle.cssselect('input[value="opt_out"][checked]')) == opt_out
 
@@ -35,8 +34,7 @@ def test_beta_view_should_correctly_display_toggled_toggle(
     ({'beta': 'opt_out'}, {'site-version': 'beta-opt_out'})
 ])
 def test_beta_view_should_correctly_update_cookie_on_post(
-        payload, cookies, testbrowser, testserver):
-    browser = testbrowser('%s/beta' % testserver.url)
-    browser.cookies.update(cookies)
-    browser.open('%s/beta' % testserver.url, data=payload)
-    __import__('pdb').set_trace()
+        payload, cookies, css_selector, testserver):
+    resp = requests.get('{}/beta'.format(testserver.url),
+                        data=payload, cookies=cookies)
+    assert css_selector('a', resp.content)
