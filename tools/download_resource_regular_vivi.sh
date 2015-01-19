59323922a1b966cde358cb8580080a79aefe74c4
Arne Seemann <arne.seemann@gmail.com>
2015-01-19 15:46:28 +0100

    Enhancement: make download scripts more robust

--- tools/download_resource_regular_vivi.sh
+++ tools/download_resource_regular_vivi.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # To download a resource from the CMS vivi, call this script with an path as
-# argument. This path should _not_ begin with a '/' and ist a subpath of
-# /cms/work. 
+# argument. This path should _not_ begin with a '/' and ist a subpath of /cms/work/.
 # To download resource and meta resource of the homepage call:
 # ./download_resource.sh index
 
 dav_host='http://cms-backend.zeit.de:9000';
+
+if [[ $1 == /* ]]; then
+	echo "ERROR: path should _not_ begin with a '/' ’cause it‘s a subpath of /cms/work/\n"
+	exit 128
+fi
 
 curl ${dav_host}/cms/work/$1 > resource 2> /dev/null
 
