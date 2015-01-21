e3ed07c3517f24093617ab97df30a0d0f09052d7
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-01-21 10:12:12 +0100

    ZON-1290: Use mock community fixture

--- src/zeit/web/magazin/test/test_security.py
+++ src/zeit/web/magazin/test/test_security.py
@@ -28,7 +28,18 @@ def test_session_cache_has_precedence(policy, dummy_request):
     assert dummy_request.session['zmo-user']['name'] == 's3crit'
 
 
-def test_session_cache_cleared_when_id_changes(policy, dummy_request):
+def test_session_cache_cleared_when_id_changes(
+        policy, dummy_request, mockcommunity_factory):
+    user_xml = """<?xml version="1.0" encoding="UTF-8"?>
+    <user>
+        <uid>457322</uid>
+        <name>test-user</name>
+        <roles>
+            <role>authenticated user</role>
+        </roles>
+    </user>
+    """
+    mockcommunity_factory(user_xml)
     dummy_request.cookies['drupal-userid'] = 23
     # Session still contains old user id and sensitive information
     dummy_request.session['zmo-user'] = dict(uid=42, name='s3crit')
@@ -38,7 +49,18 @@ def test_session_cache_cleared_when_id_changes(policy, dummy_request):
     assert dummy_request.session['zmo-user']['name'] == 'test-user'
 
 
-def test_empty_cache_triggers_backend_fills_cache(policy, dummy_request):
+def test_empty_cache_triggers_backend_fills_cache(
+        policy, dummy_request, mockcommunity_factory):
+    user_xml = """<?xml version="1.0" encoding="UTF-8"?>
+    <user>
+        <uid>457322</uid>
+        <name>test-user</name>
+        <roles>
+            <role>authenticated user</role>
+        </roles>
+    </user>
+    """
+    mockcommunity_factory(user_xml)
     dummy_request.cookies['drupal-userid'] = 23
     dummy_request.headers['Cookie'] = ''
     assert 'zmo-user' not in dummy_request.session
@@ -50,8 +72,7 @@ def test_unreachable_agatho_should_not_produce_error():
     mocked_request = MagicMock()
     mocked_request.registry.settings['agatho_host'] = (
         'http://thisurlshouldnotexist.moep/')
-    uniqueId = 'http://xml.zeit.de/artikel/01'
-    assert get_thread(uniqueId, mocked_request) is None
+    assert get_thread('http://xml.zeit.de/artikel/01', mocked_request) is None
 
 
 def test_unreachable_community_should_not_produce_error(dummy_request):
@@ -67,8 +88,7 @@ def test_malformed_agatho_response_should_not_produce_error(http_testserver):
     mocked_request = MagicMock()
     mocked_request.registry.settings['agatho_host'] = (
         'http://localhost:8889')
-    uniqueId = 'http://xml.zeit.de/artikel/01'
-    assert get_thread(uniqueId, mocked_request) is None
+    assert get_thread('http://xml.zeit.de/artikel/01', mocked_request) is None
 
 
 def test_malformed_community_response_should_not_produce_error(
