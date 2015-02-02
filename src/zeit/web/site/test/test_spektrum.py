7b5ad41b18df07df80f9867445a413da7892d4cc
Ron Drongowski <ron.drongowski@gmail.com>
2015-02-02 13:30:35 +0100

    Repair tests

--- src/zeit/web/site/test/test_spektrum.py
+++ src/zeit/web/site/test/test_spektrum.py
@@ -41,7 +41,7 @@ def test_spektrum_teaser_object_should_have_expected_attributes():
         u'Forscher entdecken ein China die \xc3\x9cberreste eines bisher '
         u'unbekannten, langhalsigen Dinosauriers.')
     assert teaser.feed_image == (
-        'http://www.spektrum.de/fm/912/thumbnails/85885_web.jpg.1616471.jpg')
+        'http://localhost:6551/static/images/img1.jpg')
 
 
 def test_spektrum_teaser_object_with_empty_values_should_not_break():
