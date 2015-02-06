0ef40b6d0bc2bbde75106adf82094b745a6591bb
Ron Drongowski <ron.drongowski@gmail.com>
2015-02-06 13:01:29 +0100

    Create comment section, if not present

--- src/zeit/web/core/view_admin.py
+++ src/zeit/web/core/view_admin.py
@@ -6,7 +6,8 @@ import zope.component
 import requests
 import zeit.web.core.view
 import pyramid.httpexceptions
-
+import lxml.etree
+import urlparse
 
 def _from_localhost(context, request):
     return request.client_addr == '127.0.0.1'
@@ -25,6 +26,9 @@ class PostComment(zeit.web.core.view.Base):
 
         self.context = zeit.content.article.article.Article()
         self.request = request
+        zwcs = zope.component.getUtility(zeit.web.core.interfaces.ISettings)
+        self.community_host = zwcs.get('community_host')
+        self.status = []
         self.post_comment()
 
     def __call__(self):
@@ -48,23 +52,32 @@ class PostComment(zeit.web.core.view.Base):
             if comment_thread:
                 nid = comment_thread['nid']
             else:
-                nid = self._create_and_load_comment_thread(uniqueId)
+                nid = self._create_and_load_comment_thread(uniqueId)['nid']
             if not nid:
                 raise pyramid.httpexceptions.HTTPInternalServerError(
                         title='No comment thread',
                         explanation='No comment thread for {} could be '
                                     'created.'.format(uniqueId))
 
-            zwcs = zope.component.getUtility(
-                zeit.web.core.interfaces.ISettings)
             action_url = '{}/agatho/thread/{}'.format(
-                zwcs.get('community_host'), request.params.get('path'))
+                self.community_host, request.params.get('path'))
 
-            requests.post(action_url, data={'nid': nid,
+            response = requests.post(action_url, data={'nid': nid,
                                             'uid': uid,
                                             'comment': request.params.get(
-                                                'comment'), 'pid': ''},
+                                                'comment'),
+                                            'pid': request.params.get('pid')},
                           cookies=dict(request.cookies))
+
+            if response.status_code == 200:
+                self.status.append('A comment for {} was posted'.format(
+                    uniqueId))
+            else:
+                raise pyramid.httpexceptions.HTTPInternalServerError(
+                        title='No comment could be posted',
+                        explanation='No comment  for {} could be '
+                                    'posted.'.format(uniqueId))
+
 
     def _create_and_load_comment_thread(self, uniqueId):
         content = None
@@ -72,10 +85,28 @@ class PostComment(zeit.web.core.view.Base):
             content = zeit.cms.interfaces.ICMSContent(uniqueId)
         except TypeError:
             raise pyramid.httpexceptions.HTTPInternalServerError(
-                    title='No comment thread',
-                    explanation='There resource {} does not exist and there is'
+                    title='Resource does not exist',
+                    explanation='The resource {} does not exist and there is'
                                 'no comment_thread for it.'.format(uniqueId))
 
-        #create comment_thread with requests
-        #load comment_thread
-        # return nid
+        xml_str = lxml.etree.tostring(content.xml)
+        headers= {
+            'X-uniqueId': 'http://{}{}'.format(
+                self.request.host, urlparse.urlparse(uniqueId)[2]),
+            'Content-Type': 'text/xml'}
+        response = requests.post(
+            "{}/agatho/commentsection".format(self.community_host),
+            headers=headers,
+            data=xml_str)
+
+        if not response.status_code == 204:
+             raise pyramid.httpexceptions.HTTPInternalServerError(
+                    title='Comment Section could not be created',
+                    explanation='The comment section for the resource {} '
+                                'could not be created'.format(uniqueId))
+
+        self.status.append('A comment section for {} was created'.format(
+                    uniqueId))
+
+        return zeit.web.core.comments.get_thread(
+                    unique_id=uniqueId, request=self.request)
