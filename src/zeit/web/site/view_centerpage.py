fc40f9a90e338ba84261f73236ca65aaa02c38f8
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-02-11 10:02:03 +0100

    Add missing imports

--- src/zeit/web/site/view_centerpage.py
+++ src/zeit/web/site/view_centerpage.py
@@ -14,6 +14,7 @@ import zeit.web.core.reach
 import zeit.web.core.template
 import zeit.web.core.utils
 import zeit.web.core.view
+import zeit.web.core.view_centerpage
 import zeit.web.site.view
 
 log = logging.getLogger(__name__)
