703572c3f2631f6a371a81841ecb3af45f47a91d
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-02-19 19:14:52 +0100

    ZON-1281: PEP8 :oncoming_police_car:

--- src/zeit/web/magazin/view_centerpage.py
+++ src/zeit/web/magazin/view_centerpage.py
@@ -142,7 +142,6 @@ class Centerpage(zeit.web.core.view_centerpage.Centerpage,
     def is_advertorial(self):
         return getattr(self.context, 'product_text', None) == 'Advertorial'
 
-
     @zeit.web.reify
     def is_hp(self):
         return self.context.type == 'ZMO'
