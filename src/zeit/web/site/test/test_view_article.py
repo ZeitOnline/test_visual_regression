d2964396826e8b4fde93d99b4a34c5a7bc615575
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2015-02-26 18:47:42 +0100

    ZON-1461: Add subheadlines in full view

--- src/zeit/web/site/test/test_view_article.py
+++ src/zeit/web/site/test/test_view_article.py
@@ -12,6 +12,14 @@ def test_article_should_render_full_view(testserver, testbrowser):
     assert len(browser.cssselect('p.paragraph')) == article.paragraphs
 
 
+def test_article_full_view_has_no_pagination(testbrowser, testserver):
+    select = testbrowser('{}/zeit-online/article/zeit/komplettansicht'.format(
+        testserver.url)).cssselect
+
+    assert len(select('.summary, .byline, .metadata')) == 3
+    assert len(select('.article__pagination')) == 0
+
+
 def test_article_with_pagination(testbrowser, testserver):
     browser = testbrowser('{}/zeit-online/article/zeit'.format(testserver.url))
     select = browser.cssselect
@@ -19,7 +27,7 @@ def test_article_with_pagination(testbrowser, testserver):
     numbers = select('.article__pager__number')
 
     assert len(select('.summary, .byline, .metadata')) == 3
-    assert len(select('.page-teaser')) == 0
+    assert len(select('.article__page-teaser')) == 0
     assert len(select('.article__pagination')) == 1
     assert len(nexttitle) == 1
     assert nexttitle[0].text.strip() == (
@@ -33,7 +41,7 @@ def test_article_pagination_active_state(testbrowser, testserver):
         testserver.url)).cssselect
 
     assert len(select('.summary, .byline, .metadata')) == 0
-    assert select('.page-teaser')[0].text.strip() == (
+    assert select('.article__page-teaser')[0].text.strip() == (
         u'Seite 3/5: Man wird schlank und lüstern')
     assert select('.article__pagination__nexttitle')[0].text.strip() == (
         u'Aus dem abenteuerlustigen Mädchen vom Dorf wurde ein Junkie')
