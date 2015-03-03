7d5592585237bec381c951ff90995c87e98538e4
Nico Brünjes <nico.bruenjes@gmail.com>
2015-03-03 14:32:17 +0100

    [Fix]: adapt test to new code structure

--- src/zeit/web/site/test/test_view_article.py
+++ src/zeit/web/site/test/test_view_article.py
@@ -17,18 +17,18 @@ def test_article_full_view_has_no_pagination(testbrowser, testserver):
         testserver.url)).cssselect
 
     assert len(select('.summary, .byline, .metadata')) == 3
-    assert len(select('.article__pagination')) == 0
+    assert len(select('.pagination')) == 0
 
 
 def test_article_with_pagination(testbrowser, testserver):
     browser = testbrowser('{}/zeit-online/article/zeit'.format(testserver.url))
     select = browser.cssselect
-    nexttitle = select('.article__pagination__nexttitle')
-    numbers = select('.article__pager__number')
+    nexttitle = select('.pagination__nexttitle')
+    numbers = select('.pager__number')
 
     assert len(select('.summary, .byline, .metadata')) == 3
     assert len(select('.article__page-teaser')) == 0
-    assert len(select('.article__pagination')) == 1
+    assert len(select('.pagination')) == 1
     assert len(nexttitle) == 1
     assert nexttitle[0].text.strip() == (
         u'Der Horror von Crystal wurzelt in der Normalität')
@@ -43,6 +43,6 @@ def test_article_pagination_active_state(testbrowser, testserver):
     assert len(select('.summary, .byline, .metadata')) == 0
     assert select('.article__page-teaser')[0].text.strip() == (
         u'Seite 3/5: Man wird schlank und lüstern')
-    assert select('.article__pagination__nexttitle')[0].text.strip() == (
+    assert select('.pagination__nexttitle')[0].text.strip() == (
         u'Aus dem abenteuerlustigen Mädchen vom Dorf wurde ein Junkie')
-    assert '--current' in (select('.article__pager__number')[2].get('class'))
+    assert '--current' in (select('.pager__number')[2].get('class'))
