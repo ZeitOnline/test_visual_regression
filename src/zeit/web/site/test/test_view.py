8ab5f0208bd133536f22c4caa8e14fb4b3efa977
Dominik Hoppe <dominik.hoppe@zeit.de>
2015-03-12 16:14:05 +0100

    ZON-1440: Update test for breaking news

--- src/zeit/web/site/test/test_view.py
+++ src/zeit/web/site/test/test_view.py
@@ -16,4 +16,4 @@ def test_missing_breaking_news_should_eval_to_false(
         application, app_settings, ephemeral_settings):
     app_settings['breaking_news'] = 'moep'
     ephemeral_settings(app_settings)
-    zeit.web.site.view.check_breaking_news()
+    assert zeit.web.site.view.check_breaking_news() is False
