f89c1d2a03e665fdc1d6f20a12c5b138b028139e
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-03-16 11:54:13 +0100

    ZON-1437: Add default attr dict class

--- src/zeit/web/core/utils.py
+++ src/zeit/web/core/utils.py
@@ -6,31 +6,6 @@ __all__ = ['defaultdict', 'nslist', 'nstuple', 'nsdict', 'nsset', 'nsstr',
            'nsunicode']
 
 
-class defaultdict(collections.defaultdict):
-    """Extension of stdlib's defaultdict that overwrites its `get` method and
-    behaviour of the `in` operator.
-    """
-
-    def get(self, key, *args):
-        """If the key cannot be found, return the (optional) function scope
-        default or the defaultdict instances default.
-
-        :param key: Key of the dictionary item to be retrieved.
-        :param default: Opional function scope default value.
-        """
-        length = len(args)
-        if length > 1:
-            raise TypeError('defaultdict expected at most 2 arguments, got %s'
-                            % length)
-        elif length == 1:
-            return super(defaultdict, self).get(key) or args[0]
-        return self.__getitem__(key)
-
-    def __contains__(self, name):
-        """Instances of defaultdict will pretend to contain any key."""
-        return True
-
-
 def fix_misrepresented_latin(val):
     """Fix misrepresentation of latin-1 chars in unicode strings."""
 
@@ -138,3 +113,35 @@ class attrdict(dict):
 
     def __getattr__(self, key):
         return key in self and self[key] or self.__getattribute__(key)
+
+
+class defaultdict(collections.defaultdict):
+    """Extension of stdlib's defaultdict that overwrites its `get` method and
+    behaviour of the `in` operator.
+    """
+
+    def get(self, key, *args):
+        """If the key cannot be found, return the (optional) function scope
+        default or the defaultdict instances default.
+
+        :param key: Key of the dictionary item to be retrieved.
+        :param default: Opional function scope default value.
+        """
+        length = len(args)
+        if length > 1:
+            raise TypeError(
+                'get() expected at most 2 arguments, got {}'.format(length))
+        elif length == 1:
+            return super(defaultdict, self).get(key) or args[0]
+        return self.__getitem__(key)
+
+    def __contains__(self, name):
+        """Instances of defaultdict will pretend to contain any key."""
+        return True
+
+
+class defaultattrdict(attrdict, defaultdict):
+    """Combines the best of both the default- and the attrdict."""
+
+    def __getattr__(self, key):
+        return self[key]
