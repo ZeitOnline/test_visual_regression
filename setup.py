bb7c095187f49f13186904450b9dc0ed13573c39
Ron Drongowski <ron.drongowski@gmail.com>
2015-03-17 15:20:40 +0100

    Preparing release 1.17

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(
     name='zeit.web',
     url='https://github.com/ZeitOnline/zeit.web',
-    version='1.16.3.dev0',
+    version='1.17',
     author=(
         'Thomas Baumann, Nico Brünjes, Nicolas Drebenstedt, Ron Drongowski, '
         'Dominik Hoppe, Marco Kaiser, Harry Keller, Tom Lazar, Thomas Lotze, '
