10230cf653339697bd8d4fd82da6dd0adc600a78
Dominik Hoppe <dominik.hoppe@zeit.de>
2015-03-19 10:27:56 +0100

    ZON-1440: Move check for published breaking news to model

--- src/zeit/web/core/test/test_view.py
+++ src/zeit/web/core/test/test_view.py
@@ -134,18 +134,27 @@ def test_inline_gallery_should_have_images(testserver, testbrowser):
     assert image.copyright[0][0] == u'\xa9'
 
 
+def test_breaking_news_should_be_provided(application):
+    view = zeit.web.core.view.Base(None, None)
+    assert zeit.web.core.interfaces.IBreakingNews.providedBy(
+        view.breaking_news)
+
+
 def test_unpublished_breaking_news_should_be_detected(application):
-    assert zeit.web.core.view.check_breaking_news() is False
+    view = zeit.web.core.view.Base(None, None)
+    assert view.breaking_news.published is False
 
 
 def test_published_breaking_news_should_be_detected(application, monkeypatch):
     monkeypatch.setattr(
         zeit.workflow.workflow.ContentWorkflow, 'published', True)
-    assert zeit.web.core.view.check_breaking_news() is True
+    view = zeit.web.core.view.Base(None, None)
+    assert view.breaking_news.published is True
 
 
 def test_missing_breaking_news_should_eval_to_false(
         application, app_settings, ephemeral_settings):
     app_settings['breaking_news'] = 'moep'
     ephemeral_settings(app_settings)
-    assert zeit.web.core.view.check_breaking_news() is False
+    view = zeit.web.core.view.Base(None, None)
+    assert view.breaking_news.published is False
