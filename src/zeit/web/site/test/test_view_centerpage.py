75642ade4470b55ad0e59090be3b75702df677fc
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2015-03-24 11:57:51 +0100

    ZON-1395: Adapt test

--- src/zeit/web/site/test/test_view_centerpage.py
+++ src/zeit/web/site/test/test_view_centerpage.py
@@ -148,7 +148,7 @@ def test_default_teaser_should_match_css_selectors(application, jinja2_env):
         'No comment text present')
 
 
-def test_first_small_teaser_has_image_on_mobile_mode(
+def test_first_small_teaser_has_no_image_on_mobile_mode(
         selenium_driver, testserver):
 
     driver = selenium_driver
@@ -158,7 +158,7 @@ def test_first_small_teaser_has_image_on_mobile_mode(
     first = box.find_elements_by_class_name('teaser-small__media')[0]
     second = box.find_elements_by_class_name('teaser-small__media')[1]
 
-    assert first.is_displayed(), 'image is not displayed'
+    assert first.is_displayed() is False, 'image is displayed'
     assert second.is_displayed() is False, 'image is displayed'
 
 
