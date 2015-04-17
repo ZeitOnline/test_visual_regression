746238b2ff3423961fe0de5b153b0be156c05d1a
Ron Drongowski <ron.drongowski@gmail.com>
2015-04-17 12:51:24 +0200

    pep8

--- src/zeit/web/magazin/test/test_article.py
+++ src/zeit/web/magazin/test/test_article.py
@@ -579,6 +579,7 @@ def test_article03_has_linked_image(testserver, testbrowser):
         output += line.strip()
     assert '<a href="http://www.test.de"><img alt="Immer' in output
 
+
 @pytest.mark.skipif(True,
                     reason="We need a way to mock liveblog in tests")
 def test_article02_uses_esi(selenium_driver, testserver):
@@ -587,6 +588,7 @@ def test_article02_uses_esi(selenium_driver, testserver):
     blog = WebDriverWait(driver, 15).until(
         EC.presence_of_element_located((By.ID, "livedesk-root")))
     assert blog.is_displayed(), 'ESI Liveblog not displayed'
+
 
 def test_article02_should_have_esi_include(testbrowser, testserver):
     browser = testbrowser('%s/artikel/02' % testserver.url)
