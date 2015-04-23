922679bffa0e1fd1ed606ce4b7797a6edf126c75
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-04-23 11:31:41 +0200

    ZON-1667: Move block test file to core package

--- src/zeit/web/core/test/test_block.py
+++ src/zeit/web/core/test/test_block.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
-from zeit.web.core.block import _inline_html
-from zeit.web.core.block import HeaderImage
-from zeit.web.core.block import HeaderVideo
-from zeit.web.core.block import Image
-from zeit.web.core.block import Video
 import lxml.etree
 import mock
+
+import zeit.web.core.block
 
 
 def test_inline_html_should_filter_to_valid_html():
@@ -18,23 +15,24 @@ def test_inline_html_should_filter_to_valid_html():
                'data-foo="bar"> ba </a> und <em>Text</em> abc invalid valid: '
                '<em>valid</em>')
 
-    assert xml_str == str(_inline_html(xml)).replace('\n', '')
+    assert xml_str == (
+        str(zeit.web.core.block._inline_html(xml)).replace('\n', ''))
 
 
 def test_inline_html_should_return_none_on_non_xml_input():
-    assert _inline_html('foo') is None
-    assert _inline_html(None) is None
+    assert zeit.web.core.block._inline_html('foo') is None
+    assert zeit.web.core.block._inline_html(None) is None
 
 
-def test_video_block_should_be_fault_tolerant_if_video_is_None():
+def test_video_block_should_be_fault_tolerant_if_video_is_none():
     model_block = mock.Mock()
     model_block.video = None
-    video = Video(model_block)
+    video = zeit.web.core.block.Video(model_block)
     assert not hasattr(video, 'video_still')
 
     model_block = mock.Mock()
     model_block.video.uniqueId = 'foo'
-    video = Video(model_block)
+    video = zeit.web.core.block.Video(model_block)
     assert hasattr(video, 'video_still')
 
 
@@ -42,8 +40,8 @@ def test_header_video_should_be_created_if_layout_is_zmo_header():
     model_block = mock.Mock()
     model_block.layout = 'zmo-xl-header'
     model_block.video.uniqueId = 'foo'
-    h_video = HeaderVideo(model_block)
-    assert type(h_video) == HeaderVideo
+    h_video = zeit.web.core.block.HeaderVideo(model_block)
+    assert type(h_video) == zeit.web.core.block.HeaderVideo
     assert h_video.format == 'zmo-xl-header'
 
 
@@ -52,7 +50,7 @@ def test_header_video_should_not_be_created_if_layout_does_not_fit():
     model_block.layout = 'zmo-xl-noheader'
     model_block.video.uniqueId = 'foo'
 
-    h_video = HeaderVideo(model_block)
+    h_video = zeit.web.core.block.HeaderVideo(model_block)
     assert h_video is None
 
 
@@ -60,8 +58,8 @@ def test_header_image_should_be_created_if_layout_is_zmo_header():
     model_block = mock.Mock()
     model_block.layout = 'zmo-xl-header'
     model_block.is_empty = False
-    h_image = HeaderImage(model_block)
-    assert type(h_image) == HeaderImage
+    h_image = zeit.web.core.block.HeaderImage(model_block)
+    assert type(h_image) == zeit.web.core.block.HeaderImage
 
 
 def test_header_image_should_not_be_created_if_layout_does_not_fit():
@@ -69,21 +67,21 @@ def test_header_image_should_not_be_created_if_layout_does_not_fit():
     model_block.layout = 'zmo-xl-noheader'
     model_block.is_empty = False
 
-    h_image = HeaderImage(model_block)
+    h_image = zeit.web.core.block.HeaderImage(model_block)
     assert h_image is None
 
 
-def test_image_should_be_None_if_is_empty_is_True():
+def test_image_should_be_none_if_is_empty_is_true():
     model_block = mock.Mock()
     model_block.layout = 'zmo-xl-noheader'
     model_block.is_empty = True
-    image = Image(model_block)
+    image = zeit.web.core.block.Image(model_block)
     assert image is None
 
 
-def test_image_should_be_Fail_if_is_empty_doesnot_exist():
+def test_image_should_be_fail_if_is_empty_doesnot_exist():
     model_block = mock.Mock(spec=('layout',))
     model_block.layout = 'zmo-xl-header'
     model_block.is_empty = None
-    image = Image(model_block)
+    image = zeit.web.core.block.Image(model_block)
     assert image is None
