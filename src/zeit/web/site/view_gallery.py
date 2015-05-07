39c3a3ae484fe52e2a3d5fd5445fddafca4b04a0
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2015-05-07 14:49:40 +0200

    ZON-1674: Update view config for galleries

--- src/zeit/web/site/view_gallery.py
+++ src/zeit/web/site/view_gallery.py
@@ -11,10 +11,7 @@ import zeit.web.site.view
 class Gallery(zeit.web.core.view_gallery.Gallery, zeit.web.site.view.Base):
     advertising_enabled = True
 
-@view_defaults(context=zeit.web.core.gallery.IGallery,
-               custom_predicates=(zeit.web.site.view.is_zon_content,),
-               renderer='templates/inc/comments/comment-form.html')
-@view_config(request_param='form=comment')
-@view_config(request_param='form=report')
-class CommentForm(zeit.web.core.view_gallery.Gallery):
+@view_config(request_param='form',
+             renderer='templates/inc/comments/comment-form.html')
+class CommentForm(Gallery):
     pass
