bc315fbb40f073fe2491bb3d1fa8c1956140bfdc
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2015-05-07 14:36:28 +0200

    ZON-1674: Add view config for galleries

--- src/zeit/web/site/view_gallery.py
+++ src/zeit/web/site/view_gallery.py
@@ -1,11 +1,20 @@
+from pyramid.view import view_defaults
 from pyramid.view import view_config
 
 import zeit.web.core.view_gallery
 import zeit.web.site.view
 
 
-@view_config(context=zeit.web.core.gallery.IGallery,
-             custom_predicates=(zeit.web.site.view.is_zon_content,),
-             renderer='templates/gallery.html')
+@view_defaults(context=zeit.web.core.gallery.IGallery,
+               custom_predicates=(zeit.web.site.view.is_zon_content,))
+@view_config(renderer='templates/gallery.html')
 class Gallery(zeit.web.core.view_gallery.Gallery, zeit.web.site.view.Base):
     advertising_enabled = True
+
+@view_defaults(context=zeit.web.core.gallery.IGallery,
+               custom_predicates=(zeit.web.site.view.is_zon_content,),
+               renderer='templates/inc/comments/comment-form.html')
+@view_config(request_param='form=comment')
+@view_config(request_param='form=report')
+class CommentForm(zeit.web.core.view_gallery.Gallery):
+    pass
