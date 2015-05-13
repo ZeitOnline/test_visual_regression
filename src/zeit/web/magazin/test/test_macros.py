ZeitOnline/test_visual_regression

commit 5262bc587e77c87bd4bda745687cd5232c03e426

BUG-226: Fix ZMO image captions should not escape xmlcharrefs

src/zeit/web/magazin/templates/macros/article_macro.tpl
@@ -216,7 +216,7 @@
                 {% if loop -%}
                 <span class="figure__index">{{ loop.index }}/{{ loop.length }}</span>
                 {% endif -%}
-                <span class="figure__text">{{ obj.caption|hide_none }}</span>
+                <span class="figure__text">{{ obj.caption | safe | hide_none }}</span>
                 {% if obj.copyright|count and obj.copyright[0][0] != '©' %}
                 <span class="figure__copyright">
                     {%- if obj.copyright[0][1] -%}
@@ -236,7 +236,7 @@
 {% macro headerimage(obj) -%}
     <div class="scaled-image is-pixelperfect article__head-image">
         {{ lama.insert_responsive_image(obj) }}
-    </div>{{ obj.caption }}{{ obj.copyright }}
+    </div>{{ obj.caption | safe | hide_none }}{{ obj.copyright }}
 {%- endmacro %}
 
 {% macro video(obj) -%}

src/zeit/web/magazin/test/test_macros.py
@@ -334,6 +334,12 @@ def test_macro_headerimage_should_produce_markup(jinja2_env):
     assert output.endswith(end)
 
 
+def test_image_macro_should_not_autoescape_markup(testserver, testbrowser):
+    browser = testbrowser('%s/feature/feature_longform' % testserver.url)
+    text = browser.cssselect('.figure-stamp--right .figure__text')[0]
+    assert u'Heckler & Koch' in text.text
+
+
 def test_macro_meta_author_should_produce_html_if_author_exists(
         application, jinja2_env):
     tpl = jinja2_env.get_template(

src/zeit/web/core/data/feature/feature_longform
@@ -155,9 +155,9 @@
         <supertitle>Friedhof Hamburg-Ohlsdorf</supertitle>
         <division type="page" teaser="Kein zeitlos sicheres Geschäft mehr">
         <image src="http://xml.zeit.de/exampleimages/artikel/05/02.jpg" type="jpg" publication-date="" expires="" layout="zmo-small-right" is_empty="False" alt="My 05er alt" title="my 05er title">
-        <bu>Ein Mann in Leipzig XXXXXXXX vor dem "Bau Büro"</bu>
-                <copyright link="http://foo.de">© Johannes Eisele/AFP/Getty Images</copyright>
-            </image>
+            <bu>In Mexiko machte unser Reporterteam einen aufsehenerregenden Fund: In Petaquillas präsentierte Salvador Alanís, der Chef der Bürgerwehr, ein G36-Sturmgewehr der deutschen Marke Heckler &amp; Koch. Mit der Waffe hatte der Drogenboss "El Talibán" Angst und Schrecken im Ort verbreitet. Wie war sie dorthin gekommen? Die ZEIT berichtete schon 2013 über illegale Exporte von Heckler und Koch nach Mexiko. Entgegen einer Auflage der Bundesregierung waren rund 2000 Gewehre an Polizisten im Bundesstaat Guerrero gelangt. Die Staatsanwaltschaft Stuttgart ermittelt.</bu>
+            <copyright link="http://foo.de">© Johannes Eisele/AFP/Getty Images</copyright>
+        </image>
             <p>Man könnte sie skurril erzählen, anhand der Kupferdiebe, die hier Statuen stehlen, anhand der
       Hamburger Vermessungstechniker, die auf den schnurgeraden Alleen ihre Infrarotgeräte
       justieren, anhand des Biologen, der seit einem Jahr die Bäume zählt und noch nicht weit

commit d22a94e7e171a45e8f934ba701b4dd2856b1936c

BUG-251: Don't display "None" in image captions, _first_ hide none, then maybe do escaping
This bug was introduced by commit:5262bc5 (for BUG-226).

src/zeit/web/magazin/templates/macros/article_macro.tpl
@@ -216,7 +216,7 @@
                 {% if loop -%}
                 <span class="figure__index">{{ loop.index }}/{{ loop.length }}</span>
                 {% endif -%}
-                <span class="figure__text">{{ obj.caption | safe | hide_none }}</span>
+                <span class="figure__text">{{ obj.caption | hide_none | safe }}</span>
                 {% if obj.copyright|count and obj.copyright[0][0] != '©' %}
                 <span class="figure__copyright">
                     {%- if obj.copyright[0][1] -%}
@@ -236,7 +236,7 @@
 {% macro headerimage(obj) -%}
     <div class="scaled-image is-pixelperfect article__head-image">
         {{ lama.insert_responsive_image(obj) }}
-    </div>{{ obj.caption | safe | hide_none }}{{ obj.copyright }}
+    </div>{{ obj.caption | hide_none | safe }}{{ obj.copyright }}
 {%- endmacro %}
 
 {% macro video(obj) -%}

src/zeit/web/magazin/test/test_macros.py
@@ -6,6 +6,9 @@ import pyramid.threadlocal
 import pyramid.config
 import lxml
 
+from zeit.cms.checkout.helper import checked_out
+import zeit.cms
+
 
 def test_macro_p_should_produce_markup(jinja2_env):
     tpl = jinja2_env.get_template(
@@ -338,6 +341,15 @@ def test_image_macro_should_not_autoescape_markup(testserver, testbrowser):
     browser = testbrowser('%s/feature/feature_longform' % testserver.url)
     text = browser.cssselect('.figure-stamp--right .figure__text')[0]
     assert u'Heckler & Koch' in text.text
+
+
+def test_image_macro_should_hide_none(testserver, testbrowser):
+    # XXX I'd much rather change a caption in the article, but trying
+    # to checkout raises ConstrainedNotSatisfiedError: xl-header. :-(
+    with mock.patch('zeit.web.core.block._inline_html') as inline:
+        inline.return_value = None
+        browser = testbrowser('%s/feature/feature_longform' % testserver.url)
+        assert '<span class="figure__text">None</span>' not in browser.contents
 
 
 def test_macro_meta_author_should_produce_html_if_author_exists(