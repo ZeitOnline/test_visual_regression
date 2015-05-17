ccfceee89adde628fb81ef69e51c62ecfadb4cf6
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2015-05-17 22:46:18 +0200

    Add tests

--- src/zeit/web/core/test/test_date.py
+++ src/zeit/web/core/test/test_date.py
@@ -10,7 +10,7 @@ delta_time = zeit.web.core.date.DeltaTime(
     zeit.web.core.date.parse_date(
         '2014-10-07T14:42:00.1+00:00'),
     zeit.web.core.date.parse_date(
-        '2014-10-09T19:22:00.1+00:00')
+        '2014-10-09T19:22:42.1+00:00')
 )
 
 
@@ -42,7 +42,7 @@ def test_parse_date_should_return_none_without_date():
 
 def test_delta_time_should_store_delta_time():
     assert type(delta_time.delta) == datetime.timedelta
-    assert delta_time.delta == datetime.timedelta(2, 16800)
+    assert delta_time.delta == datetime.timedelta(2, 16842)
 
 
 def test_delta_days_entity_should_raise_type_error_on_invalid_param():
@@ -68,6 +68,12 @@ def test_delta_minutes_entity_should_be_created():
     assert bme.text == '40 Minuten'
 
 
+def test_delta_seconds_entity_should_be_created():
+    bme = zeit.web.core.date.DeltaSecondsEntity(delta_time.delta)
+    assert bme.number == 42
+    assert bme.text == '42 Sekunden'
+
+
 def test_get_babelfied_delta_time_should_define_date_entities():
     delta_time._get_babelfied_delta_time()
     assert delta_time.hours.number == 4
@@ -79,11 +85,12 @@ def test_filter_delta_time_should_modify_date_on_exceeding_hours_limit():
         zeit.web.core.date.parse_date(
             '2014-10-09T10:42:00.1+00:00'),
         zeit.web.core.date.parse_date(
-            '2014-10-09T12:22:00.1+00:00')
+            '2014-10-09T12:22:12.1+00:00')
     )
     limited_date._get_babelfied_delta_time()
     limited_date._filter_delta_time()
     assert limited_date.minutes is None
+    assert limited_date.seconds is None
     assert limited_date.hours.number == 1
     assert limited_date.days.number == 0
 
@@ -97,6 +104,7 @@ def test_filter_delta_time_should_modify_date_on_exceeding_days_limit():
     )
     limited_date._get_babelfied_delta_time()
     limited_date._filter_delta_time()
+    assert limited_date.seconds is None
     assert limited_date.minutes is None
     assert limited_date.hours is None
     assert limited_date.days is None
@@ -105,7 +113,7 @@ def test_filter_delta_time_should_modify_date_on_exceeding_days_limit():
 def test_stringify_delta_time_should_return_string_representation_of_delta():
     delta_time._get_babelfied_delta_time()
     stringified_dt = delta_time._stringify_delta_time()
-    assert stringified_dt == 'vor 2 Tagen 4 Stunden 40 Minuten'
+    assert stringified_dt == 'vor 2 Tagen 4 Stunden 40 Minuten 42 Sekunden'
 
 
 def test_stringify_delta_time_should_ignore_values_of_zero():
@@ -125,7 +133,7 @@ def test_get_time_since_modification_should_return_none_for_older_dates():
     assert stringified_dt is None
 
 
-def test_get_time_since_modification_should_return_delta_time_string():
+def test_get_time_since_modification_should_return_filtered_time_string():
     limited_date = zeit.web.core.date.DeltaTime(
         zeit.web.core.date.parse_date(
             '2014-10-09T14:42:00.1+00:00'),
@@ -136,6 +144,11 @@ def test_get_time_since_modification_should_return_delta_time_string():
     assert stringified_dt == 'vor 2 Stunden'
 
 
+def test_get_time_since_comment_posting_should_return_string_representation():
+    stringified_dt = delta_time.get_time_since_comment_posting()
+    assert stringified_dt == 'vor 2 Tagen'
+
+
 def test_delta_time_should_default_to_now_for_base_date():
     dt = zeit.web.core.date.DeltaTime(
         zeit.web.core.date.parse_date(
@@ -144,3 +157,16 @@ def test_delta_time_should_default_to_now_for_base_date():
     dt_until_now = dt._stringify_delta_time()
     assert dt_until_now is not None
     assert dt_until_now != ''
+
+
+def test_filter_delta_time_should_strip_seconds_after_one_minute():
+    interval = zeit.web.core.date.DeltaTime(
+        datetime.datetime.fromtimestamp(0),
+        datetime.datetime.fromtimestamp(90)
+    )
+    interval._get_babelfied_delta_time()
+    assert interval.delta.seconds == 90
+    assert interval.seconds.number == 30
+    interval._filter_delta_time()
+    assert interval.seconds is None
+    assert interval._stringify_delta_time() == 'vor 1 Minute'
