65efe35bf4d25c896b592c75aa310aaa4766ed0a
Ron Drongowski <ron.drongowski@gmail.com>
2015-05-19 15:30:51 +0200

    readd destination kwarg

--- src/zeit/web/core/view.py
+++ src/zeit/web/core/view.py
@@ -438,7 +438,11 @@ class Content(Base):
         page = self.request.params.get('page', 1)
         cid = self.request.params.get('cid', None)
         return zeit.web.core.comments.get_thread(
-            self.context.uniqueId, sort=sort, page=page, cid=cid)
+            self.context.uniqueId,
+            destination=self.request.url,
+            sort=sort,
+            page=page,
+            cid=cid)
 
     @zeit.web.reify
     def obfuscated_date(self):
