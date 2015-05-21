acdaaad70459daa5815f9e1e5105c513f85efa1d
Dominik Hoppe <dominik.hoppe@zeit.de>
2015-05-21 17:15:40 +0200

    ZON-1692: Change testimage proportions

--- src/zeit/web/core/template.py
+++ src/zeit/web/core/template.py
@@ -264,7 +264,7 @@ scales = {
     'zon-article-large': (820, 462),
     'zon-printbox': (320, 234),
     'zon-printbox-wide': (320, 148),
-    'zon-topic': (908, 365),
+    'zon-topic': (980, 418),
     'brightcove-still': (580, 326),
     'brightcove-thumbnail': (120, 67),
     'spektrum': (220, 124)
