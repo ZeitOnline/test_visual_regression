910d4702fc4ef56a1c9009e69d653f1876c2b4d7
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2015-05-21 15:42:24 +0200

    This changes everything

--- src/zeit/web/magazin/test/test_macros.py
+++ src/zeit/web/magazin/test/test_macros.py
@@ -807,7 +807,7 @@ def test_macro_include_cp_ad_produces_markup(jinja2_env):
     assert '<div class="cp_button--ad">' in output
 
 
-def test_macro_liveblog_produces_html(jinja2_env, application):
+def test_macro_liveblog_produces_html(application, jinja2_env):
     tpl = jinja2_env.get_template(
         'zeit.web.magazin:templates/macros/article_macro.tpl')
     liveblog = mock.Mock()
