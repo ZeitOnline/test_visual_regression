2f6f1dc4ac468ec8b3055ddc3db332662bf70cde
Nico Brünjes <nico.bruenjes@gmail.com>
2015-06-12 16:39:34 +0200

    ZON-1830: test new features

--- src/zeit/web/site/test/test_view.py
+++ src/zeit/web/site/test/test_view.py
@@ -11,13 +11,16 @@ def test_login_state_view_should_deliver_correct_destination():
     request.host = "destination"
     request.params = {}
     result = zeit.web.site.view.login_state(request)
-    assert result == {'login': (
-        'http://community/user/login?destination=http://destination')}
-
+    assert result == {'profile': 'http://community/user/', 'login': (
+        'http://community/user/login?destination=http://destination'),
+        'logout': (
+            'http://community/user/logout?destination=http://destination')}
     request.params = {'context-uri': 'http://context-uri'}
     result = zeit.web.site.view.login_state(request)
-    assert result == {'login': (
-        'http://community/user/login?destination=http://context-uri')}
+    assert result == {'profile': 'http://community/user/', 'login': (
+        'http://community/user/login?destination=http://context-uri'),
+        'logout': (
+            'http://community/user/logout?destination=http://context-uri')}
 
 
 def test_login_state_view_should_deliver_correct_user():
@@ -36,3 +39,8 @@ def test_login_state_view_should_deliver_correct_user():
     request.session['user']['picture'] = 'http://community/pic'
     result = zeit.web.site.view.login_state(request)
     assert result['user']['picture'] == 'http://community_static/pic'
+
+    # community bug
+    request.session['user']['picture'] = '0'
+    result = zeit.web.site.view.login_state(request)
+    assert 'picture' not in result['user']
