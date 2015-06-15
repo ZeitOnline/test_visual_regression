960d6025df28fad0e5715f6c9b08928c6ec3bb45
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-06-15 15:16:04 +0200

    Back to development: 1.30.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 setup(
     name='zeit.web',
     url='https://github.com/ZeitOnline/zeit.web',
-    version='1.30.1',
+    version='1.30.2.dev0',
     author=(
         'Thomas Baumann, Nico Brünjes, Nicolas Drebenstedt, Ron Drongowski, '
         'Dominik Hoppe, Marco Kaiser, Harry Keller, Tom Lazar, Thomas Lotze, '
