0b02281c11559f2dfd9592291430f1699c2e1ef4
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2015-07-06 18:12:32 +0200

    ZON-1811: Tweak search form

--- javascript/web.site/plugins/jquery.searchTools.js
+++ javascript/web.site/plugins/jquery.searchTools.js
@@ -14,24 +14,22 @@
 */
 (function( $ ) {
 
-    $.fn.searchTools = function( options ) {
+    function toggleOptions() {
+        $( this ).closest( '.search-options' )
+            .toggleClass( 'search-options--open' )
+            .find( '.search-range' ).removeClass( 'search-range--open' );
+    }
 
-        var defaults = $.extend({
-            optionsHandle: '.search-options__on, .search-options__off',
-            dropdownHandle: '.search-range__legend'
-        }, options );
+    function toggleDropdown() {
+        $( this ).parent()
+            .toggleClass( 'search-range--open' )
+            .siblings( '.search-range' ).removeClass( 'search-range--open' );
+    }
 
-        function toggleOptions() {
-            $( this ).closest( '.search-options' ).toggleClass( 'search-options--open' );
-        }
-
-        function toggleDropdown() {
-            $( this ).parent().toggleClass( 'search-range--open' );
-        }
-
+    $.fn.searchTools = function() {
         return this.each( function() {
-            $( this ).find( defaults.optionsHandle ).on( 'click', toggleOptions );
-            $( this ).find( defaults.dropdownHandle ).on( 'click', toggleDropdown );
+            $( this ).find( '.search-options__on, .search-options__off' ).on( 'click', toggleOptions );
+            $( this ).find( '.search-range__legend' ).on( 'click', toggleDropdown );
         });
     };
 })( jQuery );
