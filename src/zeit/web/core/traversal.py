0a3194112f4d30cbb91f3664a7c998531226befa
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-07-07 16:01:28 +0200

    ZON-1934: Migrate and update traversal mechanics

---
+++ src/zeit/web/core/traversal.py
@@ -0,0 +1,187 @@
+import urlparse
+import logging
+
+import pyramid.interfaces
+import grokcore.component
+import pyramid.traversal
+import pyramid.httpexceptions
+import zope.component
+import zope.interface
+
+import zeit.cms.repository.interfaces
+import zeit.content.article.interfaces
+import zeit.content.cp.interfaces
+import zeit.content.dynamicfolder.interfaces
+import zeit.content.gallery.interfaces
+import zeit.content.video.interfaces
+import zeit.magazin.interfaces
+
+import zeit.web.site.module
+import zeit.web.core.article
+import zeit.web.core.template
+import zeit.web.core.utils
+import zeit.web.core.gallery
+import zeit.web.core.interfaces
+
+log = logging.getLogger(__name__)
+
+
+def traverser(*required):
+    def registrator(factory):
+        gsm = zope.component.getGlobalSiteManager()
+        gsm.registerSubscriptionAdapter(
+            factory, required, zeit.web.core.interfaces.ITraversable)
+        return factory
+    return registrator
+
+
+class Traversable(object):
+
+    def __init__(self, context):
+        self.context = context
+
+
+class Retraverse(Exception):
+
+    def __init__(self, request):
+        log.debug(u'Retraversing {}'.format(request.path))
+
+
+@grokcore.component.adapter(zeit.cms.repository.interfaces.IRepository)
+@grokcore.component.implementer(pyramid.interfaces.ITraverser)
+class RepositoryTraverser(pyramid.traversal.ResourceTreeTraverser):
+
+    def __call__(self, request):
+        tdict = super(RepositoryTraverser, self).__call__(request)
+        return self.call_subs(request=request, **tdict)
+
+    def call_subs(self, **tdict):
+        from pprint import pprint as pp
+        for sub in zope.component.subscribers(
+                [tdict.get('context')], zeit.web.core.interfaces.ITraversable):
+            try:
+                pp('____________')
+                pp(tdict)
+                sub(tdict)
+                pp(tdict)
+            except Retraverse:
+                return self.call_subs(**tdict)
+        return tdict
+
+
+@traverser(zeit.content.cp.interfaces.ICenterPage)
+class CP2015(Traversable):
+
+    def __call__(self, tdict):
+        try:
+            name = u'{}.cp2015'.format(tdict['context'].__name__)
+            assert name != tdict['context'].__name__
+            pos = tdict['traversed'].index(tdict['context'].__name__)
+            tdict['context'] = tdict['context'].__parent__[name]
+        except (AssertionError, KeyError, TypeError, ValueError):
+            pass
+        else:
+            travd = tdict['traversed']
+            tdict['traversed'] = travd[:pos] + (name,) + travd[pos + 1:]
+            raise Retraverse(tdict['request'])
+
+
+@traverser(zeit.content.article.interfaces.IArticle)
+class TraversableArticle(Traversable):
+
+    def __call__(self, tdict):
+        settings = zeit.magazin.interfaces.IArticleTemplateSettings(
+            self.context)
+
+        if urlparse.urlparse(
+                self.context.uniqueId).path.startswith('/feature/'):
+            # ToDo: Remove when Longform will be generally used on
+            # www.zeit.de. By then do not forget to remove marker
+            # interfaces from uniqueID http://xml.zeit.de/feature (RD)
+            zope.interface.alsoProvides(
+                self.context, zeit.web.core.article.IFeatureLongform)
+        elif settings.template == 'longform':
+            zope.interface.alsoProvides(
+                self.context, zeit.web.core.article.ILongformArticle)
+        elif settings.template == 'short':
+            zope.interface.alsoProvides(
+                self.context, zeit.web.core.article.IShortformArticle)
+        elif settings.template == 'column':
+            zope.interface.alsoProvides(
+                self.context, zeit.web.core.article.IColumnArticle)
+        elif settings.template == 'liveblog':
+            zope.interface.alsoProvides(
+                self.context, zeit.web.core.article.ILiveblogArticle)
+        elif settings.template == 'photocluster':
+            zope.interface.alsoProvides(
+                self.context, zeit.web.core.article.IPhotoclusterArticle)
+
+        if tdict['view_name'].startswith('seite') and not tdict['subpath']:
+            tdict['view_name'] = 'seite'
+
+
+@traverser(zeit.content.gallery.interfaces.IGallery)
+class TraversableGallery(Traversable):
+
+    def __call__(self, tdict):
+        metadata = zeit.content.gallery.interfaces.IGalleryMetadata(
+            self.context)
+
+        if metadata.type == 'zmo-product':
+            zope.interface.alsoProvides(
+                self.context, zeit.web.core.gallery.IProductGallery)
+        else:
+            zope.interface.alsoProvides(
+                self.context, zeit.web.core.gallery.IGallery)
+
+
+@traverser(zeit.content.cp.interfaces.ICenterPage)
+class TraversableCenterPage(Traversable):
+
+    def __call__(self, tdict):
+        area = zeit.web.core.utils.find_block(
+            self.context, attrib='area', kind='ranking')
+        if area:
+            area = zeit.web.core.template.get_area(area)
+            form = zeit.web.core.utils.find_block(
+                self.context, module='search-form')
+            # XXX: Pretty inelegant, maybe use pub/subbing in the future? (ND)
+            if form:
+                form = zeit.web.core.template.get_module(form)
+                form['q'] = u' '.join(tdict['request'].GET.getall('q'))
+                form['type'] = u' '.join(tdict['request'].GET.getall('type'))
+                form['mode'] = tdict['request'].GET.get('mode')
+                form['sort'] = tdict['request'].GET.get('sort')
+                area.raw_query = form.raw_query
+                area.sort_order = form.sort_order
+                area.query = form.query
+            else:
+                form = zeit.web.site.module.search_form.Form(tdict['context'])
+
+            form['page'] = tdict['request'].GET.get('p')
+            area.page = form.page
+
+
+@traverser(zeit.content.dynamicfolder.interfaces.IRepositoryDynamicFolder)
+class TraversableDynamic(TraversableCenterPage):
+
+    def __call__(self, tdict):
+        try:
+            tdict['context'] = self.context[tdict['view_name']]
+        except (IndexError, KeyError, TypeError):
+            pass
+        else:
+            tdict['traversed'] += (tdict['view_name'],)
+            tdict['view_name'] = ''
+
+
+@traverser(zeit.content.video.interfaces.IVideo)
+class TraversableVideo(Traversable):
+
+    def __call__(self, tdict):
+        # XXX: Let's hope no video is ever called 'imagegroup'
+        #      or 'comment-form' or 'report-form'. (ND)
+        if tdict['view_name'] not in (
+                'imagegroup', 'comment-form', 'report-form'):
+            tdict['request'].headers['X-SEO-Slug'] = tdict['view_name']
+            tdict['view_name'] = ''
