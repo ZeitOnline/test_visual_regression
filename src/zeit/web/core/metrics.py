92c230a4606dca1e2c73d0a47ea26a5776524773
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-07-27 11:57:36 +0200

    MAINT: Send coarse timing metrics (traversal, rendering) to statsd.

---
+++ src/zeit/web/core/metrics.py
@@ -0,0 +1,79 @@
+import contextlib
+import logging
+import mock
+import pyramid.events
+import statsd
+import zeit.web.core.interfaces
+import zope.component
+import zope.interface
+
+
+log = logging.getLogger(__name__)
+
+
+class Metrics(object):
+
+    zope.interface.implements(zeit.web.core.interfaces.IMetrics)
+
+    def __init__(self, hostname, port):
+        self.statsd = statsd.Connection(hostname, port)
+
+    def time(self, identifier):
+        return self.timer().time(identifier)
+
+    def increment(self, identifier, delta=1):
+        self.counter().increment(identifier, delta)
+
+    def set_gauge(self, identifier, value):
+        self.gauge().send(identifier, value)
+
+    def timer(self, identifier=None):
+        return statsd.Timer(identifier, self.statsd)
+
+    def counter(self, identifier=None):
+        return statsd.Counter(identifier, self.statsd)
+
+    def gauge(self, identifier=None):
+        return statsd.Gauge(identifier, self.statsd)
+
+
+@zope.interface.implementer(zeit.web.core.interfaces.IMetrics)
+def from_settings():
+    settings = zope.component.getUtility(zeit.web.core.interfaces.ISettings)
+    if settings.get('statsd_address'):
+        log.info('Initializing metrics collection to statsd at %s',
+                 settings['statsd_address'])
+        return Metrics(*settings['statsd_address'].split(':'))
+    else:
+        log.info(
+            'Not initializing metrics collection, no statsd_address setting')
+
+        @contextlib.contextmanager
+        def mock_time(self, identifier=None):
+            yield
+        metrics = mock.Mock()
+        metrics.time = mock_time
+        return metrics
+
+
+def timer(identifier):
+    metrics = zope.component.getUtility(zeit.web.core.interfaces.IMetrics)
+    return metrics.time(identifier)
+
+
+@pyramid.events.subscriber(pyramid.events.NewRequest)
+def view_timer_start(event):
+    metrics = zope.component.getUtility(zeit.web.core.interfaces.IMetrics)
+    event.request.view_timer = metrics.timer('zeit.web.core.view.pyramid')
+    event.request.view_timer.start()
+
+
+@pyramid.events.subscriber(pyramid.events.ContextFound)
+def view_timer_traversal(event):
+    event.request.view_timer.intermediate('traversal')
+
+
+@pyramid.events.subscriber(pyramid.events.NewResponse)
+def view_timer_rendering(event):
+    event.request.view_timer.intermediate('rendering')
+    event.request.view_timer.stop('total')
