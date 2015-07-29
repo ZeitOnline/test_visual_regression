0b27b912048e3c7a7f0e08ce68d18bf0332a6df3
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-07-29 17:36:36 +0200

    ZON-1586: Proper fallbacking for image variant getter

--- src/zeit/web/core/template.py
+++ src/zeit/web/core/template.py
@@ -30,15 +30,16 @@ log = logging.getLogger(__name__)
 
 
 @zeit.web.register_filter
-def get_image(module=None, content=None, fallback=True):
+def get_image(module, content=None, fallback=True):
     try:
         content = content or first_child(module)
         imagegroup = zeit.content.image.interfaces.IImages(content).image
     except (TypeError, AttributeError):
         imagegroup = None
 
-    if not zeit.content.image.interfaces.IImageGroup.providedBy(
-            imagegroup) and fallback:
+    if not zeit.content.image.interfaces.IImageGroup.providedBy(imagegroup):
+        if not fallback:
+            return None
         conf = zope.component.getUtility(zeit.web.core.interfaces.ISettings)
         default_id = conf.get('default_teaser_images')
         imagegroup = zeit.cms.interfaces.ICMSContent(default_id, None)
@@ -55,7 +56,7 @@ def get_image(module=None, content=None, fallback=True):
         image_pattern = 'default'
 
     variant = imagegroup.get_variant_by_name(image_pattern)
-    return zeit.web.core.interfaces.ITeaserImage(variant)
+    return zeit.web.core.interfaces.ITeaserImage(variant, None)
 
 
 @zeit.web.register_test
