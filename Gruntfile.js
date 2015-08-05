befa4b8f2fa269e92ec880d8227145af88c50b07
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2015-08-05 12:02:34 +0200

    Add another modernizr test for Mr. Julian S.

--- Gruntfile.js
+++ Gruntfile.js
@@ -342,7 +342,7 @@ module.exports = function(grunt) {
                 'uglify': true,
 
                 // Define any tests you want to implicitly include.
-                'tests': [ 'video', 'touch', 'csstransforms3d' ],
+                'tests': [ 'video', 'touch', 'csstransforms', 'csstransforms3d' ],
 
                 // By default, this task will crawl your project for references to Modernizr tests.
                 // Set to false to disable.
