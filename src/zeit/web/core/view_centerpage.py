3aaced7334e9306ca196799560df3e77cec8e176
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-08-07 16:04:43 +0200

    FIX: Subclass xml view for legacy centerpages

--- src/zeit/web/core/view_centerpage.py
+++ src/zeit/web/core/view_centerpage.py
@@ -1,5 +1,8 @@
-# -*- coding: utf-8 -*-
-from pyramid.view import view_config
+import collections
+import copy
+
+import pyramid.view
+import lxml.etree
 
 import zeit.cms.interfaces
 import zeit.cms.workflow
@@ -18,7 +21,7 @@ class Centerpage(zeit.web.core.view.Base):
         self.context.advertising_enabled = self.banner_on
 
         # Most of our resources will be purged from now on. We test this new
-        # mechanism on CPs. This might be valid for all resources in the future.
+        # mechanism on CPs. This might be valid for all resources in the future
         # (RD, 7.8.2015)
         self.request.response.headers.add('s-maxage', '21600')
 
@@ -48,8 +51,9 @@ class Centerpage(zeit.web.core.view.Base):
         return zeit.web.core.comments.get_counts(*[t.uniqueId for t in self])
 
 
-@view_config(route_name='json_update_time',
-             renderer='jsonp')
+@pyramid.view.view_config(
+    route_name='json_update_time',
+    renderer='jsonp')
 def json_update_time(request):
     try:
         resource = zeit.cms.interfaces.ICMSContent(
@@ -60,3 +64,39 @@ def json_update_time(request):
     except (AttributeError, KeyError, TypeError):
         dlps = dlp = None
     return {'last_published': dlp, 'last_published_semantic': dlps}
+
+
+@pyramid.view.view_config(
+    context=zeit.content.cp.interfaces.ICP2009,
+    name='xml',
+    renderer='string')
+class LegacyXMLView(zeit.web.core.view.Base):
+
+    def __call__(self):
+        xml = zeit.content.cp.interfaces.IRenderedXML(self.context)
+        return lxml.etree.tostring(xml, pretty_print=True)
+
+
+@pyramid.view.view_config(
+    context=zeit.content.cp.interfaces.ICP2015,
+    name='xml',
+    renderer='string')
+class XMLView(zeit.web.core.view.Base):
+
+    def __call__(self):
+        nsmap = collections.OrderedDict((
+            ('cp', 'http://namespaces.zeit.de/CMS/cp'),
+            ('py', 'http://codespeak.net/lxml/objectify/pytype'),
+            ('xi', 'http://www.w3.org/2001/XInclude'),
+            ('xsd', 'http://www.w3.org/2001/XMLSchema'),
+            ('xsi', 'http://www.w3.org/2001/XMLSchema-instance'),
+        ))
+        maker = lxml.objectify.ElementMaker(nsmap=nsmap)
+        root = getattr(maker, self.context.xml.tag)(
+            **self.context.xml.attrib)
+        root.append(copy.copy(self.context.xml.head))
+        body = lxml.objectify.E.body()
+        root.append(body)
+        for region in self.context.body.values():
+            body.append(zeit.content.cp.interfaces.IRenderedXML(region))
+        return lxml.etree.tostring(root, pretty_print=True)
