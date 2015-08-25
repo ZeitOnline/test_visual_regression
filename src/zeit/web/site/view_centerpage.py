c58e232700d260f741af24d446fb3b59ed3fb646
anszu <info@anikaszuppa.de>
2015-08-25 11:43:51 +0200

    ZON-2077: revert change in view_centerpage

--- src/zeit/web/site/view_centerpage.py
+++ src/zeit/web/site/view_centerpage.py
@@ -186,7 +186,7 @@ class Centerpage(
             return self.breadcrumbs_by_navigation(breadcrumbs)
 
         return breadcrumbs
-    
+
     @zeit.web.reify
     def regions(self):
         region_list = super(Centerpage, self).regions
