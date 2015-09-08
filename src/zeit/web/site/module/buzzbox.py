effe33ac196d07075514bfbe2300f493c60833ef
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2015-09-08 10:42:32 +0200

    ZON-1892: Add templates

--- src/zeit/web/site/module/buzzbox.py
+++ src/zeit/web/site/module/buzzbox.py
@@ -16,6 +16,7 @@ log = logging.getLogger(__name__)
 class Buzzbox(zeit.web.site.module.Module, list):
 
     header = None
+    score_factor = 1
 
     @zeit.web.reify
     def reach(self):
@@ -36,30 +37,54 @@ class Buzzbox(zeit.web.site.module.Module, list):
 class MostreadBuzzbox(Buzzbox):
 
     header = 'Meistgelesene Artikel'
+    label = 'Meist&shy;gelesen'
 
     def __init__(self, context):
         super(MostreadBuzzbox, self).__init__(context)
         self += self.reach.get_views(section=self.ressort)
         self.layout = 'buzz-mostread'
+        self.icon = 'buzz-read'
+        self.score_pattern = ['Keine Leser', '{} Leser']
 
 
 @zeit.web.register_module('mostcommented')
 class CommentsBuzzbox(Buzzbox):
 
     header = 'Meistkommentiert'
+    label = 'Meist&shy;kommentiert'
 
     def __init__(self, context):
         super(CommentsBuzzbox, self).__init__(context)
         self += self.reach.get_comments(section=self.ressort)
         self.layout = 'buzz-comments'
+        self.icon = 'buzz-comment'
+        self.score_pattern = ['Keine Kommentare', '{} Kommentar', '{} Kommentare']
 
 
 @zeit.web.register_module('mostshared')
 class FacebookBuzzbox(Buzzbox):
 
     header = 'Meistgeteilt'
+    label = 'Meist&shy;geteilt'
 
     def __init__(self, context):
         super(FacebookBuzzbox, self).__init__(context)
         self += self.reach.get_social(facet='facebook', section=self.ressort)
         self.layout = 'buzz-facebook'
+        self.icon = 'buzz-shared'
+        self.score_pattern = ['Nie geteilt', '{} mal geteilt']
+
+
+@zeit.web.register_module('mosttrending')
+class TrendingBuzzbox(Buzzbox):
+
+    header = 'Aufsteigend'
+    label = 'Aufsteigend'
+
+    def __init__(self, context):
+        super(TrendingBuzzbox, self).__init__(context)
+        self += self.reach.get_trend(section=self.ressort)
+        self.layout = 'buzz-trend'
+        self.icon = 'buzz-trend'
+        self.score_pattern = ['+ {0}']
+        self.score_factor = 10000
---
+++ src/zeit/web/site/templates/inc/teaser/buzzboard.tpl
@@ -0,0 +1,15 @@
+{%- extends "zeit.web.site:templates/inc/teaser/default.tpl" -%}
+
+{% block layout %}teaser-buzzboard{% endblock %}
+{% block teaser_journalistic_format %}{% endblock %}
+
+{#% block teaser_media_position_before_title %}
+    {% set module_layout = self.layout() %}
+    {% include "zeit.web.site:templates/inc/teaser_asset/{}.tpl".format(teaser | auto_select_asset | block_type) ignore missing %}
+{% endblock %#}
+
+{% block teaser_container %}
+	<span class="{{ self.layout() }}__metadata">
+		{{ (teaser.score * module.score_factor) | round | pluralize(*module.score_pattern) }}
+	</span>
+{% endblock %}
