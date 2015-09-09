48292c8382fa7f9e031fb4eef5bc2d9d19cad3c1
Ron Drongowski <ron.drongowski@gmail.com>
2015-09-09 10:27:04 +0200

    Test requests with newsfeed host header

---
+++ src/zeit/web/site/test/test_feed.py
@@ -0,0 +1,30 @@
+import requests
+import lxml.etree
+
+def test_newsfeed_should_only_render_cp2015(testserver):
+
+    res = requests.get(
+        '%s/index' % testserver.url,
+        headers={'Host':'newsfeed.zeit.de'})
+
+    assert res.status_code == 200
+
+    res = requests.get(
+        '%s/centerpage/index' % testserver.url,
+        headers={'Host':'newsfeed.zeit.de'})
+
+    assert res.status_code == 404
+
+    res = requests.get(
+        '%s/artikel/01' % testserver.url,
+        headers={'Host':'newsfeed.zeit.de'})
+
+    assert res.status_code == 404
+
+def test_newsfeed_should_render_some_rss(testserver):
+    res = requests.get(
+        '%s/index' % testserver.url,
+        headers={'Host':'newsfeed.zeit.de'})
+
+    xml = lxml.etree.fromstring(res.content)
+    assert xml.tag == 'rss'
