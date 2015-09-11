76c453964a729701ec2cc37b4b6a698a045b02ba
Arne Seemann <arne.seemann@gmail.com>
2015-09-11 12:14:33 +0200

    ZON-2199: Update tests to new testbrowser best practice.

--- src/zeit/web/core/test/test_link.py
+++ src/zeit/web/core/test/test_link.py
@@ -4,20 +4,18 @@ import zeit.cms.interfaces
 import zeit.web.core.template
 
 
-def test_link_object_should_redirect_permanently(testserver, testbrowser):
-    path = 'blogs/nsu-blog-bouffier'
-    browser = testbrowser('%s/%s' % (testserver.url, path))
+def test_link_object_should_redirect_permanently(testbrowser):
+    browser = testbrowser('blogs/nsu-blog-bouffier')
     assert (
         'http://blog.zeit.de/nsu-prozess-blog/2015/02/25'
         '/medienlog-zwickau-zschaepe-yozgat-verfassungsschutz-bouffier/' ==
         browser.url)
 
 
-def test_link_object_teaser_should_lead_directly_to_destination(
-        testserver, testbrowser):
-    url = '%s/zeit-magazin/test-cp/test-cp-zmo-3' % testserver.url
+def test_link_object_teaser_should_lead_directly_to_destination(testbrowser):
+    browser = testbrowser('/zeit-magazin/test-cp/test-cp-zmo-3')
     assert ('<a href="http://www.zeit.de/zeit-magazin/mode-design/2014-05/'
-            'karl-lagerfeld-interview">') in testbrowser(url).contents
+            'karl-lagerfeld-interview">') in browser.contents
 
 
 def test_create_url_filter_should_create_correct_url(testserver, testbrowser):
