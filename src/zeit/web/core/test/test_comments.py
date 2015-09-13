2ca0941d261050a7862ab3546643c48271a7d7cf
Thomas Puppe <thomas.puppe@zeit.de>
2015-09-13 16:17:44 +0200

    ZON-2126: fix PEP8

--- src/zeit/web/core/test/test_comments.py
+++ src/zeit/web/core/test/test_comments.py
@@ -473,6 +473,7 @@ def test_action_url_should_be_created_correctly(
     poster = _create_poster(monkeypatch)
     assert poster._action_url(action, path) == service
 
+
 @pytest.mark.parametrize("action", ['comment', 'report'])
 def test_post_comment_should_set_lock(application, action):
     request = mock.Mock()
@@ -483,7 +484,7 @@ def test_post_comment_should_set_lock(application, action):
 
     locker(request, action)
 
-    assert request.session['lock_commenting'] == True
+    assert request.session['lock_commenting'] is True
 
     with pytest.raises(pyramid.httpexceptions.HTTPForbidden):
         locker(request, action)
@@ -491,7 +492,7 @@ def test_post_comment_should_set_lock(application, action):
     time.sleep(0.5)
 
     locker(request, action)
-    assert request.session['lock_commenting'] == True
+    assert request.session['lock_commenting'] is True
 
 
 @pytest.mark.parametrize("action", ['recommend', 'promote', 'demote'])
@@ -499,7 +500,7 @@ def test_post_comment_should_not_set_lock(application, action):
     request = mock.Mock()
     request.session = {'user': {'name': 'foo'}}
     pc = zeit.web.core.view_comment.PostComment(mock.Mock(), request)
-    pc.lock_duration = datetime.timedelta(0,0.5)
+    pc.lock_duration = datetime.timedelta(0, 0.5)
     locker = pc.handle_comment_locking
     locker(request, action)
 
