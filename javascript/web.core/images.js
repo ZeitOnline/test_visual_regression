8dbe4037353b31d4a39cbd59216d78aaf9f938d8
Nico Brünjes <nico.bruenjes@gmail.com>
2015-10-09 17:07:26 +0200

    ZON-2292: Add lazy loading features to images.js

--- javascript/web.core/images.js
+++ javascript/web.core/images.js
@@ -1,6 +1,6 @@
 /**
  * @fileOverview Module for responsive images
- * @version  0.1
+ * @version  0.2 (adds lazy loading features)
  */
 /**
  * images.js: module for images
@@ -9,8 +9,13 @@
 define([ 'sjcl', 'jquery', 'jquery.debounce' ], function( sjcl, $ ) {
 
     var images = [],
-        isMobile,
-        isDesktop;
+        $w = $( window ),
+        threshold = 300,
+        proxyScreen = {
+            breakpoint: window.ZMO.breakpoint.get(),
+            width: $w.width()
+        },
+        isMobile, isDesktop, $triggerRegion;
 
     /**
      * images.js: create prefix
@@ -50,7 +55,7 @@ define([ 'sjcl', 'jquery', 'jquery.debounce' ], function( sjcl, $ ) {
      * images.js: initiate globals for module
      * @function prepareScaling
      */
-    function prepareScaling () {
+    function prepareScaling() {
         var breakpoint = window.ZMO.breakpoint.get();
 
         isMobile = /mobile|phablet/.test( breakpoint );
@@ -58,24 +63,20 @@ define([ 'sjcl', 'jquery', 'jquery.debounce' ], function( sjcl, $ ) {
     }
 
     /**
-     * images.js: scale one image
-     * @function scaleImage
-     * @param  {object} image HTMLImageElement
+     * images.js: get width, height and source for one image
+     * @function getImageData
+     * @param  {object} image     jquery object with image
+     * @return {object}           object with width, height, source
      */
-    function scaleImage( image, container ) {
-        var $img = $( image ),
-            $parent = container || $img.closest( '.scaled-image' ),
+    function getImageData( $img ) {
+        prepareScaling();
+        var image = $img.get( 0 ),
+            $parent = $img.closest( '.scaled-image' ),
             imageData = $img.data(),
             useMobileVariant = isMobile && imageData.mobileRatio && imageData.mobileSrc,
             ratio = useMobileVariant ? imageData.mobileRatio : imageData.ratio,
             msieWidth = false,
-            width, height, token, source, subject,
-            styles, minHeight, maxHeight;
-
-        // do not load images which are not shown anyway (zon-small on mobile for example)
-        if ( $parent.css( 'display' ) === 'none' ) {
-            return;
-        }
+            subject, width, height, leaf, styles, minHeight, maxHeight, source, token;
 
         if ( $parent.hasClass( 'is-pixelperfect' ) ) {
             // use explicit width and height from parent
@@ -90,7 +91,7 @@ define([ 'sjcl', 'jquery', 'jquery.debounce' ], function( sjcl, $ ) {
 
             // workaround for hidden images
             if ( !subject.is( ':visible' ) ) {
-                var leaf = subject;
+                leaf = subject;
 
                 do {
                     if ( leaf.prop( 'hidden' ) ) {
@@ -146,87 +147,215 @@ define([ 'sjcl', 'jquery', 'jquery.debounce' ], function( sjcl, $ ) {
             }
         }
 
+        // source berechnen
         source = useMobileVariant ? imageData.mobileSrc : imageData.src;
         width = Math.round( width );
         height = Math.round( height );
 
         if ( /bitblt/.test( source ) ) { // #TRASHME: for old BitBlt shizzle
             token = prefix( width, height );
-            image.src = source.replace( /\/bitblt-\d+x\d+-[a-z0-9]+/, token );
+            source = source.replace( /\/bitblt-\d+x\d+-[a-z0-9]+/, token );
         } else {
-            image.src = source + '__' + width + 'x' + height;
+            source = source + '__' + width + 'x' + height;
         }
+
+        return {
+            width: width,
+            height: height,
+            source: source
+        };
     }
 
     /**
-     * images.js: scale contained images
-     * @function scaleImages
-     * @param  {object} container DOM element, array of elements or jQuery object
-     *                            used as selector context, defaults to document root
+     * images.js: show one image by replaceing the src
+     * @function showImage
+     * @param  {object} $img jquery object with image
      */
-    function scaleImages( container ) {
-        prepareScaling();
-        $( '.scaled-image > noscript', container ).each( function() {
-            var $noscript = $( this ),
-                $parent = $noscript.parent(),
-                markup = $noscript.text();
+    function showImage( $img ) {
+        var imgData = $img.data();
+        $img
+            .data( 'loading', true )
+            .data( 'tolazyload', false )
+            .attr( 'src', imgData.source )
+            .attr( 'alt', imgData.alt )
+            .on( 'load', function() {
+                $( this )
+                    .removeClass( 'image--processing' )
+                    .data( 'loading', false )
+                    .data( 'loaded', true )
+                    .attr( 'width', '' )
+                    .attr( 'height', '' );
+            });
+    }
 
-            if ( markup.trim() !== '' ) {
-
-                // get rid of src attribute
-                // otherwise the browser would load it when put into the DOM
-                // markup = markup.replace( / src=("|')[^"']+\1/g, '' );
-                markup = markup.replace( 'src="', 'data-dev-null="' );
-                $parent.html( markup );
-                var $img = $parent.find( 'img' );
-
-                // remove possible fallback source information
-                $img.removeAttr( 'data-dev-null' );
-
-                // add event triggering to tell the world
-                $img.on( 'load', function( e ) {
-                    $img.trigger( 'scaling_ready' );
-                });
-
-                scaleImage( $img.get( 0 ), $parent );
-                images.push( $img.get( 0 ) );
-            } else {
-                // noscript tag contains no readable content (might happen in older browsers)
-                // therefore we have to hide allocated image spaces
-                hideImage( $parent, $noscript.attr( 'data-src' ));
+    /**
+     * images.js: take a bunch of images, prepare them for showing
+     * @function showImages
+     * @param  {array} imageArray  optional array of jQuery objects of images
+     */
+    function showImages( imageArray ) {
+        imageArray = imageArray || $( images ).filter( function() {
+            return $( this ).data( 'tolazyload' ) !== true;
+        });
+        $( imageArray ).each( function() {
+            var $img = $( this );
+            if ( $img.data( 'toscale' ) ) {
+                $img = prepareImage( $( this ) );
             }
+            if ( $img.data( 'tolazyload' ) === true ) {
+                $img.addClass( 'image--processing' );
+            }
+            showImage( $img );
         });
     }
 
     /**
-     * images.js: rescale all previously scaled images
+     * images.js: filter function to check if an item is inview
+     * called as jQuery filter, $( this ) is provided by jQuery.fn.filter
+     * @function isInView
+     * @return {Boolean}
+     */
+    function isInView() {
+        var $parent = $( this ).closest( '.scaled-image' );
+        if ( $( this ).data( 'loaded' ) === true || $( this ).data( 'loading' ) === true || $parent.css( 'display' ) === 'none' ) {
+            return false;
+        }
+        var $e = $( this ),
+            wt = $w.scrollTop(),
+            wb = wt + $w.height(),
+            et = $e.offset().top,
+            eb = et + $e.height();
+
+        return eb >= wt - threshold && et <= wb + threshold;
+    }
+
+    /**
+     * images.js: collect images in viewport and show them
+     * event for unveiling lazy images
+     */
+    function showLazyImages() {
+        var inview = $( images ).filter( isInView );
+        if ( inview.length > 0 ) {
+            showImages( inview );
+        }
+    }
+
+    /**
+     * images.js: prepare one image for loading and/or lazy loading
+     * @function prepareImage
+     * @param  {object} $img           jquery object with img
+     * @param  {Boolean} markLazyImages differ between direct and lazy loaded images
+     * @return {object}                jquery object with img
+     */
+    function prepareImage( $img, markLazyImages ) {
+        var imgData;
+        // remove possible fallback source information
+        $img.removeAttr( 'data-dev-null' );
+        imgData = getImageData( $img );
+        $img
+            .attr( 'width', imgData.width )
+            .attr( 'height', imgData.height )
+            .data( 'source', imgData.source )
+            .data( 'processed', true );
+        // add blank.gif (really, it is) to prevent rendering of broken image icon, if there's no src
+        $img.attr( 'src', function() {
+            return $( this ).attr( 'src' ) || 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
+        });
+        // add event triggering to tell the world
+        $img.on( 'load', function( e ) {
+            $img.trigger( 'scaling_ready' );
+        });
+        if ( markLazyImages && $img.closest( '.cp-region' ).data( 'lazy' ) === true ) {
+            $img.data( 'tolazyload', true );
+        }
+        return $img;
+    }
+
+    /**
+     * images.js: prepare all images on the page for direct or lazy loading
+     * @function prepareImages
+     * @return {array}           Array of images
+     */
+    function prepareImages() {
+        var images = [];
+        prepareScaling();
+        $( '.scaled-image > noscript' ).each( function() {
+            var $noscript = $( this ),
+                $parent = $noscript.parent(),
+                markup = $noscript.text(),
+                imgData;
+
+            if ( markup.trim() !== '' ) {
+                // get rid of src attribute
+                // otherwise the browser would load it when put into the DOM
+                // markup = markup.replace( / src=('|')[^'']+\1/g, '' );
+                // replace src-attr to prevent regex and remove it later
+                markup = markup.replace( 'src="', 'data-dev-null="' );
+                // hide alt from display while image is processed
+                markup = markup.replace( 'alt="', 'data-alt="' );
+                $parent.html( markup );
+                images.push( prepareImage( $parent.find( 'img' ), true ) );
+
+            } else {
+                // noscript tag contains no readable content (might happen in older browsers)
+                // therefore we have to hide allocated image spaces
+                hideImage( $parent, $noscript.attr( 'data-src' ) );
+            }
+        });
+        return images;
+    }
+
+    /**
+     * images.js: mark all images for rescale, rescale those in viewport
      * @function rescaleAll
      */
     function rescaleAll() {
-        prepareScaling();
-        for ( var i = images.length; i--; ) {
-            // verify that image is still part of the DOM
-            if ( $.contains( document.documentElement, images[i] )) {
-                scaleImage( images[i] );
-            } else {
-                // remove the image reference
-                images.splice( i, 1 );
+        var oldBreakpoint = proxyScreen.breakpoint,
+            oldWidth = proxyScreen.width;
+        // mark actual screen env
+        proxyScreen.breakpoint = window.ZMO.breakpoint.get();
+        proxyScreen.width = $w.width();
+        // only rescale if needed
+        if ( proxyScreen.breakpoint !== oldBreakpoint || proxyScreen.width > oldWidth ) {
+            $( images ).each( function() {
+                $( this )
+                    .data( 'loading', false )
+                    .data( 'loaded', false )
+                    .data( 'tolazyload', true )
+                    .data( 'toscale', true );
+            });
+            var inview = $( images ).filter( isInView );
+            if ( inview.length > 0 ) {
+                showImages( inview );
             }
         }
     }
 
     /**
-     * images.js: initialize image scaling
+     * images.js: initialize images
      * @function init
+     * @param  {object} options jQuery styled preferences object
      */
-    function init() {
-        scaleImages();
-
-        $( window ).on( 'resize', $.debounce( rescaleAll, 1000 ) );
+    function init( options ) {
+        options = $.extend( {
+            triggerRegionNumber: 3,
+            lazy: $( 'body[data-page-type="centerpage"]' ).length > 0,
+            lazyElementsSelector: '.cp-region'
+        }, options );
+        // if lazy, mark lazy regions
+        if ( options.lazy ) {
+            $( options.lazyElementsSelector + ':gt(' + ( options.triggerRegionNumber - 1 ) + ')' ).data( 'lazy', true );
+        }
+        images = prepareImages();
+        showImages();
+        // @todo: find a way to check if all images are unveiled and unbind event
+        $w.on( 'scroll.lazy', showLazyImages );
+        showLazyImages();
+        $w.on( 'resize.rescale', $.debounce( rescaleAll, 1000 ) );
     }
 
     return {
-        scale: scaleImages,
+        scale: showImages,
         init: init
     };
 
