c4f292ae470ba600e547890ed06699ac968c92ee
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-10-12 13:47:32 +0200

    ZON-2288: Fix references

--- src/zeit/web/site/area/zett.py
+++ src/zeit/web/site/area/zett.py
@@ -5,18 +5,18 @@ import grokcore.component
 import zeit.content.image.interfaces
 
 import zeit.web
-import zeit.web.core.centerpage
+import zeit.web.core.image
 import zeit.web.site.area.rss
 
 log = logging.getLogger(__name__)
 
 
 @grokcore.component.implementer(zeit.content.image.interfaces.IImageGroup)
-@grokcore.component.adapter(zeit.web.site.area.rss.RSSLink, name='zett')
-class ZettImageGroup(zeit.web.core.centerpage.LocalImageGroup):
+@grokcore.component.adapter(zeit.web.site.area.rss.IRSSLink, name='zett')
+class ImageGroup(zeit.web.core.image.LocalImageGroup):
 
     def __init__(self, context):
-        super(ZettImageGroup, self).__init__(context)
+        super(ImageGroup, self).__init__(context)
         self.image_url = context.image_url
         self.uniqueId = 'http://xml.zeit.de/zett-image{}'.format(
             context.image_url.replace('http://ze.tt', ''))
@@ -26,5 +26,3 @@ class ZettImageGroup(zeit.web.core.centerpage.LocalImageGroup):
 class Zett(zeit.web.site.area.rss.RSSArea):
 
     feed_key = 'zett_hp_feed'
-    module_layout = 'zon-small'
-    # XXX Don't split title and supertitle
