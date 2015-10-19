312da491edff32e51aaf036b70c52e0d0445c85d
Anika Szuppa <anika.szuppa@zeit.de>
2015-10-19 15:05:45 +0200

    ZON-1944: cleanup

--- javascript/web.site/plugins/jquery.animateJobs.js
+++ javascript/web.site/plugins/jquery.animateJobs.js
@@ -28,14 +28,6 @@
     */
     $.fn.animateJobs = function() {
 
-        $.each( ['show', 'hide'], function( i, ev ) {
-            var el = $.fn[ev];
-            $.fn[ev] = function() {
-                this.trigger( ev );
-                return el.apply( this, arguments );
-            };
-        }) ;
-
         var box = {
             current: 0,
             maxJobs: 0,
