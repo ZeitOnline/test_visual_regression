5dfcc77a4aeef34d2b67a903dd91d1abcfd1205b
Nico Brünjes <nico.bruenjes@gmail.com>
2015-10-20 10:57:59 +0200

    ZON-2144: Add tests

--- src/zeit/web/site/test/test_modules.py
+++ src/zeit/web/site/test/test_modules.py
@@ -3,6 +3,7 @@ import pytest
 import zeit.content.cp.interfaces
 
 import zeit.web.site.module.headerimage
+import zeit.web.site.module.xml
 import zeit.web.core.utils
 
 
@@ -33,3 +34,26 @@ def test_headerimage_should_have_complete_source(testbrowser):
     assert len(browser.cssselect('.header-image')) == 1
     assert len(browser.cssselect('.header-image__kicker')) == 1
     assert len(browser.cssselect('.header-image__title')) == 1
+
+
+@pytest.fixture
+def rawXml(application):
+    context = zeit.cms.interfaces.ICMSContent(
+        'http://xml.zeit.de/zeit-online/index-with-image')
+    block = zeit.web.core.utils.find_block(
+        context, module='xml')
+    return zeit.web.core.template.get_module(block)
+
+
+def test_raw_xml_should_have_attribute_alldevices(rawXml):
+    assert rawXml.alldevices is True
+
+
+def test_raw_xml_contains_html(rawXml):
+    assert '<section class="servicelinks">' in rawXml.xml
+    assert 'http://services.zeit.de/toptarif/vergleichsrechner.p' in rawXml.xml
+    assert 'http://marktplatz.zeit.de/angebote/job-und-karriere' in rawXml.xml
+    assert 'http://www.bellevue-ferienhaus.de/zeit/' in rawXml.xml
+    assert 'http://ad-emea.doubleclick.net/clk;265976593;9126704' in rawXml.xml
+    assert 'https://premium.zeit.de/?wt_mc=pm.intern.fix.zeitde.' in rawXml.xml
+    assert 'https://premium.zeit.de/abo/digitalpaket' in rawXml.xml
