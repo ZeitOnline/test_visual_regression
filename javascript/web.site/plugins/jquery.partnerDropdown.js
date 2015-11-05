eeb7eeaca1d0d7832cceced5ac9dab1b5b774cc2
Anika Szuppa <anika.szuppa@zeit.de>
2015-11-05 11:44:58 +0100

    FIX: use class instead of selector

--- javascript/web.site/plugins/jquery.partnerDropdown.js
+++ javascript/web.site/plugins/jquery.partnerDropdown.js
@@ -36,7 +36,7 @@
                     tracking = $dropdown.data( 'tracklink' );
 
                 $button.on( 'click', function( event ) {
-                    var link = $dropdown.find( 'option:selected' ).attr( 'value' );
+                    var link = $dropdown.find( '.pa-dropdown__option:selected' ).attr( 'value' );
                     if ( link ) {
                         window.location.href = link + tracking;
                     }
