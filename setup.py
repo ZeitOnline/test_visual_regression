16b0cf7514f3d913e532d027c058da4a5db375ce
Arne Seemann <arne.seemann@gmail.com>
2015-11-09 16:55:17 +0100

    Back to development: 2.25

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='zeit.web',
     url='https://github.com/ZeitOnline/zeit.web',
-    version='2.24',
+    version='2.25.dev0',
     author=(
         'Thomas Baumann, Nico Bruenjes, Nicolas Drebenstedt, Ron Drongowski, '
         'Dominik Hoppe, Marco Kaiser, Harry Keller, Tom Lazar, Thomas Lotze, '
