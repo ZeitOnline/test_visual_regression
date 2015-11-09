88ed31f227e002fcc3ebcb2f41335ba6a6adfa75
Nico Brünjes <nico.bruenjes@gmail.com>
2015-11-09 14:07:56 +0100

    ZON-2331: Add initializing

--- javascript/web.core/adReload.js
+++ javascript/web.core/adReload.js
@@ -6,9 +6,27 @@
 define( [ 'jquery' ], function( $ ) {
 
     var debug = false,
-    interaction = function( event ) {
-
+    /**
+     * initialize a new counting mandator by checking id against configuration
+     * @return {bool}   return state when ready
+     */
+    initialize = function() {
+        console.debug( 'initialize' );
+        // ggf. config laden
+        // im localstorage ablegen
+        // returnen
     },
+    interaction = function( event, sender, message ) {
+        console.debug( 'interaction', event, sender, message );
+        // check config
+        // check session storage
+        // defer_counting
+    },
+    /**
+     * channel filtered window.messages to interaction api
+     * @param  {object} event DOM event
+     * @return {void}
+     */
     message = function( event ) {
         var messageData, sender, message;
         try {
@@ -17,18 +35,25 @@ define( [ 'jquery' ], function( $ ) {
             return;
         }
         if ( typeof messageData.sender !== 'string' || typeof messageData.message !== 'string' ) {
-            console.error('messageData not completely set')
+            console.error( 'messageData not completely set' );
             return;
         }
-
+        $( window ).trigger( 'interaction.adreload.z', [ sender, message ]);
     };
 
     return {
         init: function() {
-            // add eventlistener
-            $( window ).on( 'interaction.z', interaction );
-            // listen to window.messages for channel interactions
-            $( window ).on( 'message', message );
+            // configure page
+            if ( initialize() ) {
+                // add eventlistener
+                $( window ).on( 'interaction.adreload.z', interaction );
+                // listen to window.messages for channel interactions
+                $( window ).on( 'message', message );
+            }
+            // on page unload unbind all at unload to prevent memory leaks
+            $( window ).on( 'unload', function( event ) {
+                $( window ).off( 'adreload' );
+            });
         }
     };
 });
