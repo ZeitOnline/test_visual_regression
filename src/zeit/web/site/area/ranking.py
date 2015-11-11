dba3a5732ddba870068620bd803408aa5288903f
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-11-11 17:27:46 +0100

    ZON-2159: Respect automatic area query attribute API

--- src/zeit/web/site/area/ranking.py
+++ src/zeit/web/site/area/ranking.py
@@ -139,7 +139,7 @@ class Ranking(zeit.content.cp.automatic.AutomaticArea):
         return doc
 
     @property
-    def query(self):
+    def query_string(self):
         request = pyramid.threadlocal.get_current_request()
         param = u' '.join(request.GET.getall('q'))
         if param and self.raw_query:
