f055d6dc6f1848e821585d78fd5b661672d925d8
Nicolas Drebenstedt <cutoffthetop@me.com>
2015-11-13 17:14:49 +0100

    ZON-2159: Thanks for nothing pyramid DummyRequest

--- src/zeit/web/core/test/conftest.py
+++ src/zeit/web/core/test/conftest.py
@@ -18,6 +18,7 @@ import pytest
 import repoze.bitblt.processor
 import selenium.webdriver
 import transaction
+import webob.multidict
 import webtest
 import zope.browserpage.metaconfigure
 import zope.event
@@ -349,6 +350,7 @@ def config(application, request):
 @pytest.fixture
 def dummy_request(request, config):
     req = pyramid.testing.DummyRequest(is_xhr=False)
+    req.GET = webob.multidict.MultiDict(req.GET)
     req.response.headers = set()
     req.registry.settings = config.registry.settings
     req.matched_route = None
