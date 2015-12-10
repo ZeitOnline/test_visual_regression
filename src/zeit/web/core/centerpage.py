721379a1741e5531f701020aaab12a5bc6f62180
Ron Drongowski <ron.drongowski@gmail.com>
2015-12-10 14:24:08 +0100

    Add missing import

--- src/zeit/web/core/centerpage.py
+++ src/zeit/web/core/centerpage.py
@@ -4,6 +4,8 @@ import grokcore.component
 import zope.component
 
 import zeit.cms.interfaces
+import zeit.content.author
+import zeit.content.author.interfaces
 import zeit.content.cp.area
 import zeit.content.cp.blocks.teaser
 import zeit.content.cp.interfaces
