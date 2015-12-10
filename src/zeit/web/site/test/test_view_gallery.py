ZeitOnline/test_visual_regression

commit d9347308a32897bfda79893a9e1dd18abce07898

ZON-2429: Implement gallery image filter (still using bitblt)

src/zeit/web/core/gallery.py
@@ -9,6 +9,7 @@ import zeit.content.image.image
 import zeit.content.gallery.gallery
 
 import zeit.web.core.block
+import zeit.web.core.template
 import zeit.web.core.utils
 
 # Custom interface classes to destinguish between regular galleries (inline
@@ -34,8 +35,8 @@ class IGalleryImage(zope.interface.Interface):
 class GalleryImage(zeit.web.core.image.BaseImage):
 
     def __init__(self, item):
-        self.caption = item.caption
-        self.layout = 'large'  # item.layout
+        self.layout = item.layout or 'large'
+        self.image_pattern = 'zon-large'
         self.title = item.title
         self.text = item.text
 
@@ -44,11 +45,25 @@ class GalleryImage(zeit.web.core.image.BaseImage):
             self.uniqueId = item.image.uniqueId
             self.image = item.image
 
-        meta = zeit.content.image.interfaces.IImageMetadata(item)
+        meta = zeit.content.image.interfaces.IImageMetadata(item.image)
         fix_ml = zeit.web.core.utils.fix_misrepresented_latin
         self.copyright = list((fix_ml(i[0]),) + i[1:] for i in meta.copyrights)
         self.alt = meta.alt
         self.align = meta.alignment
+        self.caption = meta.caption
+
+
+@zeit.web.register_global
+def get_gallery_image(module=None, content=None, **kwargs):
+    # XXX Re-implement once we have a solution for RepositoryImage variants.
+
+    if content is None:
+        content = zeit.web.core.template.first_child(module)
+
+    if content is None:
+        return
+
+    return zeit.web.core.template.first_child(Gallery(content).itervalues())
 
 
 class Gallery(collections.OrderedDict):
@@ -62,7 +77,7 @@ class Gallery(collections.OrderedDict):
         return object.__repr__(self)
 
     @property
-    def galleryText(self):
+    def galleryText(self):  # NOQA
         return zeit.wysiwyg.interfaces.IHTMLContent(
             self.context).html
 

commit 75e6279255ede6180e4856627496f8beebf1aa04

HOTFIX: Display gallery entry captions again
This was accidentaly broken in commit:d934730 for ZON-2429

src/zeit/web/core/gallery.py
@@ -50,7 +50,7 @@ class GalleryImage(zeit.web.core.image.BaseImage):
         self.copyright = list((fix_ml(i[0]),) + i[1:] for i in meta.copyrights)
         self.alt = meta.alt
         self.align = meta.alignment
-        self.caption = meta.caption
+        self.caption = item.caption
 
 
 @zeit.web.register_global

src/zeit/web/site/test/test_view_centerpage.py
@@ -395,7 +395,9 @@ def test_snapshot_should_display_correct_teaser_title(testbrowser):
 def test_snapshot_should_display_correct_image_caption(testbrowser):
     browser = testbrowser('/zeit-online/teaser-gallery-setup')
     caption = browser.cssselect('.snapshot-caption')[0]
-    assert caption.text.strip() == 'Eisschwimmen im chinesischen Shenyang'
+    assert (caption.text.strip() ==
+            u'Ford präsentiert auf der Automesse in Detroit'
+            u' den neuen Pick-up F-150.')
 
 
 def test_snapshot_should_display_copyright_with_nonbreaking_space(testbrowser):
@@ -407,7 +409,9 @@ def test_snapshot_should_display_copyright_with_nonbreaking_space(testbrowser):
 def test_snapshot_should_have_description_text(testserver, testbrowser):
     browser = testbrowser('/zeit-online/teaser-gallery-setup')
     caption = browser.cssselect('.snapshot-caption')[0]
-    assert caption.text.strip() == u'Eisschwimmen im chinesischen Shenyang'
+    assert (caption.text.strip() ==
+            u'Ford präsentiert auf der Automesse in Detroit'
+            u' den neuen Pick-up F-150.')
 
 
 def test_small_teaser_without_image_has_no_padding_left(

src/zeit/web/site/test/test_view_gallery.py
@@ -23,6 +23,13 @@ def test_zon_gallery_should_have_metadata(testbrowser, testserver):
     assert len(select('.summary, .byline, .metadata')) == 3
 
 
+def test_gallery_should_display_entry_caption(testbrowser, testserver):
+    browser = testbrowser('{}/zeit-online/gallery/biga_1'.format(
+        testserver.url))
+    assert browser.cssselect('.figure__text')[0].text.startswith(
+        'Mathias Modica')
+
+
 def test_zon_gallery_should_have_no_pagination(testbrowser, testserver):
     select = testbrowser('{}/zeit-online/gallery/biga_1'.format(
         testserver.url)).cssselect