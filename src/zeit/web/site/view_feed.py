80020915ba6d1cf61d0912fb444441f0211693f9
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2015-12-17 10:50:08 +0100

    FIX: Retrieve mobile text from the correct field for roost feed
    In commit:8431e0b we forgot to check the channel; and anyway
    duplicating this implementation was not a good idea, using
    IAccountData is much better.

--- src/zeit/web/site/view_feed.py
+++ src/zeit/web/site/view_feed.py
@@ -475,7 +475,4 @@ class RoostFeed(SocialFeed):
         return content.supertitle
 
     def social_value(self, content):
-        push = zeit.push.interfaces.IPushMessages(content)
-        for config in push.message_config:
-            if config.get('type') == 'parse':
-                return config.get('override_text')
+        return zeit.push.interfaces.IAccountData(content).mobile_text
