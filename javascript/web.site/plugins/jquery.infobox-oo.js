f4cb1f4e95508b76b81e3cb6e10a3c3a362e08fd
Valentin von Guttenberg <valentin.vonguttenberg@zeit.de>
2015-12-18 16:10:51 +0100

    ZON-2230: refactoring

--- javascript/web.site/plugins/jquery.infobox-oo.js
+++ javascript/web.site/plugins/jquery.infobox-oo.js
@@ -1,88 +1,65 @@
 /**
  * @fileOverview jQuery Plugin for additional functionality in infoboxes
- * @author nico.bruenjes@zeit.de
- * @version  0.1
+ * @author nico.bruenjes@zeit.de, valentin.vonguttenberg@zeit.de
+ * @version  0.2
  */
 (function( $, window ) {
 
     function InfoboxOO( infobox ) {
         this.id = infobox.id;
-        this.isDesktop = this.isDesktopView();
         this.infobox = $( infobox );
         this.navigation = $( '#' + this.id + '-navigation', this.infobox );
         this.tabpanels = $( '.infobox-tab__panel', this.infobox );
-        this.tabtitlesMobile = $( '.infobox-tab__title', this.infobox );
-        this.tabtitles = this.tabtitlesMobile
+        this.tabtitles = $( '.infobox-tab__title', this.infobox )
                             .clone()
                             .appendTo( this.navigation );
-        this.tablinks = $( this.tabtitles ).find( 'a' );
-        this.tablinksMobile = $( this.tabtitlesMobile ).find( 'a' );
-        this.currentTab = this.tablinks.first();
+        this.tabs = $( this.tabtitles ).find( 'a' );
+        this.hasSidebar = this.hasSidebarNavigation();
 
         this.init();
     }
 
     InfoboxOO.prototype.init = function() {
-        var self = this;
+        var self = this,
+            openTab = this.getSelectedTabByHash() || this.tabs.first();
 
         this.navigation.attr( 'role', 'tablist' );
+        this.setTabsAndTabtitles();
 
-        if ( this.isDesktop ) {
-            // TODO refactor this
-            this.tabtitles
-                .attr( 'role', 'tab' )
-                .addClass( 'infobox-tab__title--displayed' );
-            this.tablinks.each( function() {
-                $( this ).attr( 'aria-controls', $( this ).data( 'aria-controls' ) );
-            });
+        if ( this.hasSidebar ) {
+            this.switchTo( openTab );
         } else {
-            this.tabtitlesMobile
-                .attr( 'role', 'tab' )
-                .addClass( 'infobox-tab__title--displayed' );
-            this.tablinksMobile.each( function() {
-                $( this ).attr( 'aria-controls', $( this ).data( 'aria-controls' ) );
-            });
             this.markHidden( this.tabpanels );
         }
 
-        // TODO switch to tab according to location.hash
-        this.switchTo( this.tablinks.first() );
-
-        this.tablinks.on( 'click', function( event ) {
+        this.infobox.on( 'click', '.infobox-tab__link', function( event ) {
+            var tab = $( this );
             event.preventDefault();
-            self.switchTo( $( this ) );
-            if ( self.isDesktop ) {
-                var id = $( this ).attr( 'id' );
+            self.switchTo( tab );
+            if ( self.hasSidebarNavigation() ) {
+                var id = tab.attr( 'id' );
                 if ( history.pushState ) {
                     history.pushState( null, null, '#' + id.substring( 0, id.length - 4 ) );
                 }
             }
         });
 
-        this.tablinksMobile.on( 'click', function( event ) {
-            event.preventDefault();
-            self.switchTo( $( this ) );
-        });
-
         $( window ).on( 'resize', function() {
-            if ( self.isDesktopView() === false ) {
-                self.tabtitlesMobile
-                    .attr( 'role', 'tab' )
-                    .addClass( 'infobox-tab__title--displayed' );
-                self.tabtitles
-                    .removeClass( 'infobox-tab__title--displayed' );
-            }
+            var openTabs = self.infobox.find( 'infobox-tab__link--active' ).first();
+            self.switchNavigationMode( openTab );
         });
+    };
 
+    InfoboxOO.prototype.hasSidebarNavigation = function() {
+        return this.navigation.is( ':visible' );
     };
 
     InfoboxOO.prototype.switchTo = function( selectedTab ) {
         var relatedPanelId = '#' + selectedTab.attr( 'aria-controls' ),
             relatedPanel = $( relatedPanelId );
 
-        if ( this.isDesktop ) {
-            this.currentTab.removeClass( 'infobox-tab__link--active' );
-            this.currentTab = selectedTab;
+        if ( this.hasSidebar ) {
+            this.tabs.removeClass( 'infobox-tab__link--active' );
             selectedTab.addClass( 'infobox-tab__link--active' );
 
             this.markHidden( this.tabpanels );
@@ -112,24 +89,70 @@
         });
     };
 
-    InfoboxOO.prototype.isDesktopView = function() {
-        return $( '#' + this.id + '-navigation', this.infobox ).is( ':visible' );
+    /**
+    * Switches the presentation of navigation according to the hasSidebar boolean
+    *
+    * @param {boolean} hasSidebar Wether to use sidebar navigation or if false the
+    *                  folded navigation.
+    * @param {jQuery} openTab Defines the panel that is opened after the switch
+    */
+    InfoboxOO.prototype.switchNavigationMode = function( openTab ) {
+        // Do not rebuild the navigation if not necessary
+        if ( this.hasSidebar === this.hasSidebarNavigation()) {
+            return;
+        }
+
+        this.hasSidebar = this.hasSidebarNavigation();
+        this.setTabsAndTabtitles();
+
+        if ( this.hasSidebar ) {
+            this.switchTo( openTab );
+        } else {
+            this.markHidden( this.tabpanels.not( openTab ) );
+        }
     };
 
-    InfoboxOO.prototype.getCurrentHash = function() {
+    /**
+    * Adds the necessary attributes to the tab navigation according to the
+    * navigation mode (sidebar or folded).
+    */
+    InfoboxOO.prototype.setTabsAndTabtitles = function() {
+
+        this.tabtitles
+            .removeAttr( 'role', 'tab' )
+            .removeClass( 'infobox-tab__title--displayed' );
+        this.tabs.removeAttr( 'aria-controls' );
+
+        if ( this.hasSidebar ) {
+            this.tabtitles = $( '.infobox__navigation .infobox-tab__title', this.infobox );
+        } else {
+            this.tabtitles = $( '.infobox-tab .infobox-tab__title', this.infobox );
+        }
+
+        this.tabtitles
+            .attr( 'role', 'tab' )
+            .addClass( 'infobox-tab__title--displayed' );
+
+        this.tabs = this.tabtitles.find( 'a' );
+        this.tabs.each( function() {
+            $( this ).attr( 'aria-controls', $( this ).data( 'aria-controls' ) );
+        });
+    };
+
+    /**
+    * Checks the URL for possible hashs that are related to tabs of this infobox.
+    *
+    * @return {jQuery} The tab related to the hash or undefined if no related
+    *                  tab has been found.
+    */
+    InfoboxOO.prototype.getSelectedTabByHash = function() {
         if ( window.location.hash ) {
             var hash = window.location.hash.substring( 1 );
-            $( '.infobox-tab', this.infobox ).each( function( index, element ) {
-                if ( element.id === hash ) {
-                    return element.id;
-                }
-            });
+            return this.tabs.filter( '#' + hash + '-tab' );
         }
-        return false;
     };
 
     $.fn.infoboxOO = function() {
-
         return this.each( function() {
             new InfoboxOO( this );
         });
