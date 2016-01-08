14b73db0e7267d3cbb3662d32b9915717a48b70b
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-01-08 10:40:35 +0100

    ZON-2575: Add test for disabled feature toggles

--- src/zeit/web/core/test/test_sources.py
+++ src/zeit/web/core/test/test_sources.py
@@ -4,4 +4,8 @@ import zeit.web.core.sources
 
 def test_feature_toggle_source_should_be_parsed(application):
     assert zeit.web.core.sources.FEATURE_TOGGLE_SOURCE.factory.find(
-        'third_party_modules') is True
+        'third_party_modules')
+    assert not zeit.web.core.sources.FEATURE_TOGGLE_SOURCE.factory.find(
+        'dummy')
+    assert not zeit.web.core.sources.FEATURE_TOGGLE_SOURCE.factory.find(
+        'nonexistent')
