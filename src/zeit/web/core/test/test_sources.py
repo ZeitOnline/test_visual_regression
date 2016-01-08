e0dd7661b6e467c7a3918b5d66ee7afb326bfc2e
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-01-08 11:34:16 +0100

    ZON-2575: Don't change third-party toggle value for tests (belongs to commit:702b267)

--- src/zeit/web/core/test/test_sources.py
+++ src/zeit/web/core/test/test_sources.py
@@ -2,7 +2,6 @@ import zeit.web.core.sources
 
 
 def test_feature_toggle_source_should_be_parsed(application):
-    assert zeit.web.core.sources.FEATURE_TOGGLE_SOURCE.find(
-        'third_party_modules')
+    assert zeit.web.core.sources.FEATURE_TOGGLE_SOURCE.find('article_lineage')
     assert not zeit.web.core.sources.FEATURE_TOGGLE_SOURCE.find('dummy')
     assert not zeit.web.core.sources.FEATURE_TOGGLE_SOURCE.find('nonexistent')
