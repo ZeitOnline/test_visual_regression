d331123959049c5bc6147c97cf56f41b41df61af
Ron Drongowski <ron.drongowski@gmail.com>
2016-01-18 17:59:10 +0100

    Add tests for navigation sources

--- src/zeit/web/core/test/test_sources.py
+++ src/zeit/web/core/test/test_sources.py
@@ -29,3 +29,29 @@ def test_igd_mobile_ids_source_should_be_parsed(application):
 def test_banner_id_mappings_source_should_be_parsed(application):
     assert len(
         zeit.web.core.sources.BANNER_ID_MAPPINGS_SOURCE.mapping_list) == 20
+
+
+def test_navigation_source_should_be_parsed(application):
+    navigation = zeit.web.core.sources.NAVIGATION_SOURCE
+    assert len(navigation.navigation.navigation_items) == 15
+    assert len(navigation.navigation_by_name.navigation_items) == 15
+
+
+def test_navigation_classifieds_source_should_be_parsed(application):
+    navigation = zeit.web.core.sources.NAVIGATION_CLASSIFIEDS_SOURCE
+    assert len(navigation.navigation.navigation_items) == 10
+
+
+def test_navigation_services_source_should_be_parsed(application):
+    navigation = zeit.web.core.sources.NAVIGATION_SERVICES_SOURCE
+    assert len(navigation.navigation.navigation_items) == 4
+
+
+def test_navigation_footer_publisher_source_should_be_parsed(application):
+    navigation = zeit.web.core.sources.NAVIGATION_FOOTER_PUBLISHER_SOURCE
+    assert len(navigation.navigation.navigation_items) == 3
+
+
+def test_navigation_footer_links_source_should_be_parsed(application):
+    navigation = zeit.web.core.sources.NAVIGATION_FOOTER_LINKS_SOURCE
+    assert len(navigation.navigation.navigation_items) == 2
