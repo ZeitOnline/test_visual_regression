d98460b3a4f9aa4ec0c7d237978bdb280a0dede6
Anika Szuppa <anika.szuppa@zeit.de>
2016-01-18 15:54:47 +0100

    ZON-2644: add cp extra stuff

--- src/zeit/web/magazin/module/buzzbox.py
+++ src/zeit/web/magazin/module/buzzbox.py
@@ -1,6 +1,19 @@
+import zope.component
 import zeit.web.site.module
 
 
 @zeit.web.register_module('zmo-buzzbox')
-class BuzzBox(zeit.web.site.module.Module):
-    pass
+class Buzzbox(zeit.web.site.module.Module):
+
+    def __init__(self, context):
+        super(Buzzbox, self).__init__(context)
+        self.layout = 'buzzbox'
+
+    @zeit.web.reify
+    def area_buzz(self):
+        conn = zope.component.getUtility(zeit.web.core.interfaces.IReach)
+        return (
+            ('views', conn.get_views(section='zeit-magazin')),
+            ('facebook', conn.get_social(
+                facet='facebook', section='zeit-magazin')),
+            ('comments', conn.get_comments(section='zeit-magazin')))
