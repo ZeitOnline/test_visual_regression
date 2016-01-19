09b85c3c10ac63bd96ff73ffda09a69e42b54a53
Nico Brünjes <nico.bruenjes@gmail.com>
2016-01-19 13:22:49 +0100

    MAINT: Fix test to be more defensive

--- src/zeit/web/site/test/test_view_centerpage.py
+++ src/zeit/web/site/test/test_view_centerpage.py
@@ -1749,7 +1749,7 @@ def test_imagecopyright_is_shown_on_click(selenium_driver, testserver):
             expected_conditions.presence_of_element_located(
                 (By.CLASS_NAME, 'image-copyright-footer')))
     except TimeoutException:
-        assert False, 'Image Copyright in Footer not visible within 10 seconds'
+        assert False, 'Image Copyright in Footer not visible within 5 seconds'
     else:
         copyrights = driver.find_elements_by_css_selector(
             '.image-copyright-footer__item')
@@ -1759,11 +1759,17 @@ def test_imagecopyright_is_shown_on_click(selenium_driver, testserver):
             '.image-copyright-footer__item a')
         assert len(linked_copyrights) == 1
 
-    closelink = driver.find_element_by_class_name(
-        'js-image-copyright-footer-close')
-    closelink.click()
-    copyright = driver.find_element_by_class_name('image-copyright-footer')
-    assert copyright.is_displayed() is False, 'copyright is not displayed'
+        closelink = driver.find_element_by_class_name(
+            'js-image-copyright-footer-close')
+        closelink.click()
+        try:
+            WebDriverWait(driver, 5).until(
+                expected_conditions.visibility_of_element_located(
+                    (By.CLASS_NAME, 'image-copyright-footer')))
+        except TimeoutException:
+            assert False, 'Image Copyright in Footer not hiddden within 5 seconds'
+        else:
+            assert True
 
 
 def test_zmo_teaser_kicker_should_contain_logo(testbrowser):
