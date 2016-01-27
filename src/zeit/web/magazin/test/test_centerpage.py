e38956ae90be4b780736cba4d494ee89e5e168bf
Anika Szuppa <anika.szuppa@zeit.de>
2016-01-27 14:52:31 +0100

    ZON-2665: change test

--- src/zeit/web/magazin/test/test_centerpage.py
+++ src/zeit/web/magazin/test/test_centerpage.py
@@ -22,7 +22,7 @@ import zeit.web.magazin.view_centerpage
 def test_cp_should_have_buzz_module(
         testserver, testbrowser):
     browser = testbrowser(
-        '%s/zeit-magazin/test-cp-2015/index' % testserver.url)
+        '%s/zeit-magazin/test-cp-2015/buzz' % testserver.url)
     assert '<section class="buzzboard">' in browser.contents
     assert '<table class="buzzboard__table' in browser.contents
     assert '<div class="buzzboard__container">' in browser.contents
@@ -37,21 +37,17 @@ def test_get_reaches_from_centerpage_view(application):
         'node_comment_statistics']
 
     cp = zeit.cms.interfaces.ICMSContent(
-        'http://xml.zeit.de/zeit-magazin/test-cp-2015/index')
+        'http://xml.zeit.de/zeit-magazin/test-cp-2015/buzz')
     block = zeit.web.core.utils.find_block(
-        cp, module='zmo-buzzbox')
+        cp, module='zmo-mostread')
     module = zeit.web.core.template.get_module(block)
+    buzz = module.reach
 
-    buzz = module.area_buzz
+    buzz_views = buzz.get_views(section='zeit-magazin')[1].score
+    buzz_facebook = buzz.get_social(facet='facebook', section='zeit-magazin')[1].score
+    buzz_comments = buzz.get_comments(section='zeit-magazin')[1].score
 
-    buzz_views = buzz[0]
-    buzz_facebook = buzz[1]
-    buzz_comments = buzz[2]
+    assert buzz_views == 69167
+    assert buzz_facebook == 408
+    assert buzz_comments == 461
 
-    assert buzz_views[0] == 'views'
-    assert buzz_facebook[0] == 'facebook'
-    assert buzz_comments[0] == 'comments'
-
-    assert len(buzz_views[1]) == 3
-    assert len(buzz_facebook[1]) == 3
-    assert len(buzz_comments[1]) == 3
