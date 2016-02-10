2abeec4f8d5bb45f7439714ff50b4fb17591c983
Nicolas Drebenstedt <cutoffthetop@me.com>
2016-02-10 13:00:42 +0100

    ZON-2507: Feature-toggle campus views

--- src/zeit/web/campus/view.py
+++ src/zeit/web/campus/view.py
@@ -1,9 +1,11 @@
+import zeit.web.core.application
 import zeit.web.core.view
 
 
 def is_zco_content(context, request):
     # XXX This is a mocked predicate until we have a zeit.campus interface
-    return context.uniqueId == 'http://xml.zeit.de/campus/index'
+    toggle = zeit.web.core.application.FEATURE_TOGGLES.find('campus_launch')
+    return toggle and context.uniqueId.startswith('http://xml.zeit.de/campus')
 
 
 class Base(zeit.web.core.view.Base):
