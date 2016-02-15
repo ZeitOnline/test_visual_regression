2d3f6ac29c917c6992e46796556f8c8c134ac379
Nicolas Drebenstedt <cutoffthetop@me.com>
2016-02-15 10:14:53 +0100

    Dedupe zco include, remove outdated comment

--- src/zeit/web/campus/view.py
+++ src/zeit/web/campus/view.py
@@ -5,7 +5,6 @@ import zeit.web.core.view
 
 
 def is_zco_content(context, request):
-    # XXX This is a mocked predicate until we have a zeit.campus interface
     toggle = zeit.web.core.application.FEATURE_TOGGLES.find('campus_launch')
     return toggle and zeit.campus.interfaces.IZCOContent.providedBy(context)
 
