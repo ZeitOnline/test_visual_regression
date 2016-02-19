ZeitOnline/test_visual_regression

commit 575dd7d74b08cc2956e2308e8c6c3d200d8e65cd

ZON-1612: Make CP modules to preserve on pagination configurable
Also, preserve the areas containing the module, too. This way we
support both `area(keep-module),area_ranking` and
`area(keep-module),area(keep-module),area_ranking`

src/zeit/web/site/view_centerpage.py
@@ -270,14 +270,20 @@ class CenterpagePage(Centerpage):
         regions = [LegacyRegion([zeit.web.core.centerpage.IRendered(
             self.area_ranking)])]
 
-        # Try to preserve centerpage header modules.
-        # XXX This could be a lot sleeker if done in config.
-        find = zeit.web.core.utils.find_block
-        header = find(values[0], module='headerimage') or find(
-            values[0], module='centerpage-header') or find(
-                values[0], module='search-form')
-        if header:
-            regions.insert(0, LegacyRegion([LegacyArea([header])]))
+        # We keep any areas of the first region that contain at least one kind
+        # of preserve-worthy module.
+        conf = zope.component.getUtility(zeit.web.core.interfaces.ISettings)
+        preserved_areas = []
+        for mod in conf.get('cp_preserve_modules_on_pagination', '').split():
+            module = zeit.web.core.utils.find_block(values[0], module=mod)
+            if module:
+                area = module.__parent__
+                if area not in preserved_areas:
+                    preserved_areas.append(
+                        zeit.web.core.centerpage.IRendered(area))
+
+        if preserved_areas:
+            regions.insert(0, LegacyRegion(preserved_areas))
 
         return regions
 

src/zeit/web/site/test/test_view_centerpage.py
@@ -2010,6 +2010,35 @@ def test_centerpage_page_should_be_reconstructed(application, dummy_request):
     assert view.regions[1].values()[0].kind == 'ranking'
 
 
+def test_centerpage_page_should_reconstruct_multiple_modules(
+        application, dummy_request):
+    dummy_request.GET['p'] = '2'
+    solr = zope.component.getUtility(zeit.solr.interfaces.ISolr)
+    solr.results = [{'uniqueId': 'http://zeit.de/%s' % i} for i in range(15)]
+    cp = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/dynamic/umbrien')
+    mod = cp.body.values()[0].values()[0].create_item('cpextra')
+    mod.cpextra = 'search-form'
+    view = zeit.web.site.view_centerpage.CenterpagePage(cp, dummy_request)
+    assert len(view.regions) == 2
+    assert view.regions[0].values()[0].values()[0].supertitle == 'Griechenland'
+    assert view.regions[0].values()[0].values()[1].cpextra == 'search-form'
+
+
+def test_centerpage_page_should_reconstruct_multiple_areas(
+        application, dummy_request):
+    dummy_request.GET['p'] = '3'
+    solr = zope.component.getUtility(zeit.solr.interfaces.ISolr)
+    solr.results = [{'uniqueId': 'http://zeit.de/%s' % i} for i in range(35)]
+    cp = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/dynamic/umbrien')
+    area = cp.body.values()[0].create_item('area', 1)
+    mod = area.create_item('cpextra')
+    mod.cpextra = 'search-form'
+    view = zeit.web.site.view_centerpage.CenterpagePage(cp, dummy_request)
+    assert len(view.regions) == 2
+    assert view.regions[0].values()[0].values()[0].supertitle == 'Griechenland'
+    assert view.regions[0].values()[1].values()[0].cpextra == 'search-form'
+
+
 def test_centerpage_page_should_require_ranking(application, dummy_request):
     cp = zeit.cms.interfaces.ICMSContent(
         'http://xml.zeit.de/zeit-online/slenderized-index')

src/zeit/web/core/data/config/zeitweb-settings.xml
@@ -16,6 +16,8 @@
 
   <setting name="reach_timeout" xsi:type="xsd:double">0.2</setting>
 
+  <setting name="cp_preserve_modules_on_pagination" xsi:type="xsd:string">headerimage centerpage-header search-form authorlist-alphabet-pager</setting>
+
   <setting name="caching_time_content" xsi:type="xsd:int">5</setting>
   <setting name="caching_time_article" xsi:type="xsd:int">10</setting>
   <setting name="caching_time_centerpage" xsi:type="xsd:int">20</setting>

commit addb517b3034aaea44370e81d2451b7814218f17

HOTFIX: Re-enable ranking area to hide curated content (was broken in commit:575dd7d)
When the page is rendered, view.area_ranking is called _first_, so if
that calls super().regions, _that_ result is reified, and the
overridden regions() definition that hides the curated stuff never runs.

src/zeit/web/site/view_centerpage.py
@@ -282,7 +282,9 @@ class CenterpagePage(Centerpage):
     def area_ranking(self):
         # Prevent infloop with our tweaked self.regions
         # XXX Is there a better factoring than copy&paste?
-        for region in super(CenterpagePage, self).regions:
+        regions = [zeit.web.core.centerpage.IRendered(x)
+                   for x in self.context.values()]
+        for region in regions:
             for area in region.values():
                 if zeit.web.core.interfaces.IPagination.providedBy(area):
                     return area

src/zeit/web/site/test/test_view_centerpage.py
@@ -2057,6 +2057,16 @@ def test_centerpage_page_should_require_ranking(application, dummy_request):
         list(view.regions)
 
 
+def test_centerpage_page_integration(testbrowser, datasolr):
+    browser = testbrowser('/dynamic/umbrien?p=2')
+    # Curated content is not shown
+    assert 'Ich bin nicht intellektuell' not in browser.contents
+    # Header is kept
+    assert 'Das linke Experiment' in browser.contents
+    # Ranking is kept
+    assert 'cp-area--ranking' in browser.contents
+
+
 def test_ranking_area_should_determine_uids_above(application, dummy_request):
     cp = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de/dynamic/umbrien')
     context = zeit.web.core.utils.find_block(cp, attrib='area', kind='ranking')