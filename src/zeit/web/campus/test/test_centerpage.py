b0d9f7e607ec1787f8ecc2a9bc5059ba4846ca82
Dominik Hoppe <dominik.hoppe@zeit.de>
2016-02-24 14:01:21 +0100

    ZON-2834: Provide test for article topiclinks

--- src/zeit/web/campus/test/test_centerpage.py
+++ src/zeit/web/campus/test/test_centerpage.py
@@ -10,7 +10,15 @@ import zeit.web.core.interfaces
 
 def test_campus_centerpage_should_produce_regular_topiclinks(application):
     context = zeit.cms.interfaces.ICMSContent(
-        'http://xml.zeit.de/campus/centerpage/topiclinks')
+        'http://xml.zeit.de/campus/index')
+    topiclink = zeit.web.core.interfaces.ITopicLink(context)
+    labels = sorted(dict(topiclink).keys())
+    assert labels == ['Ausdauersport', 'Bayerische Landesbank', 'Paul Auster']
+
+
+def test_campus_article_should_import_topiclinks(application):
+    context = zeit.cms.interfaces.ICMSContent(
+        'http://xml.zeit.de/campus/article/simple')
     topiclink = zeit.web.core.interfaces.ITopicLink(context)
     labels = sorted(dict(topiclink).keys())
     assert labels == ['Ausdauersport', 'Bayerische Landesbank', 'Paul Auster']
