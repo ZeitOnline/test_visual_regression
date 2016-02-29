ddd3d3ce8b15bfa5f33836e336ea396527300747
Thomas Puppe <thomas.puppe@zeit.de>
2016-02-29 14:28:10 +0100

    ZON-2496: move ZCO test data ...
    ... according to https://github.com/ZeitOnline/zeit.web/wiki/Testdaten-Guidelines

--- src/zeit/web/campus/test/test_centerpage.py
+++ src/zeit/web/campus/test/test_centerpage.py
@@ -10,7 +10,7 @@ import zeit.web.core.interfaces
 
 def test_campus_centerpage_should_produce_regular_topiclinks(application):
     context = zeit.cms.interfaces.ICMSContent(
-        'http://xml.zeit.de/campus/centerpage')
+        'http://xml.zeit.de/campus/centerpage/index')
     topiclink = zeit.web.core.interfaces.ITopicLink(context)
     labels = sorted(dict(topiclink).keys())
     assert labels == ['Ausdauersport', 'Bayerische Landesbank', 'Paul Auster']
