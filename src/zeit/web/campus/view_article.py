ea008f7dd98183d51cddef574fb537fda4b32e8a
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2016-03-01 12:13:35 +0100

    ZON-2834: Add Komplettansicht

--- src/zeit/web/campus/view_article.py
+++ src/zeit/web/campus/view_article.py
@@ -6,10 +6,13 @@ import zeit.web.campus.view
 import zeit.web.core.view_article
 
 
-@pyramid.view.view_config(
+@pyramid.view.view_defaults(
     context=zeit.content.article.interfaces.IArticle,
     custom_predicates=(zeit.web.campus.view.is_zco_content,),
-    renderer='templates/article.html')
+    request_method='GET')
+@pyramid.view.view_config(renderer='templates/article.html')
+@pyramid.view.view_config(name='komplettansicht',
+                          renderer='templates/komplett.html')
 class Article(
         zeit.web.core.view_article.Article, zeit.web.campus.view.Base):
     pass
