d0e36cd0a801ad98846747fa9e565a320e77aea1
Nicolas Drebenstedt <cutoffthetop@me.com>
2016-03-09 11:36:23 +0100

    MAINT: Replace purpose-bound with fixed-sized wide variant

--- src/zeit/web/site/view_feed.py
+++ src/zeit/web/site/view_feed.py
@@ -336,18 +336,19 @@ class SpektrumFeed(Base):
                 E.guid(content.uniqueId, isPermaLink='false'),
             )
             variant = zeit.web.core.template.get_image(
-                content=content, variant_id='rss-spektrum-flavoured',
-                fallback=False)
-            image = zeit.content.image.interfaces.IMasterImage(
-                variant.group, None)
-            if variant is not None and image is not None:
-                item.append(E.enclosure(
-                    url=self.request.image_host + variant.fallback_path,
-                    # XXX Incorrect length, since bitblt will resize the
-                    # image, but since that happens outside of the
-                    # application, we cannot know the real size here.
-                    length=str(image.size),
-                    type=image.mimeType))
+                content=content, variant_id='wide', fallback=False)
+            if variant is not None:
+                image = zeit.content.image.interfaces.IMasterImage(
+                    variant.group, None)
+                if image is not None:
+                    item.append(E.enclosure(
+                        url='{}{}__220x124'.format(
+                            self.request.image_host, variant.path),
+                        # XXX Incorrect length, since bitblt will resize the
+                        # image, but since that happens outside of the
+                        # application, we cannot know the real size here.
+                        length=str(image.size),
+                        type=image.mimeType))
             channel.append(item)
         return root
 
