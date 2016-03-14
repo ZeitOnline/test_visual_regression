f82cdf872d9d30bcc00896a152102f502f0d063a
Thomas Puppe <thomas.puppe@zeit.de>
2016-03-14 11:33:55 +0100

    ZON-2496: add tests for ZCO CP
    new branch, because old one was messed up

--- src/zeit/web/campus/test/test_view_centerpage.py
+++ src/zeit/web/campus/test/test_view_centerpage.py
@@ -1,3 +1,4 @@
+import re
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
@@ -35,7 +36,7 @@ def test_campus_navigation_should_present_flyout(selenium_driver, testserver):
             assert True
 
 
-def test_teaser_wide_small_should_not_display_its_image_on_mobile(
+def test_campus_teaser_wide_small_should_not_display_its_image_on_mobile(
         selenium_driver, testserver):
     driver = selenium_driver
     driver.set_window_size(320, 480)
@@ -46,3 +47,51 @@ def test_teaser_wide_small_should_not_display_its_image_on_mobile(
 
     driver.set_window_size(768, 800)
     assert teaser_image.is_displayed()
+
+
+def test_campus_teaser_wide_small_has_correct_structure(testbrowser):
+    select = testbrowser('/campus/centerpage/teaser-wide-small').cssselect
+
+    assert len(select('.teaser-wide-small')) == 3
+    assert len(select('.teaser-wide-small__metadata')) == 3
+    assert len(select('.teaser-wide-small__byline')) == 2
+    assert len(select('.teaser-wide-small__content')) == 0
+
+    byline = select('.teaser-wide-small__byline')[1]
+    byline_text = re.sub(' +', ' ', byline.text.strip())
+    assert byline_text == 'Von Viola Diem'
+
+
+def test_campus_teaser_wide_large_has_correct_structure(testbrowser):
+    select = testbrowser('/campus/centerpage/teaser-wide-large').cssselect
+
+    assert len(select('.teaser-wide-large')) == 2
+
+    assert len(select('.teaser-wide-large .teaser-wide-large__heading \
+        .teaser-wide-large__kicker')) == 2
+    assert len(select('.teaser-wide-large .teaser-wide-large__heading \
+        .teaser-wide-large__title')) == 2
+
+    assert len(select('.teaser-wide-large__metadata')) == 2
+    assert len(select('.teaser-wide-large__byline')) == 1
+    assert len(select('.teaser-wide-large__content')) == 0
+
+
+def test_campus_teaser_square_exists(testbrowser):
+    select = testbrowser('/campus/centerpage/teaser-square').cssselect
+    assert len(select('.teaser-square')) == 4
+
+
+def test_campus_teaser_lead_portrait_has_correct_structure(testbrowser):
+    select = testbrowser('/campus/centerpage/teaser-lead-portrait').cssselect
+    assert len(select('.teaser-lead-portrait')) == 1
+    assert len(select('.teaser-lead-portrait .teaser-lead-portrait__content \
+        .teaser-lead-portrait__heading .teaser-lead-portrait__title')) == 1
+    assert len(select('.teaser-lead-portrait__metadata')) == 1
+
+
+def test_campus_teaser_lead_cinema_has_correct_structure(testbrowser):
+    select = testbrowser('/campus/centerpage/teaser-lead-cinema').cssselect
+    assert len(select('.teaser-lead-cinema')) == 1
+    assert len(select('.teaser-lead-cinema__content')) == 0
+    assert len(select('.teaser-lead-cinema__metadata')) == 1
