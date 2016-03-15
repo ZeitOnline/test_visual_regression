8c7c3e87bc5de6a730da0e8b6c67b0430ebdbcca
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2016-03-15 17:52:43 +0100

    ZON-2777: Update test

--- src/zeit/web/campus/test/test_view_gallery.py
+++ src/zeit/web/campus/test/test_view_gallery.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 
-def test_gallery_renders(testbrowser):
-    select = testbrowser('/campus/gallery/gallery').cssselect
-    assert len(select('.summary, .byline, .metadata')) == 3
+def test_gallery_contains_basic_elements(testbrowser):
+    browser = testbrowser('/campus/gallery/gallery')
+    gallery = browser.cssselect('.gallery')[0]
+    assert len(browser.cssselect('.article-header__byline'))
+    assert len(browser.cssselect('.article-header__metadata'))
+    assert len(browser.cssselect('.article__summary'))
+    assert len(browser.cssselect('.gallery__description'))
+    assert len(gallery.cssselect('.slides')) == 13
