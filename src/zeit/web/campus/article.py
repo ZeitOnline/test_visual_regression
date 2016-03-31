d7af36adaf38bccb7d36fef2eeaca43858e40f39
Anika Szuppa <anika.szuppa@zeit.de>
2016-03-31 16:14:35 +0200

    ZON-2776: delete mismatched file

--- src/zeit/web/campus/article.py
+++
@@ -1,9 +0,0 @@
-import zeit.content.article
-import zope.interface
-
-
-@zope.interface.implementer(zeit.web.core.interfaces.IPage)
-class Page(zeit.web.core.article.Page):
-
-    def get_content_ad(self):
-        return 'stoas-artikelanker'
