460065292a3fb0c2244d0c27cef8f8e386920ff1
Thomas Puppe <thomas.puppe@zeit.de>
2016-04-06 13:42:04 +0200

    ZON-2494: Limit search of paragraphs to page

--- javascript/web.core/clicktracking.js
+++ javascript/web.core/clicktracking.js
@@ -134,7 +134,7 @@ define( [ 'jquery', 'web.core/zeit' ], function( $, Zeit ) {
          * @return {string}          formatted linkId-string for webtrekk call
          */
         linkInArticleContent: function( $element, $page ) {
-            var $currentParagraph =  $element.closest( 'p, [data-clicktracking]' ),
+            var $currentParagraph =  $element.closest( 'p, [data-clicktracking]', $page ),
                 currentPageNumber = $page.data( 'page-number' ) || 0,
                 currentParagraphNumber = $currentParagraph.prevAll( 'p' ).length + 1,
                 trackType = $element.closest( '[data-clicktracking]' ).data( 'clicktracking' ) || 'intext',
