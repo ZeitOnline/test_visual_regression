657951ba806ff33ddb96641c5303bacb5518ec34
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-04-11 20:14:08 +0200

    Preparing release 2.71.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='zeit.web',
     url='https://github.com/ZeitOnline/zeit.web',
-    version='2.72.dev0',
+    version='2.71.1',
     author=(
         'Thomas Baumann, Nico Bruenjes, Nicolas Drebenstedt, Ron Drongowski, '
         'Dominik Hoppe, Marco Kaiser, Harry Keller, Tom Lazar, Thomas Lotze, '
