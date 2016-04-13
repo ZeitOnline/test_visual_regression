e9a0096bb0a32a75dee040c6da31c1a1e4585759
Anika Szuppa <anika.szuppa@zeit.de>
2016-04-13 16:04:30 +0200

    ZON-2920: introduce font-sizes variables

--- Gruntfile.js
+++ Gruntfile.js
@@ -83,7 +83,6 @@ module.exports = function(grunt) {
                 options: {
                     specify: [
                         project.sourceDir + 'sass/**/*.s{a,c}ss',
-                        '!' + project.sourceDir + 'sass/**/advertorial.*',
                         '!' + project.sourceDir + 'sass/**/unresponsive.*',
                         '!' + project.sourceDir + 'sass/**/all-old-ie.*',
                         '!' + project.sourceDir + 'sass/**/ie-navi.*'
