94b1bc3c28f5c30df06fe65ac83ec6e7e336511c
Thomas Puppe <thomas.puppe@zeit.de>
2016-04-26 14:39:46 +0200

    ZON-2997: Fix after faulty merge

--- src/zeit/web/campus/view_centerpage.py
+++ src/zeit/web/campus/view_centerpage.py
@@ -24,3 +24,12 @@ class Centerpage(
             return 'index'
         else:
             return 'centerpage'
+
+
+@pyramid.view.view_config(
+    context=zeit.content.cp.interfaces.ICP2015,
+    custom_predicates=(zeit.web.campus.view.is_zco_content,
+                       zeit.web.core.view.is_paginated),
+    renderer='templates/centerpage.html')
+class CenterpagePage(zeit.web.core.view_centerpage.CenterpagePage, Centerpage):
+    pass
