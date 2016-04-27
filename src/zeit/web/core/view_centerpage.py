a4ef5f33ed8e49f120f85166dbd161568e154195
Dominik Hoppe <dominik.hoppe@zeit.de>
2016-04-27 11:36:31 +0200

    ZON-2504: Fix templates to make cardstack work again after merge

--- src/zeit/web/core/view_centerpage.py
+++ src/zeit/web/core/view_centerpage.py
@@ -1,10 +1,12 @@
 import pyramid.view
 import lxml.etree
+import zope.component
 
 import zeit.cms.interfaces
 import zeit.cms.workflow
 import zeit.content.cp.interfaces
 
+import zeit.web.core.centerpage
 import zeit.web.core.view
 
 
@@ -46,6 +48,36 @@ class Centerpage(zeit.web.core.view.CeleraOneMixin, zeit.web.core.view.Base):
         return url + param_str
 
     @zeit.web.reify
+    def area_ranking(self):
+        for region in self.regions:
+            for area in region.values():
+                if zeit.web.core.interfaces.IPagination.providedBy(area):
+                    return area
+        return None
+
+    @zeit.web.reify
+    def next_page_url(self):
+        ranking = self.area_ranking
+        if ranking is None:
+            return None
+        if ranking.current_page < len(ranking.pagination):
+            return zeit.web.core.template.append_get_params(
+                self.request, p=ranking.current_page + 1)
+
+    @zeit.web.reify
+    def prev_page_url(self):
+        ranking = self.area_ranking
+        if ranking is None:
+            return None
+        # suppress page param for page 1
+        if ranking.current_page == 2:
+            return zeit.web.core.template.remove_get_params(
+                self.request.url, 'p')
+        elif ranking.current_page > 2:
+            return zeit.web.core.template.append_get_params(
+                self.request, p=ranking.current_page - 1)
+
+    @zeit.web.reify
     def is_hp(self):
         return self.context.type == 'homepage'
 
@@ -85,6 +117,53 @@ class Centerpage(zeit.web.core.view.CeleraOneMixin, zeit.web.core.view.Base):
     def cardstack_body(self):
         url = super(Centerpage, self).cardstack_body
         return zeit.web.core.utils.update_query(url, static='true')
+
+
+class CenterpagePage(object):
+
+    @zeit.web.reify
+    def regions(self):
+        if self.area_ranking is None:
+            # A paginatable centerpage needs a ranking area.
+            raise pyramid.httpexceptions.HTTPNotFound(
+                'This centerpage is not paginatable.')
+
+        values = self.context.values()
+        if len(values) == 0:
+            return []
+
+        # Reconstruct a paginated cp with optional header and ranking area.
+        regions = [zeit.web.core.centerpage.Region(
+            [zeit.web.core.centerpage.IRendered(self.area_ranking)])]
+
+        # We keep any areas of the first region that contain at least one kind
+        # of preserve-worthy module.
+        conf = zope.component.getUtility(zeit.web.core.interfaces.ISettings)
+        preserved_areas = []
+        for mod in conf.get('cp_preserve_modules_on_pagination', '').split():
+            module = zeit.web.core.utils.find_block(values[0], module=mod)
+            if module:
+                area = module.__parent__
+                if area not in preserved_areas:
+                    preserved_areas.append(
+                        zeit.web.core.centerpage.IRendered(area))
+
+        if preserved_areas:
+            regions.insert(0, zeit.web.core.centerpage.Region(preserved_areas))
+
+        return regions
+
+    @zeit.web.reify
+    def area_ranking(self):
+        # Prevent infloop with our tweaked self.regions
+        # XXX Is there a better factoring than copy&paste?
+        regions = [zeit.web.core.centerpage.IRendered(x)
+                   for x in self.context.values()]
+        for region in regions:
+            for area in region.values():
+                if zeit.web.core.interfaces.IPagination.providedBy(area):
+                    return area
+        return None
 
 
 @pyramid.view.view_config(
--- src/zeit/web/site/view_centerpage.py
+++ src/zeit/web/site/view_centerpage.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-import collections
 import logging
-import uuid
 
 import grokcore.component
 import lxml.etree
@@ -14,6 +12,7 @@ import zeit.cms.interfaces
 import zeit.content.cp.area
 import zeit.content.cp.interfaces
 
+from zeit.web.core.centerpage import Region, Area
 import zeit.web.core.centerpage
 import zeit.web.core.interfaces
 import zeit.web.core.navigation
@@ -27,102 +26,6 @@ import zeit.web.site.module.printbox
 
 
 log = logging.getLogger(__name__)
-
-
-@zope.interface.implementer(zeit.web.core.interfaces.IBlock)
-class LegacyModule(
-        zeit.web.core.centerpage.Module, zeit.web.core.utils.nslist):
-
-    def __init__(self, arg, **kw):
-        zeit.web.core.utils.nslist.__init__(self, [v for v in arg if v])
-        self.layout = kw.pop('layout', 'default')
-        self.type = kw.pop('type', 'teaser')
-        self.__parent = kw.pop('parent', None)
-
-    def __hash__(self):
-        if getattr(self.layout, 'id', None):
-            return hash((self.layout.id, id(self)))
-        else:
-            raise NotImplementedError()
-
-    def __repr__(self):
-        return object.__repr__(self)
-
-
-@zope.interface.implementer(zeit.content.cp.interfaces.IArea)
-class LegacyArea(collections.OrderedDict, zeit.content.cp.area.AreaFactory):
-
-    def __init__(self, arg, **kw):
-        collections.OrderedDict.__init__(
-            self, [('id-{}'.format(uuid.uuid1()), v) for v in arg if v])
-        self.kind = kw.pop('kind', 'solo')
-        self.xml = kw.pop('xml', self.get_xml())
-        self.automatic = kw.pop('automatic', False)
-        self.__parent__ = kw.pop('parent', None)
-
-        for key in kw:
-            try:
-                assert not hasattr(self, key)
-                setattr(self, key, kw[key])
-            except:
-                continue
-
-    def append(self, value):
-        self['id-{}'.format(uuid.uuid1())] = value
-
-    def __hash__(self):
-        if self.kind:
-            return hash((self.kind, id(self)))
-        else:
-            raise NotImplementedError()
-
-    def __repr__(self):
-        return object.__repr__(self)
-
-    # XXX This is really crude, but since these Legacy-classes throw away
-    # their vivi-side objects, we can't get to the interfaces anymore,
-    # so a full re-implementation is just plain impossible.
-    # So we hard-code the only use-case that this should ever be called for,
-    # which is ITeaseredContent.
-    def select_modules(self, *interfaces):
-        for module in zeit.content.cp.interfaces.IRenderedArea(self).values():
-            if getattr(module, 'type', None) == 'teaser':
-                yield module
-
-
-@zope.interface.implementer(zeit.content.cp.interfaces.IRegion)
-class LegacyRegion(LegacyArea, zeit.content.cp.area.RegionFactory):
-
-    def __init__(self, arg, **kw):
-        LegacyArea.__init__(self, arg, **kw)
-
-
-@grokcore.component.adapter(
-    zeit.content.cp.interfaces.IArea,
-    zeit.content.cp.interfaces.ITeaserBlock)
-@grokcore.component.implementer(
-    zeit.content.cp.interfaces.IRenderedArea)
-class RenderedLegacyArea(LegacyArea):
-
-    def __init__(self, area, block):
-        area.count = int(block.xml.get('display_amount', 3))
-        uid = unicode(block.xml.find('./referenced_cp'))
-        area.referenced_cp = zeit.cms.interfaces.ICMSContent(uid, None)
-        auto = zeit.content.cp.interfaces.IRenderedArea(area)
-        # XXX We really should call auto.values() here instead of private API.
-        auto._v_try_to_retrieve_content = True
-        auto._v_retrieved_content = 0
-        values = auto._query_centerpage()[:area.count]
-
-        lids = [block.layout.id] + area.count * ['zon-parquet-small']
-        modules = [LegacyModule(
-            [t], layout=lids.pop(0), parent=self) for t in values]
-        LegacyArea.__init__(self, modules, kind='parquet', is_teaserbar=True)
-
-        self.read_more = block.read_more
-        self.read_more_url = block.read_more_url
-        self.title = block.title
-        self.referenced_cp = area.referenced_cp
 
 
 @pyramid.view.view_config(
@@ -239,51 +142,8 @@ class Centerpage(
     custom_predicates=(zeit.web.site.view.is_zon_content,
                        zeit.web.core.view.is_paginated),
     renderer='templates/centerpage.html')
-class CenterpagePage(Centerpage):
-
-    @zeit.web.reify
-    def regions(self):
-        if self.area_ranking is None:
-            # A paginatable centerpage needs a ranking area.
-            raise pyramid.httpexceptions.HTTPNotFound(
-                'This centerpage is not paginatable.')
-
-        values = self.context.values()
-        if len(values) == 0:
-            return []
-
-        # Reconstruct a paginated cp with optional header and ranking area.
-        regions = [LegacyRegion([zeit.web.core.centerpage.IRendered(
-            self.area_ranking)])]
-
-        # We keep any areas of the first region that contain at least one kind
-        # of preserve-worthy module.
-        conf = zope.component.getUtility(zeit.web.core.interfaces.ISettings)
-        preserved_areas = []
-        for mod in conf.get('cp_preserve_modules_on_pagination', '').split():
-            module = zeit.web.core.utils.find_block(values[0], module=mod)
-            if module:
-                area = module.__parent__
-                if area not in preserved_areas:
-                    preserved_areas.append(
-                        zeit.web.core.centerpage.IRendered(area))
-
-        if preserved_areas:
-            regions.insert(0, LegacyRegion(preserved_areas))
-
-        return regions
-
-    @zeit.web.reify
-    def area_ranking(self):
-        # Prevent infloop with our tweaked self.regions
-        # XXX Is there a better factoring than copy&paste?
-        regions = [zeit.web.core.centerpage.IRendered(x)
-                   for x in self.context.values()]
-        for region in regions:
-            for area in region.values():
-                if zeit.web.core.interfaces.IPagination.providedBy(area):
-                    return area
-        return None
+class CenterpagePage(zeit.web.core.view_centerpage.CenterpagePage, Centerpage):
+    pass
 
 
 @pyramid.view.view_config(
@@ -404,11 +264,10 @@ class LegacyCenterpage(Centerpage):
         regions = []
 
         if(len(self.area_solo.values()) > 0):
-            region_fullwidth = LegacyRegion([self.area_solo])
+            region_fullwidth = Region([self.area_solo])
             regions.append(region_fullwidth)
 
-        region_multi = LegacyRegion([self.area_major, self.area_minor],
-                                    kind='multi')
+        region_multi = Region([self.area_major, self.area_minor], kind='multi')
         regions.append(region_multi)
 
         regions += self.region_list_parquet
@@ -424,7 +283,7 @@ class LegacyCenterpage(Centerpage):
                 'zon-fullwidth')
 
         area = self.context.values()[0]['lead']
-        return LegacyArea([m for m in area.itervalues() if valid_module(m)])
+        return Area([m for m in area.itervalues() if valid_module(m)])
 
     @zeit.web.reify
     def area_major(self):
@@ -435,15 +294,16 @@ class LegacyCenterpage(Centerpage):
                 'zon-fullwidth', 'hide')
 
         area = self.context.values()[0]['lead']
-        return LegacyArea([m for m in area.itervalues() if valid_module(m)],
-                          kind='major')
+        return Area([m for m in area.itervalues() if valid_module(m)],
+                    kind='major')
 
     @zeit.web.reify
     def area_minor(self):
         """Return an automated informatives-style area with buzz and ads."""
 
-        return LegacyArea([m for m in (self.module_buzz_mostread,
-                           self.module_printbox) if m], kind='minor')
+        return Area([m for m in (
+            self.module_buzz_mostread, self.module_printbox) if m],
+            kind='minor')
 
     @zeit.web.reify
     def module_buzz_mostread(self):
@@ -477,6 +337,35 @@ class LegacyCenterpage(Centerpage):
                 except (zope.component.interfaces.ComponentLookupError,
                         AttributeError, TypeError):
                     continue
-                regions.append(LegacyRegion([legacy], kind='parquet'))
+                regions.append(Region([legacy], kind='parquet'))
 
         return regions
+
+
+@grokcore.component.adapter(
+    zeit.content.cp.interfaces.IArea,
+    zeit.content.cp.interfaces.ITeaserBlock)
+@grokcore.component.implementer(
+    zeit.content.cp.interfaces.IRenderedArea)
+class RenderedLegacyArea(zeit.web.core.centerpage.Area):
+
+    def __init__(self, area, block):
+        area.count = int(block.xml.get('display_amount', 3))
+        uid = unicode(block.xml.find('./referenced_cp'))
+        area.referenced_cp = zeit.cms.interfaces.ICMSContent(uid, None)
+        auto = zeit.content.cp.interfaces.IRenderedArea(area)
+        # XXX We really should call auto.values() here instead of private API.
+        auto._v_try_to_retrieve_content = True
+        auto._v_retrieved_content = 0
+        values = auto._query_centerpage()[:area.count]
+
+        lids = [block.layout.id] + area.count * ['zon-parquet-small']
+        modules = [zeit.web.core.centerpage.TeaserModule(
+            [t], layout=lids.pop(0), parent=self) for t in values]
+        super(RenderedLegacyArea, self).__init__(
+            modules, kind='parquet', is_teaserbar=True)
+
+        self.read_more = block.read_more
+        self.read_more_url = block.read_more_url
+        self.title = block.title
+        self.referenced_cp = area.referenced_cp
