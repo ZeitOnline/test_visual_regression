dd3d8a396abc24b46d3f24a706b69e35a124fb4d
Anika Szuppa <anika.szuppa@zeit.de>
2016-05-02 14:25:58 +0200

    MAINT: prepare conf

--- src/zeit/web/campus/module/toolbox.py
+++ src/zeit/web/campus/module/toolbox.py
@@ -8,3 +8,22 @@ class Toolbox(zeit.web.core.centerpage.Module):
     def __init__(self, context):
         super(Toolbox, self).__init__(context)
         self.layout = 'toolbox'
+
+        services = zeit.web.campus.module.toolbox.ToolSource(
+            'centerpage-toolbox-source')
+
+
+class ToolSource(zeit.cms.content.sources.SimpleXMLSourceBase):
+
+    product_configuration = 'zeit.web'
+
+    def __init__(self, config_url):
+        self.config_url = config_url
+
+    def __iter__(self):
+        tree = self._get_tree()
+        for column in tree.iterchildren('column'):
+            yield [Service(unicode(service.get('id')),
+                           unicode(service.text.strip()),
+                           unicode(service.get('href')))
+                   for service in column.iterchildren('*')]
