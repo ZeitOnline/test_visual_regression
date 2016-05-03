9a35bb14b0ca45539945aae0c0f302e560784be1
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-05-03 09:13:01 +0200

    Try harder to make solr HTML error pages consumable
    See https://bugsnag.com/zeit-online-gmbh/friedbert/errors/57274ea09f57ff6cb498a1e2

--- src/zeit/web/core/application.py
+++ src/zeit/web/core/application.py
@@ -511,8 +511,10 @@ requests.sessions.get_netrc_auth = lambda *args, **kw: None
 # Allow pysolr error handling to deal with non-ascii HTML error pages,
 # see <https://bugs.python.org/issue11033>.
 def scrape_response_nonascii(self, headers, response):
+    if isinstance(response, str):
+        response = response.decode('utf-8')
     if isinstance(response, unicode):
-        response = response.encode('utf-8')
+        response = response.encode('ascii', errors='ignore')
     return original_scrape_response(self, headers, response)
 original_scrape_response = pysolr.Solr._scrape_response
 pysolr.Solr._scrape_response = scrape_response_nonascii
