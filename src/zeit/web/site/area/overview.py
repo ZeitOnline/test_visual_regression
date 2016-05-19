5b44f6a2c1a1340b3994682317a95cbb581345ac
Ron Drongowski <ron.drongowski@gmail.com>
2016-05-19 18:04:26 +0200

    Make number of news pages configurable

--- src/zeit/web/site/area/overview.py
+++ src/zeit/web/site/area/overview.py
@@ -3,6 +3,7 @@ import copy
 import datetime
 import dateutil
 import logging
+import zope.component
 
 import zeit.solr.query
 
@@ -13,7 +14,7 @@ import zeit.web.core.area.ranking
 log = logging.getLogger(__name__)
 
 
-SANITY_BOUND = 10
+SANITY_BOUND = 500
 
 
 @zeit.web.register_area('overview')
@@ -21,6 +22,10 @@ class Overview(zeit.web.core.area.ranking.Ranking):
 
     count = SANITY_BOUND
     sort_order = 'publikation'
+
+    @zeit.web.reify
+    def start(self):
+        return 0
 
     @staticmethod
     def clone_factory(jango, count):
@@ -51,16 +56,27 @@ class Overview(zeit.web.core.area.ranking.Ranking):
             datetime.datetime.combine(tomorrow, datetime.time()))
         query = zeit.find.search.query(filter_terms=[
             zeit.solr.query.field_raw('published', 'published*')])
+
         return zeit.solr.query.and_(query, range_)
 
     @zeit.web.reify
-    def pagination_info(self):
-        today = today = datetime.datetime.today().replace(
+    def total_pages(self):
+        conf = zope.component.getUtility(zeit.web.core.interfaces.ISettings)
+        if conf.get('total_news_pages'):
+            return int(conf.get('total_news_pages'))
+        else:
+            return super(Overview, self).total_pages
+
+    @zeit.web.reify
+    def _today(self):
+        return datetime.datetime.today().replace(
             hour=0, minute=0, second=0, microsecond=0)
 
-        previous_day = (today+datetime.timedelta(
+    @zeit.web.reify
+    def pagination_info(self):
+        previous_day = (self._today+datetime.timedelta(
             days=self.current_page)).strftime("%Y-%m-%d")
-        next_day = (today-datetime.timedelta(
+        next_day = (self._today-datetime.timedelta(
             days=self.current_page)).strftime("%Y-%m-%d")
         return {
             'previous_label': u'Vorheriger Tag',
@@ -71,10 +87,7 @@ class Overview(zeit.web.core.area.ranking.Ranking):
     def page_info(self, page_nr):
         if not page_nr:
             page_nr = 1
-        today = today = datetime.datetime.today().replace(
-            hour=0, minute=0, second=0, microsecond=0)
-
-        date = today - datetime.timedelta(days=(page_nr-1))
+        date = self._today - datetime.timedelta(days=(page_nr-1))
 
         date_label = date.strftime("%d.%m")
         date_param = date.strftime("%Y-%m-%d")
@@ -92,6 +105,4 @@ class Overview(zeit.web.core.area.ranking.Ranking):
             self.request.GET['date']))
 
     def date_to_page(self, date):
-        today = datetime.datetime.today().replace(
-            hour=0, minute=0, second=0, microsecond=0)
-        return (today-date).days + 1
+        return (self._today-date).days + 1
