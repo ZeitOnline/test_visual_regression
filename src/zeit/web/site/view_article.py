5989d0c55559bc0315e7119d52483d9d7c9e33f5
Thomas Puppe <thomas.puppe@zeit.de>
2016-06-06 14:07:18 +0200

    ZON-1985: Introduce storystreams to view_article

--- src/zeit/web/site/view_article.py
+++ src/zeit/web/site/view_article.py
@@ -66,6 +66,28 @@ class Article(zeit.web.core.view_article.Article, zeit.web.site.view.Base):
     def newsletter_optin_tracking(self):
         return self.request.GET.get('newsletter-optin', None)
 
+    @zeit.web.reify
+    def storystream(self):
+        if self.context.storystreams:
+            return self.context.storystreams[0].references
+        else:
+            return None
+
+    @zeit.web.reify
+    def storystream_items(self):
+        storystream_cp = self.storystream
+        if not storystream_cp:
+            return ()
+        atoms = list(zeit.content.cp.interfaces.ICMSContentIterable(
+            storystream_cp))
+        pos = atoms.index(self.context)
+        if pos == 0:
+            return atoms[:3]
+        elif pos == len(atoms) - 1:
+            return atoms[-3:]
+        else:
+            return atoms[pos - 1:pos + 2]
+
 
 @view_config(name='seite',
              path_info='.*seite-(.*)',
