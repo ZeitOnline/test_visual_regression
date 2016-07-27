93c74685a7990807dce6be22b16d7ec9800eadac
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2016-07-27 12:16:59 +0200

    ZON-3198: Add tests

--- src/zeit/web/core/test/test_view.py
+++ src/zeit/web/core/test/test_view.py
@@ -800,3 +800,46 @@ def test_reader_revenue_status_should_default_to_registration_for_ZEI(
         'http://xml.zeit.de/zeit-online/article/02')
     view = zeit.web.site.view_article.Article(context, dummy_request)
     assert view.webtrekk['customParameter']['cp28'] == 'registration'
+
+
+def test_jquery_not_overwritten(testserver, selenium_driver):
+    script = 'return jQuery.fn.jquery'
+
+    # ZON article
+    selenium_driver.get(
+        '{}/zeit-online/article/jquery-local-scope'.format(testserver.url))
+    assert '2.2.4' == selenium_driver.execute_script(script)
+
+    # ZMO article
+    selenium_driver.get(
+        '{}/zeit-magazin/article/jquery-local-scope'.format(testserver.url))
+    assert '2.2.4' == selenium_driver.execute_script(script)
+
+    # ZCO article
+    selenium_driver.get(
+        '{}/campus/article/jquery-local-scope'.format(testserver.url))
+    assert '2.2.4' == selenium_driver.execute_script(script)
+
+
+def test_jquery_not_in_window_scope(testserver, selenium_driver):
+    script = 'return typeof window.jQuery'
+
+    # ZON article
+    selenium_driver.get('{}/zeit-online/article/simple'.format(testserver.url))
+    assert 'undefined' == selenium_driver.execute_script(script)
+
+    # ZMO article
+    selenium_driver.get('{}/zeit-magazin/article/10'.format(testserver.url))
+    assert 'undefined' == selenium_driver.execute_script(script)
+
+    # ZCO article
+    selenium_driver.get('{}/campus/article/simple'.format(testserver.url))
+    assert 'undefined' == selenium_driver.execute_script(script)
+
+    # ZON Framebuilder
+    selenium_driver.get('{}/framebuilder'.format(testserver.url))
+    assert 'undefined' == selenium_driver.execute_script(script)
+
+    # ZCO Framebuilder
+    selenium_driver.get('{}/campus/framebuilder'.format(testserver.url))
+    assert 'undefined' == selenium_driver.execute_script(script)
