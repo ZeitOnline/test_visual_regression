b75c4a41b779b369cbd1cc0814a2ebc943a188e9
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-08-04 10:18:15 +0200

    ZON-3227: Update overview area to IContentQuery API

--- src/zeit/web/site/area/overview.py
+++ src/zeit/web/site/area/overview.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import copy
 import datetime
-import dateutil
 import logging
+
+import dateutil
+import grokcore.component
 import zope.component
 
 import zeit.solr.query
@@ -27,36 +29,6 @@ class Overview(zeit.web.core.area.ranking.Ranking):
     def start(self):
         return 0
 
-    @staticmethod
-    def clone_factory(jango, count):
-        for _ in range(count):
-            xml = copy.copy(jango.xml)
-            xml.attrib.pop('{http://namespaces.zeit.de/CMS/cp}__name__', None)
-            clone = type(jango)(jango.__parent__, xml)
-            yield clone
-
-    def _query_solr(self, query, sort_order):
-        query = zeit.solr.query.and_(query, self._range_query())
-        result = super(Overview, self)._query_solr(query, sort_order)
-        values = self.context.values()
-        length = len(values)
-        if length and self.hits > length:
-            overhang = min(self.hits, SANITY_BOUND) - length
-            for clone in self.clone_factory(values[-1], overhang):
-                self.context.add(clone)
-        return result
-
-    def _range_query(self):
-        offset = datetime.timedelta(days=self.current_page - 1)
-        today = self._today - offset
-        tomorrow = today + datetime.timedelta(days=1)
-        range_ = zeit.solr.query.datetime_range(
-            'date_first_released',
-            datetime.datetime.combine(today, datetime.time()),
-            datetime.datetime.combine(tomorrow, datetime.time()))
-
-        return range_
-
     @zeit.web.reify
     def total_pages(self):
         conf = zope.component.getUtility(zeit.web.core.interfaces.ISettings)
@@ -68,15 +40,15 @@ class Overview(zeit.web.core.area.ranking.Ranking):
             return int(total_pages)
 
     @zeit.web.reify
-    def _today(self):
+    def today(self):
         return datetime.datetime.today().replace(
             hour=0, minute=0, second=0, microsecond=0)
 
     @zeit.web.reify
     def pagination_info(self):
-        previous_day = (self._today+datetime.timedelta(
+        previous_day = (self.today + datetime.timedelta(
             days=self.current_page)).strftime("%Y-%m-%d")
-        next_day = (self._today-datetime.timedelta(
+        next_day = (self.today - datetime.timedelta(
             days=self.current_page)).strftime("%Y-%m-%d")
         return {
             'previous_label': u'Nächster Tag',
@@ -87,7 +59,7 @@ class Overview(zeit.web.core.area.ranking.Ranking):
     def page_info(self, page_nr):
         if not page_nr:
             page_nr = 1
-        date = self._today - datetime.timedelta(days=(page_nr-1))
+        date = self.today - datetime.timedelta(days=(page_nr - 1))
 
         date_label = date.strftime("%d.%m")
         date_param = date.strftime("%Y-%m-%d")
@@ -112,4 +84,48 @@ class Overview(zeit.web.core.area.ranking.Ranking):
             self.current_page, self.total_pages, slots=6)
 
     def date_to_page(self, date):
-        return (self._today-date).days + 1
+        return (self.today - date).days + 1
+
+
+class DateContentQuery(zeit.web.core.area.ranking.SolrContentQuery):
+
+    grokcore.component.context(Overview)
+
+    def __call__(self):
+        result = super(DateContentQuery, self).__call__()
+        self._fill_up_blocks_to_match_total_hits()
+        return result
+
+    def _fill_up_blocks_to_match_total_hits(self):
+        # XXX This actually rather is the responsibility of the Area, but we
+        # cannot do it there at the proper time (when total_hits is available,
+        # but before auto blocks are filled with the query content).
+        area = self.context.context
+        values = area.values()
+        length = len(values)
+        if length and self.total_hits > length:
+            overhang = min(self.total_hits, SANITY_BOUND) - length
+            for clone in self.clone_factory(values[-1], overhang):
+                area.add(clone)
+
+    @staticmethod
+    def clone_factory(jango, count):
+        for _ in range(count):
+            xml = copy.copy(jango.xml)
+            xml.attrib.pop('{http://namespaces.zeit.de/CMS/cp}__name__', None)
+            clone = type(jango)(jango.__parent__, xml)
+            yield clone
+
+    def _query_solr(self, query, sort_order):
+        query = zeit.solr.query.and_(query, self._range_query())
+        return super(DateContentQuery, self)._query_solr(query, sort_order)
+
+    def _range_query(self):
+        offset = datetime.timedelta(days=self.context.current_page - 1)
+        today = self.context.today - offset
+        tomorrow = today + datetime.timedelta(days=1)
+        range_ = zeit.solr.query.datetime_range(
+            'date_first_released',
+            datetime.datetime.combine(today, datetime.time()),
+            datetime.datetime.combine(tomorrow, datetime.time()))
+        return range_
