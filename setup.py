b86a2337176f309eeb780418f617ce0d97e80428
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-08-11 15:42:55 +0200

    Back to development: 2.98

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='zeit.web',
     url='https://github.com/ZeitOnline/zeit.web',
-    version='2.97.4',
+    version='2.98.dev0',
     author=(
         'Thomas Baumann, Nico Bruenjes, Nicolas Drebenstedt, Ron Drongowski, '
         'Dominik Hoppe, Marco Kaiser, Harry Keller, Tom Lazar, Thomas Lotze, '
