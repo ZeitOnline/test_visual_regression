88e10b7d2202b16ca35b3ff5bee3adcbc74f6485
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-09-09 11:10:08 +0200

    BUG-531: Ensure meta files are rendered on xml.zeit.de
    The actual fix for this issue happens in zeit.connector-2.8.0

--- src/zeit/web/core/test/test_xml.py
+++ src/zeit/web/core/test/test_xml.py
@@ -221,3 +221,12 @@ def test_xml_adds_cors_header(testserver):
         '%s/davcontent/text' % testserver.url,
         headers={'Host': 'xml.zeit.de'})
     assert res.headers['Access-Control-Allow-Origin'] == '*'
+
+
+def test_xml_renders_image_meta_files(testserver):
+    # For example, quiz.zeit.de parses those.
+    res = requests.get(
+        '%s/zeit-online/image/filmstill-hobbit-schlacht-fuenf-hee.meta' %
+        testserver.url, headers={'Host': 'xml.zeit.de'})
+    assert res.status_code == 200
+    assert res.content.startswith('<?xml')
