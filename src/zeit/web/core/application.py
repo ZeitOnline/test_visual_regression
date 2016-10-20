0fe62a7c4f41cbf7c27d8ee4c76e6aa10f1187ee
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2016-10-20 15:35:10 +0200

    MAINT: Send referrer and user-agent to solr (should help when analyzing incidents in access logs)

--- src/zeit/web/core/application.py
+++ src/zeit/web/core/application.py
@@ -530,3 +530,15 @@ def solr_timeout_from_settings(self):
     return conf.get('solr_timeout', 5)
 pysolr.Solr.timeout = property(
     solr_timeout_from_settings, lambda self, value: None)
+
+
+def solr_send_request_with_referrer(self, *args, **kw):
+    headers = kw.setdefault('headers', {})
+    request = pyramid.threadlocal.get_current_request()
+    headers['Referer'] = request.url
+    headers['User-Agent'] = requests.utils.default_user_agent(
+        'zeit.web-%s/pysolr/python-requests' % request.registry.settings.get(
+            'version', 'unknown'))
+    return original_send_request(self, *args, **kw)
+original_send_request = pysolr.Solr._send_request
+pysolr.Solr._send_request = solr_send_request_with_referrer
