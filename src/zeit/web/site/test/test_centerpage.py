a931fb28af0a128121ae7389db6b80e977da9d22
Dominik Hoppe <dominik.hoppe@zeit.de>
2016-11-03 16:06:59 +0100

    ZON-3287: Fix test, using dummy_request properly

--- src/zeit/web/site/test/test_centerpage.py
+++ src/zeit/web/site/test/test_centerpage.py
@@ -3,7 +3,6 @@
 import urllib2
 
 import lxml.html
-import pyramid.testing
 import pytest
 import zope.component
 
@@ -207,18 +206,14 @@ def test_pagination_should_be_validated(testbrowser):
             '/dynamic/angela-merkel?p=1moep').headers
 
 
-def test_centerpage_markdown_module_is_rendered(jinja2_env):
+def test_centerpage_markdown_module_is_rendered(jinja2_env, dummy_request):
     tpl = jinja2_env.get_template('zeit.web.site:templates/centerpage.html')
     content = zeit.cms.interfaces.ICMSContent(
         'http://xml.zeit.de/zeit-online/thema')
-    request = pyramid.testing.DummyRequest(
-        route_url=lambda x: 'http://foo.bar/',
-        asset_host='',
-        image_host='')
-    view = zeit.web.site.view_centerpage.Centerpage(content, request)
+    view = zeit.web.site.view_centerpage.Centerpage(content, dummy_request)
     view.meta_robots = ''
     view.canonical_url = ''
-    html_str = tpl.render(view=view, request=request)
+    html_str = tpl.render(view=view, request=dummy_request)
     html = lxml.html.fromstring(html_str)
 
     assert len(html.cssselect('.markup')) == 1
