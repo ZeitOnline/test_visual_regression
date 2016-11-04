91b7990316e720364ba23bff87ce6e80c4e04bee
Tobias Weiß <tobias.weiss@zeit.de>
2016-11-04 16:38:29 +0100

    ZON-3385: Inject Markup via JS

--- javascript/web.core/plugins/jquery.notifications.js
+++ javascript/web.core/plugins/jquery.notifications.js
@@ -1,9 +1,17 @@
 (function( $, window ) {
     $.fn.notifications = function() {
-        if ( window.location.hash.substr( 1 ) === 'registration_success' ) {
-            $( '.notification--registration' ).removeClass( 'notification--hidden' );
+        var location = window.location,
+            msg_registration_success = "<div class='notification notification--success notification--registration'>" +
+                "<span class='notification notification__message'>Herzlich willkommen! Mit Ihrer Anmeldung können Sie nun unsere Artikel lesen.</span></div>";
+        if ( location.hash.substr( 1 ) === 'registration_success' ) {
+            $( msg_registration_success ).insertAfter( 'header' );
+            if ("replaceState" in history) {
+                history.replaceState( "", document.title, location.pathname + location.search );
+            } else {
+                location.hash = "";
+            }
         } else {
-            $( '.notification--registration' ).remove();
+            return;
         }
     };
-})( jQuery, window );
+})( jQuery, window);
