d911c8141922d19747367db5e1b87df8d14a5ab0
Tobias Weiß <tobias.weiss@zeit.de>
2016-11-07 16:47:54 +0100

    ZON-3385: Consider a11y

--- javascript/web.core/plugins/jquery.notifications.js
+++ javascript/web.core/plugins/jquery.notifications.js
@@ -1,8 +1,10 @@
 (function( $, window ) {
     $.fn.notifications = function() {
         var location = window.location,
-            msgRegistrationSuccess = '<div class="notification notification--success">' +
+            msgRegistrationSuccess = '<div class="notification notification--success" tabindex="0">' +
                 '<span>Herzlich willkommen! Mit Ihrer Anmeldung können Sie nun unsere Artikel lesen.</span></div>';
+
+        // display hash only when loading article page from email-link
         if ( location.hash.substr( 1 ) === 'registration_success' ) {
             $( msgRegistrationSuccess ).insertAfter( 'header' );
             if ( 'replaceState' in history ) {
@@ -13,5 +15,20 @@
         } else {
             return;
         }
+
+        // a11y: remove :focus outline only for mouse users
+        // keep enabled for keyboard users
+        function handleFocusStyle ( $, e ) {
+            var notification = e.currentTarget;
+            e.type === 'mousedown' ? $( notification ).addClass( 'notification--no-focus' ) : false;
+            $( notification ).on( 'blur', function() {
+                $( this ).removeClass( 'notification--no-focus' );
+            });
+        }
+
+        $( '.notification' ).on( 'mousedown', function( e )  {
+            handleFocusStyle( $, e );
+        });
+
     };
 })( jQuery, window );
