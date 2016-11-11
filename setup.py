df40f76a430cdb56f9ba55b2aaa47a877743420e
Tobias Weiß <tobias.weiss@zeit.de>
2016-11-11 16:01:07 +0100

    Preparing release 3.17

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='zeit.web',
     url='https://github.com/ZeitOnline/zeit.web',
-    version='3.17.dev0',
+    version='3.17',
     author=(
         'Thomas Baumann, Nico Bruenjes, Nicolas Drebenstedt, Ron Drongowski, '
         'Dominik Hoppe, Marco Kaiser, Harry Keller, Tom Lazar, Thomas Lotze, '
