c91826c49dbe23f4668c9a3beaea1de8d536ff0f
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2016-11-16 11:11:19 +0100

    MAINT: Add code style linting to ZMO

--- javascript/web.magazin/plugins/jquery.backgroundvideo.js
+++ javascript/web.magazin/plugins/jquery.backgroundvideo.js
@@ -3,8 +3,8 @@
  * @author anika.szuppa@zeit.de
  * @version  0.1
  */
-(function( $, Modernizr ){
-	/**
+(function( $, Modernizr ) {
+    /**
      * See (http://jquery.com/).
      * @name jQuery
      * @alias $
@@ -26,51 +26,50 @@
      * @memberOf jQuery.fn
      * @return {object} jQuery-Object for chaining
      */
-	$.fn.backgroundVideo = function() {
+    $.fn.backgroundVideo = function() {
 
-		var el = {
-			//change left position of video to center it
-			setVideoPosition: function( $element ){
+        //change left position of video to center it
+        function setVideoPosition( $element ) {
 
-				var video = $element.find("video");
-				video.css({"left": ($element.width() - video.width())/2 + "px"});
-				video.css({"top": ($element.height() - video.height())/2 + "px"});
+            var video = $element.find( 'video' ),
+                positionLeft = ( $element.width() - video.width() ) / 2,
+                positionTop = ( $element.height() - video.height() ) / 2;
 
-				// prevents the video from glitching
-				if( $element.attr( "data-backgroundvideo" ) ){
-					video.removeAttr( "poster" );
-				}
-			},
+            video.css({ left: positionLeft + 'px', top: positionTop + 'px' });
 
-		};
+            // prevents the video from glitching
+            if ( $element.attr( 'data-backgroundvideo' ) ) {
+                video.removeAttr( 'poster' );
+            }
+        }
 
-		//run through data-backgroundvideo elements
-		return this.each( function() {
+        //run through data-backgroundvideo elements
+        return this.each( function() {
 
-			var $element = $( this );
-			var video = $element.find( "video" );
+            var $element = $( this ),
+                video = $element.find( 'video' );
 
-			//set video position
-			el.setVideoPosition( $element );
+            //set video position
+            setVideoPosition( $element );
 
-			//start video
-			if ( Modernizr.video ) {
-				$(video).get(0).play();
-			}
+            //start video
+            if ( Modernizr.video ) {
+                $( video ).get( 0 ).play();
+            }
 
-			//on video play, hide image and show video
-			$( video ).on( "play", function() {
-				$element.find( ".video--fallback" ).fadeOut();
-				$( video ).show();
-			});
+            //on video play, hide image and show video
+            $( video ).on( 'play', function() {
+                $element.find( '.video--fallback' ).fadeOut();
+                $( video ).show();
+            });
 
-			//reset video position on resize
-			$( window ).on( "resize", function(){
-				el.setVideoPosition( $element );
-			});
+            //reset video position on resize
+            $( window ).on( 'resize', function() {
+                setVideoPosition( $element );
+            });
 
-		});
+        });
 
-	};
+    };
 
 })( jQuery, window.Modernizr );
