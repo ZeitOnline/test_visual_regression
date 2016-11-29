d0ec7fb675bc302c6a727a76ce615744a78b6786
Nico Bruenjes <nico.bruenjes@gmail.com>
2016-11-29 10:18:29 +0100

    BUG-549: Be more defensive in several places

--- javascript/web.site/plugins/jquery.adaptnav.js
+++ javascript/web.site/plugins/jquery.adaptnav.js
@@ -31,6 +31,7 @@
     $.fn.adaptToSpace = function() {
 
         function Container( element ) {
+            this.maxWidth = 0;
             this.node = element;
             this.element = $( element );
             this.featured = this.element.find( '.nav__ressorts-item--featured' );
@@ -74,8 +75,11 @@
                 // BUG-549
                 // safari gets the width of pseudo elements wrong
                 // found no better fix than measuring again late in process
-                $( window ).one( 'load', function() {
-                    self.adapt();
+                $( window ).on( 'load', function( event ) {
+                    if ( self.isDesktop() && self.node.scrollWidth > self.maxWidth ) {
+                        self.adapt();
+                        $( this ).off( event );
+                    }
                 });
             },
             adapt: function() {
@@ -87,12 +91,12 @@
                 if ( this.isDesktop() ) {
                     parent.removeClass( 'nav__ressorts--fitted' );
                     this.clonedItems.hide();
+                    this.maxWidth = parent.width();
 
-                    var maxWidth = parent.width(),
-                        index;
+                    var index = this.items.length;
 
-                    for ( index = this.items.length; index--; ) {
-                        if ( this.node.scrollWidth > maxWidth ) {
+                    for ( ; index--; ) {
+                        if ( this.node.scrollWidth > this.maxWidth ) {
                             this.items.eq( index ).hide();
                             this.clonedItems.eq( index ).show();
                         } else {
