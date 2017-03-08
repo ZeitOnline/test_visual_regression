36f0cf8c6fe52dd9e1e53532eecc01d8ac7469f2
Thomas Puppe <thomas.puppe@zeit.de>
2017-03-08 16:38:55 +0100

    Enable testbrowser/cssselect in smoke tests

--- smoketest/conftest.py
+++ smoketest/conftest.py
@@ -3,7 +3,11 @@ import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
+import cssselect
+import json
+import lxml.html
 import pytest
+import requests
 import selenium.webdriver
 import selenium.webdriver.firefox.firefox_binary
 
@@ -74,3 +78,70 @@ def selenium_driver(request):
     browser.get = get_and_wait_for_body.__get__(browser)
 
     return browser
+
+
+@pytest.fixture
+def testbrowser():
+    return BaseBrowser()
+
+
+# When testing ESI fragments, autodetection of the encoding may not work (no
+# head, so no meta charset declaration), so we specify it explicitly.
+HTML_PARSER = lxml.html.HTMLParser(encoding='UTF-8')
+
+
+class BaseBrowser(object):
+    """Base class for custom test browsers that allow direct access to CSS and
+    XPath selection on their content.
+
+    Usage examples:
+
+    # Create a test browser
+    browser = Browser('/foo/bar')
+    # Test only one h1 exists
+    assert len(browser.cssselect('h1.only-once')) == 1
+    # Test all divs contain at least one span
+    divs = browser.cssselect('div')
+    assert all(map(lambda d: d.cssselect('span'), divs))
+    # Test the third paragraph has two links with class batz
+    paragraph = browser.cssselect('p')[2]
+    assert len(paragraph.cssselect('a.batz')) == 2
+
+    """
+
+    def __call__(self, uri=None, **kw):
+        if uri is not None:
+            self.open(uri, **kw)
+        return self
+
+    def open(self, uri, **kw):
+        r = requests.get(uri, **kw)
+        self.contents = r.text
+
+    @property
+    def document(self):
+        """Return an lxml.html.HtmlElement instance of the response body."""
+        if self.contents is not None:
+            return lxml.html.document_fromstring(
+                self.contents, parser=HTML_PARSER)
+
+    @property
+    def json(self):
+        """Return a dictionary of the parsed json body if available."""
+        if self.contents is not None:
+            return json.loads(self.contents)
+
+    def cssselect(self, selector):
+        """Return a list of lxml.HTMLElement instances that match a given CSS
+        selector.
+        """
+        xpath = cssselect.HTMLTranslator().css_to_xpath(selector)
+        if self.document is not None:
+            return self.document.xpath(xpath)
+
+    def xpath(self, selector):
+        """Return a list of lxml.HTMLElement instances that match a given
+        XPath selector.
+        """
+        if self.document is not None:
+            return self.document.xpath(selector)
