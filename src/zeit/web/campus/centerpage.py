04de54f063eff88628f6596d5b7cca003d077c2b
Thomas Puppe <thomas.puppe@zeit.de>
2017-03-10 14:50:42 +0100

    ZON-3778: Use HP topiclinks on CPs

--- src/zeit/web/campus/centerpage.py
+++ src/zeit/web/campus/centerpage.py
@@ -13,11 +13,6 @@ DEFAULT_TERM_CACHE = zeit.web.core.cache.get_region('default_term')
 @grokcore.component.implementer(zeit.web.core.interfaces.ITopicLink)
 @grokcore.component.adapter(zeit.campus.interfaces.IZCOContent)
 def campus_topiclink(context):
-    # Accepting any IZCOContent here only works because the TopicLink class is
-    # implemented tolerantly when its context does not provide ICenterPage.
-    topiclink = zeit.web.core.centerpage.TopicLink(context)
-    if topiclink:
-        return topiclink
 
     def get_default_topics():
         context = zeit.cms.interfaces.ICMSContent(
