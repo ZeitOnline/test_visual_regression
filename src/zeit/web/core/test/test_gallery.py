32b7e760e8db8cc87b119385df88b15511229bca
Thomas Baumann <thomas.baumann@zeit.de>
2017-05-02 11:25:10 +0200

    ZON-3870: add test for redirect '/seite-n' redirects in galleries

---
+++ src/zeit/web/core/test/test_gallery.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import requests
+
+import zeit.web.core
+
+def test_gallery_seite_path_should_redirect_to_gallery_start(testserver):
+    resp = requests.get('%s/zeit-online/gallery/biga_1/seite-3'
+                        % testserver.url,
+                        allow_redirects=False)
+    assert(resp.headers['location'] ==
+           '%s/zeit-online/gallery/biga_1' % testserver.url)
+    assert resp.status_code == 301
