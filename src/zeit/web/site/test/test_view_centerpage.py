a7b1016a3b6b2087a9e8153693ab51aab8b5c960
paulbauer <jan-paul.bauer@zeit.de>
2017-06-14 15:08:45 +0200

    ZON-3955: Fix test

--- src/zeit/web/site/test/test_view_centerpage.py
+++ src/zeit/web/site/test/test_view_centerpage.py
@@ -2800,24 +2800,12 @@ def test_d17_icon_is_not_display_on_zmo_teaser(monkeypatch, testbrowser):
         '.teaser-small__kicker-logo--tag + .teaser-small__kicker-logo--zmo')
 
 
-def test_d17_icon_is_not_display_on_zett_teaser(monkeypatch, testbrowser):
-    monkeypatch.setattr(zeit.web.core.application.FEATURE_TOGGLES, 'find', {
-        'tag_logos': True, 'reader_revenue': True}.get)
-    browser = testbrowser('/zeit-online/centerpage/taglogo')
-    assert not browser.cssselect(
-        'teaser-small__kicker-logo--zett + .teaser-small__kicker-logo--tag')
-    assert not browser.cssselect(
-        '.teaser-small__kicker-logo--tag + .teaser-small__kicker-logo--zett')
-
-
 def test_d17_icon_is_not_display_on_d17_teaser(monkeypatch, testbrowser):
     monkeypatch.setattr(zeit.web.core.application.FEATURE_TOGGLES, 'find', {
         'tag_logos': True, 'reader_revenue': True}.get)
     browser = testbrowser('/zeit-online/parquet')
     assert not browser.cssselect(
-        'teaser-large__kicker--d17-parquet')
-    assert not browser.cssselect(
-        'teaser-small__kicker--d17-parquet')
+        '.cp-area--d17-parquet .teaser-large__kicker-logo--tag')
 
 
 def test_d17_icon_is_display_on_auto_area(monkeypatch, testbrowser):
