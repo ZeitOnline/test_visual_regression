c329d173efce1ae33278bf0325cdc249f7084901
Thomas Puppe <thomas.puppe@zeit.de>
2017-07-11 14:41:28 +0200

    ARBEIT-40: Basic styling of header and navi
    not yet cleanedup

--- src/zeit/web/arbeit/view.py
+++ src/zeit/web/arbeit/view.py
@@ -25,3 +25,12 @@ class Content(Base):
     @zeit.web.reify
     def article_layout(self):
         return 'default'
+
+
+@zeit.web.view_config(
+    route_name='login_state',
+    renderer='templates/inc/navigation/login-state.html',
+    request_param='for=arbeit',
+    http_cache=60)
+def login_state(request):
+    return zeit.web.core.security.get_login_state(request)
