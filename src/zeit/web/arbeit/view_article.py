f5e3be1d1ba45c9faedd7dfa803da348a424d390
Tobias Weiß <tobias.weiss@zeit.de>
2017-08-11 16:25:49 +0200

    ARBEIT-7: Maint: Apply IVertical adapter to ZAR column articles

--- src/zeit/web/arbeit/view_article.py
+++ src/zeit/web/arbeit/view_article.py
@@ -41,28 +41,19 @@ class ArticlePage(zeit.web.core.view_article.ArticlePage, Article):
     pass
 
 
-def is_column_article(context, request):
-    return getattr(context, 'serie', None) and context.serie.column
-
-
+@zeit.web.view_defaults(
+    context=zeit.web.core.article.IColumnArticle,
+    vertical='zar')
 @zeit.web.view_config(
-    custom_predicates=(zeit.web.arbeit.view.is_zar_content,
-                       is_column_article),
     renderer='templates/article.html')
 @zeit.web.view_config(
-    custom_predicates=(zeit.web.arbeit.view.is_zar_content,
-                       is_column_article,
-                       zeit.web.core.view.is_paywalled),
+    custom_predicates=(zeit.web.core.view.is_paywalled,),
     renderer='zeit.web.core:templates/paywall.html')
 @zeit.web.view_config(
-    custom_predicates=(zeit.web.arbeit.view.is_zar_content,
-                       is_column_article),
     name='komplettansicht',
     renderer='templates/komplettansicht.html')
 @zeit.web.view_config(
-    custom_predicates=(zeit.web.arbeit.view.is_zar_content,
-                       is_column_article,
-                       zeit.web.core.view.is_paywalled),
+    custom_predicates=(zeit.web.core.view.is_paywalled,),
     name='komplettansicht',
     renderer='zeit.web.core:templates/paywall.html')
 class ColumnArticle(Article):
