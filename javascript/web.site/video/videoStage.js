de39cd6bf923f52886ac1a4e085bd4f30b17783b
paulbauer <jan-paul.bauer@zeit.de>
2017-08-24 10:38:53 +0200

    ZON-4172: Ask if ad or no ad

--- javascript/web.site/video/videoStage.js
+++ javascript/web.site/video/videoStage.js
@@ -10,7 +10,7 @@ define([ 'jquery', 'web.site/video/video' ], function( $, video ) {
     return {
         init: function() {
             var videoArticles = $( '.video-stage-main .video-large' ),
-                links;
+                links, options;
             if ( videoArticles.length > 0 ) {
                 links = videoArticles.find( 'a' );
                 links.one( 'click', function( event ) {
@@ -18,13 +18,26 @@ define([ 'jquery', 'web.site/video/video' ], function( $, video ) {
                     var article = $( this ).closest( 'article' ),
                         container = article.find( '.video-large__container' ),
                         elem = article.find( '.video-thumbnail' ),
-                        videoId = article.data( 'video-id' );
+                        videoId = article.data( 'video-id' ),
+                        playerId = article.data( 'video-player-id' );
                     if ( typeof videoId !== 'undefined' ) {
                         article.find( '.video-text-playbutton' ).addClass( 'video-text-playbutton--hidden' );
                         article.find( '.video-large__inner' ).addClass( 'video-large__inner--playing' );
                         article.find( '.video-large-title' ).addClass( 'video-large-title--playing' );
                         container.unwrap();
-                        video.displayVideo( videoId, { elem: elem });
+                        if ( playerId ) {
+                            options = {
+                                elem: elem,
+                                playerData: {
+                                    'accountId': '18140073001',
+                                    'playerId': playerId,
+                                    'embed': 'default'
+                                }
+                            };
+                        } else {
+                            options = { elem: elem };
+                        }
+                        video.displayVideo( videoId, options );
                     }
                 });
             }
