ea35b88d74a5d31c8ff506b062be34c6a3590660
Thomas Puppe <thomas.puppe@zeit.de>
2017-08-29 16:44:57 +0200

    ARBEIT-114: Fix the tests

--- src/zeit/web/arbeit/test/test_advertising.py
+++ src/zeit/web/arbeit/test/test_advertising.py
@@ -17,7 +17,7 @@ def test_zar_homepage_should_have_proper_ivw_script_integration(
 
 
 def test_zar_adcontroller_head_code_is_present(
-        testbrowser,togglepatch):
+        testbrowser, togglepatch):
     togglepatch({'third_party_modules': True, 'iqd': True})
 
     browser = testbrowser('/arbeit/slenderized-index')
@@ -27,24 +27,14 @@ def test_zar_adcontroller_head_code_is_present(
     assert '<!-- mandanten object -->' in browser.contents
 
 
-def test_zar_adcontroller_adtags_are_present(testbrowser, monkeypatch):
+def test_zar_adcontroller_adtags_are_present(testbrowser, togglepatch):
     browser = testbrowser('/arbeit/slenderized-index')
     assert 'ad-desktop-1' in browser.contents
     assert 'ad-desktop-2' in browser.contents
     assert 'ad-desktop-3' in browser.contents
+    assert 'ad-desktop-16' in browser.contents
     assert 'ad-mobile-1' in browser.contents
-    assert 'ad-mobile-3' in browser.contents
-    assert 'ad-mobile-4' in browser.contents
     assert 'ad-mobile-8' in browser.contents
-    # adtile 7 will be renumbered to… 8
-    monkeypatch.setattr(zeit.web.core.application.FEATURE_TOGGLES, 'find', {
-        'iqd_digital_transformation': False}.get)
-    browser = testbrowser('/arbeit/slenderized-index')
-    assert 'ad-desktop-7' in browser.contents
-    monkeypatch.setattr(zeit.web.core.application.FEATURE_TOGGLES, 'find', {
-        'iqd_digital_transformation': True}.get)
-    browser = testbrowser('/arbeit/slenderized-index')
-    assert 'ad-desktop-8' in browser.contents
 
 
 def test_zar_adcontroller_finalizer_is_present(
@@ -64,10 +54,8 @@ def test_zar_adcontroller_js_var_isset(
     assert adctrl == "object"
 
 
-def test_zar_adplaces_present_on_empty_cp(testbrowser, monkeypatch):
-    monkeypatch.setattr(zeit.web.core.application.FEATURE_TOGGLES, 'find', {
-        'third_party_modules': True, 'iqd': True,
-        'iqd_digital_transformation': True}.get)
+def test_zar_adplaces_present_on_empty_cp(testbrowser, togglepatch):
+    togglepatch({'third_party_modules': True, 'iqd': True})
 
     browser = testbrowser('/arbeit/slenderized-index')
     assert len(browser.cssselect('#iqadtileOOP')) == 1
@@ -80,9 +68,8 @@ def test_zar_adplaces_present_on_empty_cp(testbrowser, monkeypatch):
     assert len(browser.cssselect('#ad-mobile-8')) == 1
 
 
-def test_zar_adplaces_present_on_cp(testbrowser, monkeypatch):
-    monkeypatch.setattr(zeit.web.core.application.FEATURE_TOGGLES, 'find', {
-        'iqd_digital_transformation': True}.get)
+def test_zar_adplaces_present_on_cp(testbrowser, togglepatch):
+    togglepatch({'third_party_modules': True, 'iqd': True})
 
     browser = testbrowser('/arbeit/index')
     assert len(browser.cssselect('#iqadtileOOP')) == 1
@@ -100,9 +87,8 @@ def test_zar_adplaces_present_on_cp(testbrowser, monkeypatch):
 
 
 def test_zar_adcontroller_values_return_values_on_article(
-        application, monkeypatch):
-    monkeypatch.setattr(zeit.web.core.application.FEATURE_TOGGLES, 'find', {
-        'iqd_digital_transformation': True}.get)
+        application, togglepatch):
+    togglepatch({'iqd_digital_transformation': True})
     content = zeit.cms.interfaces.ICMSContent(
         'http://xml.zeit.de/arbeit/article/keywords')
     adcv = [
@@ -120,9 +106,8 @@ def test_zar_adcontroller_values_return_values_on_article(
 
 
 def test_zar_ads_are_rendered_on_articles_with_multiple_pages(
-        testbrowser, monkeypatch):
-    monkeypatch.setattr(zeit.web.core.application.FEATURE_TOGGLES, 'find', {
-        'iqd_digital_transformation': True}.get)
+        testbrowser, togglepatch):
+    togglepatch({'iqd_digital_transformation': True})
     selector = (
         '#ad-desktop-8', '#ad-desktop-4', '#ad-mobile-3', '#ad-mobile-4')
 
@@ -152,23 +137,22 @@ def test_zar_ads_are_rendered_on_articles_with_multiple_pages(
     assert len(browser.cssselect(selector[3])) == 1
 
 
-def test_zar_tile8_is_rendered_on_cp(
-        testbrowser, monkeypatch):
-    monkeypatch.setattr(zeit.web.core.application.FEATURE_TOGGLES, 'find', {
-        'iqd_digital_transformation': True}.get)
-    browser = testbrowser('/arbeit/centerpage/adplace8')
-    assert len(browser.cssselect('#ad-desktop-8')) == 1
-
-
-def test_zar_desktop_ads_are_rendered_on_cp(
-        testbrowser, togglepatch):
-
+def test_zar_tile8_is_rendered_on_cp(testbrowser, togglepatch):
     togglepatch({
         'third_party_modules': True,
         'iqd': True,
         'iqd_digital_transformation': True
     })
+    browser = testbrowser('/arbeit/centerpage/adplace8')
+    assert len(browser.cssselect('#ad-desktop-8')) == 1
 
+
+def test_zar_desktop_ads_are_rendered_on_cp(testbrowser, togglepatch):
+    togglepatch({
+        'third_party_modules': True,
+        'iqd': True,
+        'iqd_digital_transformation': True
+    })
     browser = testbrowser('/arbeit/index')
 
     assert len(browser.cssselect('#ad-desktop-3')) == 1
