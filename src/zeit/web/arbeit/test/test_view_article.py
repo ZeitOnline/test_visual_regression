95b88f9151869b0eae4b5d8fd0bf63e78b3c6f9e
Thomas Puppe <thomas.puppe@zeit.de>
2017-08-30 15:35:41 +0200

    ARBEIT-105: Adjust test to dummy content

--- src/zeit/web/arbeit/test/test_view_article.py
+++ src/zeit/web/arbeit/test/test_view_article.py
@@ -226,7 +226,7 @@ def test_zar_article_should_provide_jobboxticker(
     article = zeit.cms.interfaces.ICMSContent(
         'http://xml.zeit.de/arbeit/article/jobbox-ticker')
     body = zeit.content.article.edit.interfaces.IEditableBody(article)
-    jobbox_ticker = zeit.web.core.interfaces.IFrontendBlock(body[2])
+    jobbox_ticker = zeit.web.core.interfaces.IFrontendBlock(body[1])
     ticker_item = next(jobbox_ticker.items)
 
     assert 'my_title' == ticker_item.title
