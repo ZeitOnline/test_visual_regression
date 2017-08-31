b5b0ebedebbdd1c89c6c29bb31e5c7a009fc72c5
Nico Bruenjes <nico.bruenjes@gmail.com>
2017-08-31 12:18:24 +0200

    ZON-4131: Enforce code style

--- javascript/web.core/brightcove-plugins/videojs-custom.js
+++ javascript/web.core/brightcove-plugins/videojs-custom.js
@@ -5,11 +5,11 @@
 */
 
 // Removing focus
-function handleFirstTab(e) {
-  if (e.keyCode === 9) {
-    document.body.classList.add('user-tabbing');
-    window.removeEventListener('keydown', handleFirstTab);
-  }
+function handleFirstTab( e ) {
+    if ( e.keyCode === 9 ) {
+        document.body.classList.add( 'user-tabbing' );
+        window.removeEventListener( 'keydown', handleFirstTab );
+    }
 }
 
-window.addEventListener('keydown', handleFirstTab);
+window.addEventListener( 'keydown', handleFirstTab );
