e583372ddb24164dbaf54b48087311a91c25722f
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2017-09-12 09:55:25 +0200

    [BUG-789] Add scriptPath to framebuilder scripts

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,8 @@ if ( false ) {
 module.exports = {
     context: path.resolve(__dirname, 'javascript'),
     entry: {
-        'web.campus/frame': 'web.campus/frame.js',
-        'web.site/frame': 'web.site/frame.js',
+        'web.campus/frame': ['./web.core/scriptPath', 'web.campus/frame.js'],
+        'web.site/frame': ['./web.core/scriptPath', 'web.site/frame.js'],
         arbeit: ['./web.core/scriptPath', './arbeit.js'],
         campus: ['./web.core/scriptPath', './campus.js'],
         magazin: ['./web.core/scriptPath', './magazin.js'],
