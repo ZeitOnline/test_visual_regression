fff49c63f96f07ee61934a4c0ad0dba1329169a8
Thomas Puppe <thomas.puppe@zeit.de>
2017-10-18 16:02:49 +0200

    ARBEIT-20: Change and reveal a regular link onclick

--- javascript/web.arbeit/plugins/jquery.shareBlocks.js
+++ javascript/web.arbeit/plugins/jquery.shareBlocks.js
@@ -8,10 +8,7 @@
 
     'use strict';
 
-    var debugMode = location.hash.indexOf( 'debug-shareblocks' ) > -1,
-        rootElement,
-        labelElement,
-        itemsElement;
+    var debugMode = location.hash.indexOf( 'debug-shareblocks' ) > -1;
 
     function log() {
         if ( debugMode ) {
@@ -28,28 +25,9 @@
     function ShareBlock( rootElement ) {
         log( 'ShareBlock()' ); // REMOVE
         this.rootElement = rootElement;
+        this.clickEventListener = null;
         this.init();
     }
-
-    ShareBlock.prototype.init = function() {
-        this.labelElement = this.rootElement.querySelector( '.js-shareblock__label' );
-        this.itemsElement = this.rootElement.querySelector( '.js-shareblock__items' );
-
-        this.itemsElement.style.display = 'none';
-        this.labelElement.addEventListener( 'click', this.clickEvent );
-    };
-
-    ShareBlock.prototype.clickEvent = function( event ) {
-        event.preventDefault();
-
-        // getSharebertData().then(exchangeLink())
-        // slideInTheButtons();
-
-        window.console.debug( this.rootElement );
-
-        this.itemsElement.style.display = 'inline-block'; // TODO: use classes/aria
-    };
-
 
     var getSharebertData = function( sharebertShotUrl, sharebertRedirectUrl ) {
         return new window.Promise( function( resolve, reject ) {
@@ -89,22 +67,15 @@
         });
     };
 
-    var share = function( event ) {
-
-        event.preventDefault();
+    var clickEventListener = function() {
+        // event.preventDefault();
 
         // getSharebertData().then(exchangeLink())
         // slideInTheButtons();
 
-        itemsElement.style.display = 'inline-block'; // TODO: use classes/aria
-
-        getSharebertData();
-
-        /*
-        // Lösung: innerhalb von init() oder share() die Daten halten. Nicht im Plugin.
-        var $elem = $( event.target ).closest( '.js-shareblock' );
-        var sharebertRedirectUrl = $elem.data( 'sharebert-redirect-url' );
-        var sharebertShotUrl = $elem.data( 'sharebert-screenshot-target' );
+        var $elem = this.rootElement.querySelector( '.quote-sharing__link--twitter' );
+        var sharebertRedirectUrl = $elem.getAttribute( 'data-sharebert-redirect-url' );
+        var sharebertShotUrl = $elem.getAttribute( 'data-sharebert-screenshot-target' );
         // for testing locally (Sharebert cannot reach your localhost), use nginx or:
         // sharebertShotUrl = 'http://live0.zeit.de/twitter-quote/?quote=Das%20hat%20alles%20keinen%20Gin.';
         log( 'sharebertRedirectUrl: ' + sharebertRedirectUrl );
@@ -113,33 +84,25 @@
         getSharebertData( sharebertShotUrl, sharebertRedirectUrl )
             .then( function( response ) {
                 log( 'SUCCESS, got URL:' + response.src_url );
-                var shareLink = 'https://twitter.com/intent/tweet?text=' +
-                    encodeURIComponent( response.src_url );
-
-                // window.open() is popup-blocked, so we change the link and trigger a click
-                var elem = $elem[ 0 ];
-                elem.removeEventListener( 'click', share );
-                elem.setAttribute( 'href', shareLink );
-                elem.click();
-
+                var shareLink = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent( response.src_url );
+                $elem.setAttribute( 'href', shareLink );
             }, function( error ) {
-                log( 'error', error );
+                log( 'ERROR', error );
             });
 
-        $elem.blur();
-        */
+        this.itemsElement.style.display = 'inline-block'; // TODO: use classes/aria
     };
 
-    function initShareBlocks() {
-        if ( !window.fuck ) {
-            return;
-        }
-        labelElement = rootElement.querySelector( '.js-shareblock__label' );
-        itemsElement = rootElement.querySelector( '.js-shareblock__items' );
+    ShareBlock.prototype.init = function() {
+        this.labelElement = this.rootElement.querySelector( '.js-shareblock__label' );
+        this.itemsElement = this.rootElement.querySelector( '.js-shareblock__items' );
 
-        itemsElement.style.display = 'none';
-        labelElement.addEventListener( 'click', share );
-    }
+        this.itemsElement.style.display = 'none';
+
+        // bind this on runtime to make it revokable
+        this.clickEventListener = clickEventListener.bind( this );
+        this.labelElement.addEventListener( 'click', this.clickEventListener );
+    };
 
     $.fn.shareBlocks = function() {
 
@@ -153,7 +116,6 @@
 
         return this.each( function() {
             new ShareBlock( this );
-            initShareBlocks(); // REMOVE
         });
     };
 })( jQuery );
