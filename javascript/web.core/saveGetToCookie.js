d69ac60262d7f8ca811096cdacd1ec01ebbe2db8
Thomas Puppe <thomas.puppe@zeit.de>
2017-10-19 11:41:05 +0200

    ZON-3934: Store DPV icode value from GET in cookie

--- javascript/web.core/saveGetToCookie.js
+++ javascript/web.core/saveGetToCookie.js
@@ -26,7 +26,7 @@ define([ 'web.core/zeit' ], function( Zeit ) {
                 // param sent by DPV, stored in cookie, and consumed by premium.zeit.de
                 var icodeValue = findGetParameter( 'wt_cc1' );
                 if ( icodeValue ) {
-                    Zeit.cookieCreate( 'icode', icodeValue, 365, 'zeit.de' );
+                    Zeit.cookieCreate( 'icode', icodeValue, 365 );
                 }
             });
 
