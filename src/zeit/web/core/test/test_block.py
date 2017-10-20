6b1f3b491a00526243982cb1c3fba96ae5a316f2
Dominik Hoppe <dominik.hoppe@zeit.de>
2017-10-20 14:50:17 +0200

    ZON-4235: Make itunes podcast url configurable

--- src/zeit/web/core/test/test_block.py
+++ src/zeit/web/core/test/test_block.py
@@ -583,6 +583,6 @@ def test_podcast_should_provied_podcast_links(testbrowser):
     deezer_link = browser.cssselect('.podigee-podcast__links--deezer')[0]
     assert deezer_link.get('href') == 'http://xml.zeit.de/deezer_url'
     itunes_link = browser.cssselect('.podigee-podcast__links--itunes')[0]
-    assert itunes_link.get('href') == 'id1656'
+    assert itunes_link.get('href') == 'http://example.com/podcast/id1656'
     spotify_link = browser.cssselect('.podigee-podcast__links--spotify')[0]
     assert spotify_link.get('href') == 'http://xml.zeit.de/spotify_url'
