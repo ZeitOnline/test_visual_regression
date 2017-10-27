5f6601f200dd0832cf68252b2055d302f89b7660
Nico Bruenjes <nico.bruenjes@gmail.com>
2017-10-27 16:58:08 +0200

    BUG-762: Update adblockertest script
    - removes bug where undetected events where recorded as `undefined`
    - changes booleans in GA-Values to strings `detected|not_detected`
    - ditches jQuery
    - change debug query parameter to standard `debug-adblockertest`

--- javascript/web.site/adblockCount.js
+++ javascript/web.site/adblockCount.js
@@ -1,66 +1,73 @@
 /**
  * @fileOverview Module for losely counting adblocker user (just trends; not excact counts)
  * @author nico.bruenjes@zeit.de
- * @version  0.1
+ * @version  0.2
  */
-define([ 'jquery' ], function( $ ) {
+define( function() {
 
-    /**
-     * oldAdBlockTest
-     * checks if the old adblocker honeypotdiv is blocked
-     * @return {bool}
-     */
-    function oldAdBlockTest( $elem ) {
-        return $elem.length > 0 && $elem.is( ':hidden' );
+
+    function adsActive() {
+        return document.querySelectorAll( 'head script[src*="iqadcontroller"]' ).length > 0;
     }
 
     /**
-     * adControllerBlocked
-     * checks if the adcontroller is linked, but blocked by user
-     * @return {bool}
+     * the old trick of the block ad div
+     * @return {void}
      */
-    function adControllerBlocked() {
-        if ( $( 'head script[src*="iqadcontroller"]' ).length > 0 ) {
-            return typeof window.AdController === 'undefined';
+    function testAdblockDiv() {
+        var elem = document.querySelector( '#ad3999' ) || [];
+        return elem.length > 0 && elem.hidden;
+    }
+
+    /**
+     * check if adcontroller is blocked from download
+     * @return {void}
+     */
+    function testAdcontroller() {
+        return typeof window.AdController === 'undefined';
+    }
+
+    /**
+     * logging helper - wraps if debug --> console.log
+     * @return {void}
+     */
+    function log() {
+        if ( window.location.search.indexOf( 'debug-adblocktest' ) !== -1 ) {
+            console.log.apply( console, arguments );
         }
-        return false;
     }
 
     return {
-
-        /**
-         * Init
-         * prepare Adblockertest and start them on load
-         */
         init: function() {
-            var track = {
-                    event: 'dataLayerEvent',
-                    dataLayerEventCategory: 'AdBlockerDetection',
-                    dataLayerEventAction: false
-                },
-                debug = window.location.search.indexOf( 'ablocktestdebug' ) !== -1;
-
-            $( window ).on( 'load', function() {
-                var $elem = $( '#ad3999' );
-                if ( $elem.length > 0 ) {
-                    if ( oldAdBlockTest( $elem ) ) {
-                        track.dataLayerEventAction = true;
-                        track.dataLayerEventLabel = 'adblockdesktop';
-                    } else if ( adControllerBlocked() ) {
-                        track.dataLayerEventAction = true;
+            log( 'adblocktest debug mode' );
+            window.addEventListener( 'load', function() {
+                var track = {
+                    'event': 'dataLayerEvent',
+                    'dataLayerEventCategory': 'AdBlockerDetection',
+                    'dataLayerEventAction': 'not_detected',
+                    'dataLayerEventLabel': undefined
+                };
+                if ( adsActive() ) {
+                    if ( typeof window.gaData === 'undefined' ) {
+                        // GA not present, we can't count anything
+                        log( 'Google Analytics not present or suppressed.' );
+                    }
+                    if ( testAdcontroller() ) {
+                        track.dataLayerEventAction = 'detected';
                         track.dataLayerEventLabel = 'adcontrollerblocked';
+                        log( 'detected blocked adcontroller', track );
+                    } else if ( testAdblockDiv() ) {
+                        track.dataLayerEventAction = 'detected';
+                        track.dataLayerEventLabel = 'adblockdesktop';
+                        log( 'detected blocked element', track );
                     }
                     window.dataLayer = window.dataLayer || [];
                     window.dataLayer.push( track );
-                    if ( debug ) {
-                        console.debug( track );
-                    }
                 } else {
-                    if ( debug ) {
-                        console.info( 'Not AdBlockerDetection tracked, ads disabled.' );
-                    }
+                    log( 'No AdBlockerDetection tracked, ads disabled.' );
                 }
-            });
+            }, false );
         }
     };
+
 });
