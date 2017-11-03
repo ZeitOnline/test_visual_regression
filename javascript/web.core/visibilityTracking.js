9664d3a4c72f146267affa924ca4820068b5f3a8
Thomas Puppe <thomas.puppe@zeit.de>
2017-11-03 15:20:37 +0100

    ZON-4262: Check browsersupport

--- javascript/web.core/visibilityTracking.js
+++ javascript/web.core/visibilityTracking.js
@@ -12,13 +12,34 @@ define([ 'web.core/clicktracking' ], function( Clicktracking ) {
     };
 
     var init = function() {
-        // Beizeiten schauen, ob der Browser das überhaupt mitmacht.
-        // Polyfill?
-        observer = new window.IntersectionObserver(
-            handleIntersectionEvent,
-            { threshold: 0.5 }
-        );
-        observer.observe( document.querySelector( '.gate' ) );
+
+        // TODO: Use the whole polyfill?
+
+        // Checks via https://github.com/w3c/IntersectionObserver/tree/gh-pages/polyfill
+        // Exits early if all IntersectionObserver and IntersectionObserverEntry
+        // features are natively supported.
+        if ( 'IntersectionObserver' in window &&
+            'IntersectionObserverEntry' in window &&
+            'intersectionRatio' in window.IntersectionObserverEntry.prototype ) {
+
+            // Minimal polyfill for Edge 15's lack of `isIntersecting`
+            // See: https://github.com/w3c/IntersectionObserver/issues/211
+            if ( !( 'isIntersecting' in window.IntersectionObserverEntry.prototype ) ) {
+                Object.defineProperty( window.IntersectionObserverEntry.prototype,
+                    'isIntersecting', {
+                        get: function() {
+                            return this.intersectionRatio > 0;
+                        }
+                    });
+            }
+
+            observer = new window.IntersectionObserver(
+                handleIntersectionEvent,
+                { threshold: 0.5 }
+            );
+            observer.observe( document.querySelector( '.gate' ) );
+
+        }
 
         // Die API unseres Plugins muss das Observen beliebiger
         // Dinge entgegennhmen. Und diese Targets muss man alle
