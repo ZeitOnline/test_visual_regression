d9350e48cedfd2224b8d642080470e834d38b7cc
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-11-09 13:17:41 +0100

    MAINT: Move video/image adapter to a more related location

--- src/zeit/web/core/video.py
+++ src/zeit/web/core/video.py
@@ -3,9 +3,29 @@ import datetime
 import grokcore.component
 import pytz
 
+import zeit.content.image.interfaces
 import zeit.content.video.interfaces
 
 import zeit.web.core.image
+
+
+@grokcore.component.implementer(zeit.content.image.interfaces.IImageGroup)
+@grokcore.component.adapter(zeit.content.video.interfaces.IVideo)
+class ImageGroup(zeit.web.core.image.RemoteImageGroup):
+
+    @classmethod
+    def get_image_url(cls, context):
+        return context.video_still
+
+    @zeit.web.reify
+    def uniqueId(self):
+        return '{}/imagegroup/'.format(self.context.uniqueId)
+
+
+@grokcore.component.implementer(zeit.content.image.interfaces.IImages)
+@grokcore.component.adapter(zeit.content.video.interfaces.IVideo)
+class VideoImages(zeit.web.core.image.RemoteImages):
+    pass
 
 
 class VideoExpiration(zeit.web.core.image.ImageExpiration):
--- src/zeit/web/site/test/test_video.py
+++ src/zeit/web/site/test/test_video.py
@@ -4,33 +4,10 @@ import pytest
 import requests
 
 import zeit.cms.interfaces
-import zeit.content.image.interfaces
 import zeit.content.video.video
 
-import zeit.web.core.application
 import zeit.web.core.template
-import zeit.web.site.module.playlist
 import zeit.web.site.view_video
-
-
-def test_video_imagegroup_should_adapt_videos(application):
-    video = zeit.cms.interfaces.ICMSContent(
-        'http://xml.zeit.de/zeit-online/video/3537342483001')
-    group = zeit.content.image.interfaces.IImageGroup(video)
-    assert isinstance(group, zeit.web.site.module.playlist.ImageGroup)
-
-
-@pytest.mark.parametrize('img,res', [
-    ('wide', (579, 326)),
-    ('cinema', (580, 248))
-])
-def test_video_imagegroup_should_set_local_image_fileobj(
-        img, res, application):
-    unique_id = 'http://xml.zeit.de/zeit-online/video/3537342483001'
-    video = zeit.cms.interfaces.ICMSContent(unique_id)
-    group = zeit.content.image.interfaces.IImageGroup(video)
-    image = group[img]
-    assert image.getImageSize() == res
 
 
 def test_video_page_should_feature_sharing_images(testbrowser):
