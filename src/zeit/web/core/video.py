be92e9cd458101f97d97d6e8121d4b41f31803f1
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-11-15 10:42:48 +0100

    BUG-747: Cache Brighcove API responses

--- src/zeit/web/core/video.py
+++ src/zeit/web/core/video.py
@@ -3,10 +3,16 @@ import datetime
 import grokcore.component
 import pytz
 
+import zeit.brightcove.connection
 import zeit.content.image.interfaces
 import zeit.content.video.interfaces
 
+import zeit.web.core.cache
 import zeit.web.core.image
+import zeit.web.core.metrics
+
+
+LONG_TERM_CACHE = zeit.web.core.cache.get_region('long_term')
 
 
 @grokcore.component.implementer(zeit.content.image.interfaces.IImageGroup)
@@ -38,3 +44,12 @@ class VideoExpiration(zeit.web.core.image.ImageExpiration):
             return None
         now = datetime.datetime.now(pytz.UTC)
         return int((self.context.expires - now).total_seconds())
+
+
+@LONG_TERM_CACHE.cache_on_arguments()
+def get_video_with_caching(self, id):
+    with zeit.web.core.metrics.timer(
+            'zeit.web.core.video.brightcove.playback.http.reponse_time'):
+        return original_get_video(self, id)
+original_get_video = zeit.brightcove.connection.PlaybackAPI.get_video
+zeit.brightcove.connection.PlaybackAPI.get_video = get_video_with_caching
