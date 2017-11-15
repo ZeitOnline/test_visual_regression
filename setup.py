d6adf208b48b44f5c43eadea2faad0351210ba6f
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2017-11-15 10:27:58 +0100

    BUG-747: Support reading media data via IPlayer instead of trying to import those into vivi

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         'zc.iso8601',
         'zc.sourcefactory',
         'zeit.arbeit>=1.2.2.dev0',
-        'zeit.brightcove',
+        'zeit.brightcove>=2.12.0.dev0',
         'zeit.cms>=3.1.0.dev0',
         'zeit.campus>=1.6.0.dev0',
         'zeit.connector>=2.9.0.dev0',
