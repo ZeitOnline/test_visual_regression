0097734ed57f0bb8b2eb35b2117a28416cd2dbc1
Ron <ron@drongowski.de>
2017-11-21 13:48:01 +0100

    SSL-39: Produce https links for vivi link objects

--- src/zeit/web/core/test/test_link.py
+++ src/zeit/web/core/test/test_link.py
@@ -36,3 +36,14 @@ def test_create_url_filter_should_create_correct_url(application):
     obj = zeit.cms.interfaces.ICMSContent('http://xml.zeit.de%s-2' % path)
     assert zeit.web.core.template.create_url(None, obj) == (
         'http://www.zeit.de' + path)
+
+
+def test_link_object_should_point_to_https_target_if_possible(application,
+                                                              monkeypatch):
+    monkeypatch.setattr(zeit.web.core.template, 'toggles', lambda x: True)
+    link = zeit.cms.interfaces.ICMSContent(
+        'http://xml.zeit.de/zeit-magazin/'
+        'mode-design/2014-05/karl-lagerfeld-interview-2')
+    assert zeit.web.core.template.create_url(None, link) == (
+        'https://www.zeit.de/zeit-magazin/mode-design/'
+        '2014-05/karl-lagerfeld-interview')
