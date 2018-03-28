86683d16f861204fde767bd6de700101cd1bca8d
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2018-03-28 16:35:50 +0200

    Back to development: 3.136

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     name='zeit.web',
     url='https://github.com/ZeitOnline/zeit.web',
-    version='3.135',
+    version='3.136.dev0',
     author=(
         'Thomas Baumann, Nico Bruenjes, Nicolas Drebenstedt, Ron Drongowski, '
         'Dominik Hoppe, Marco Kaiser, Harry Keller, Tom Lazar, Thomas Lotze, '
