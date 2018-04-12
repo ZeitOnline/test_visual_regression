c30fa184ba08fa8cf50787debd4a1e41987e578d
Knut Goetz <Knut.Goetz@zeit.de>
2018-04-12 14:33:26 +0200

    pep8

--- src/zeit/web/core/test/test_application.py
+++ src/zeit/web/core/test/test_application.py
@@ -25,6 +25,7 @@ def test_asset_host_supports_url_prefix(dummy_request):
     dummy_request.application_url = 'http://example.com/foo'
     assert dummy_request.asset_host == 'http://example.com/foo/assets'
 
+
 def test_asset_host_should_be_neutral_to_protocol(dummy_request):
     conf = zope.component.getUtility(zeit.web.core.interfaces.ISettings)
     conf['asset_prefix'] = '//assets.example.com/'
