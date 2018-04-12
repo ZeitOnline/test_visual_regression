bf5a17993579eaf2a180abd828b17afeb1a2dc6f
Ron <ron@drongowski.de>
2018-04-12 13:25:17 +0200

    SSL-206: Implement protocol neutrality on configured hosts

--- src/zeit/web/core/test/test_application.py
+++ src/zeit/web/core/test/test_application.py
@@ -25,6 +25,11 @@ def test_asset_host_supports_url_prefix(dummy_request):
     dummy_request.application_url = 'http://example.com/foo'
     assert dummy_request.asset_host == 'http://example.com/foo/assets'
 
+def test_asset_host_should_be_neutral_to_protocol(dummy_request):
+    conf = zope.component.getUtility(zeit.web.core.interfaces.ISettings)
+    conf['asset_prefix'] = '//assets.example.com/'
+    assert dummy_request.asset_host == '//assets.example.com'
+
 
 def test_acceptable_pagination_should_not_redirect(testserver):
     resp = requests.get('%s/zeit-magazin/article/03/seite-3' % testserver.url,
