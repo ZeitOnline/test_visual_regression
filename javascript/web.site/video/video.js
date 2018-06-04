57eea138eacaca871088df5c35750da0002caf87
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2018-06-04 10:42:06 +0200

    BUG-935: Consider brightcove player loaded as AMD

--- javascript/web.site/video/video.js
+++ javascript/web.site/video/video.js
@@ -2,100 +2,124 @@
  * @fileOverview Module for displaying brightcove video
  * @version  0.1
  */
+
+var $ = require( 'jquery' ),
+    zeit = require( 'web.core/zeit' );
+
 /**
  * Module for displaying brightcove video
  * @module video
  */
 var video = {
-    init: function() {
-        var $ = require( 'jquery' );
+    scripts: {},
 
-        return {
+    /**
+     * Replaces a given list of dom nodes with video
+     * @function displayVideo
+     * @param {string} videoId id of the video to show
+     * @param {object} config configuration object
+     */
+    displayVideo: function( videoId, config ) {
+
+        var advertising = ( zeit.cookieRead( 'gdpr' ) === 'dnt' ) ?
+                'withoutAds' : config.advertising,
+            videoPlayerId = zeit.videoPlayers[ config.playertype ][ advertising ];
+
+        /**
+         * configuration object, extends config
+         * @type {object}
+         * @property {object} elem jQuery collection to replace by video
+         * @property {object} playerData standard data about the bc player
+         */
+        var defaults = $.extend({
+                elem: [],
+                playerData: {
+                    'accountId': '18140073001',
+                    'playerId': videoPlayerId,
+                    'embed': 'default'
+                }
+            }, config ),
 
             /**
-             * Replaces a given list of dom nodes with video
-             * @function displayVideo
-             * @param {string} videoId id of the video to show
-             * @param {object} config configuration object
+             * html template for player code
              */
-            displayVideo: function( videoId, config ) {
+            template = '<div class="video-player">' +
+                        '<video id="player-{{videoId}}" data-account="{{accountId}}" ' +
+                        'data-player="{{playerId}}" data-embed="{{embed}}" data-video-id="{{videoId}}" ' +
+                        'class="video-js video-player__videotag" preload="none" controls></video></div>',
+            snippet,
+            scriptSrc = 'https://players.brightcove.net/{{accountId}}/{{playerId}}_{{embed}}/index.min.js';
 
-                var advertising = ( window.Zeit.cookieRead( 'gdpr' ) === 'dnt' ) ?
-                        'withoutAds' : config.advertising,
-                    videoPlayerId = window.Zeit.videoPlayers[ config.playertype ][ advertising ];
+        if ( typeof videoId === 'undefined' && defaults.elem.length < 1 ) {
+            return;
+        }
 
-                /**
-                 * configuration object, extends config
-                 * @type {object}
-                 * @property {object} elem jQuery collection to replace by video
-                 * @property {object} playerData standard data about the bc player
-                 */
-                var defaults = $.extend({
-                        elem: [],
-                        playerData: {
-                            'accountId': '18140073001',
-                            'playerId': videoPlayerId,
-                            'embed': 'default'
-                        }
-                    }, config ),
+        snippet = template
+            .replace( /\{{accountId}}/g, defaults.playerData.accountId )
+            .replace( /\{{playerId}}/g, defaults.playerData.playerId )
+            .replace( /\{{videoId}}/g, videoId )
+            .replace( /\{{embed}}/g, 'default' );
 
-                    /**
-                     * html template for player code
-                     */
-                    template = '<div class="video-player">' +
-                                '<video id="player-{{videoId}}" data-account="{{accountId}}" ' +
-                                'data-player="{{playerId}}" data-embed="{{embed}}" data-video-id="{{videoId}}" ' +
-                                'class="video-js video-player__videotag" preload="none" controls></video></div>',
-                    snippet,
-                    scriptSrc = 'https://players.brightcove.net/{{accountId}}/{{playerId}}_{{embed}}/index.min.js';
-                if ( typeof videoId === 'undefined' && defaults.elem.length < 1 ) {
-                    return;
-                } else {
-                    snippet = template
-                        .replace( /\{{accountId}}/g, defaults.playerData.accountId )
-                        .replace( /\{{playerId}}/g, defaults.playerData.playerId )
-                        .replace( /\{{videoId}}/g, videoId )
-                        .replace( /\{{embed}}/g, 'default' );
+        defaults.elem.empty().html( snippet );
 
-                    defaults.elem.empty().html( snippet );
+        // if your site uses require.js, this same script needs to be required
+        // but for each player individually, so we need n local requires
 
-                    // if your site uses require.js, this same script needs to be required
-                    // but for each player individually, so we need n local requires
+        // build video and account dependent script source address
+        scriptSrc = scriptSrc
+            .replace( /\{{accountId}}/g, defaults.playerData.accountId )
+            .replace( /\{{playerId}}/g, defaults.playerData.playerId )
+            .replace( /\{{embed}}/g, 'default' );
 
-                    // build video and account dependent script source address
-                    scriptSrc = scriptSrc
-                        .replace( /\{{accountId}}/g, defaults.playerData.accountId )
-                        .replace( /\{{playerId}}/g, defaults.playerData.playerId )
-                        .replace( /\{{embed}}/g, 'default' );
+        // skip loading if script already added
+        if ( this.scripts[ scriptSrc ]) {
+            return;
+        }
 
-                    // add script with callback to create a player instance and play the video
-                    var script = document.createElement( 'script' ),
-                        _define = window.define, // be nice, keep a reference
-                        done = false;
+        // We have to take care of the brightcove plaver if loaded as AMD
+        // https://support.brightcove.com/requirejs-and-brightcove-player
+        // brighcove checks for global define to recognize RequireJS
+        // This is ill. It will break the internet.
+        if ( typeof window.define === 'function' && window.define.amd ) {
+            loadAmdModule( videoId, scriptSrc );
+        } else {
+            // add script with callback
+            var script = document.createElement( 'script' ),
+                done = false;
 
-                    // prevent clash of brightcove script with third party scripts using require.
-                    // brighcove checks for global define to recognize RequireJS
+            this.scripts[ scriptSrc ] = true;
+            script.src = scriptSrc;
+            script.onload = script.onreadystatechange = function() {
+                if ( !done && ( !this.readyState || this.readyState === 'loaded' || this.readyState === 'complete' ) ) {
+                    done = true;
+
+                    // Check again if require was defined *after* adding the script but before execution
+                    // and thus brightcove module was loaded as AMD
                     // This is ill. It will break the internet.
-                    window.define = undefined;
+                    if ( typeof window.bc !== 'function' && typeof window.define === 'function' && window.define.amd ) {
+                        loadAmdModule( videoId, scriptSrc );
+                    }
 
-                    script.src = scriptSrc;
-                    script.onload = script.onreadystatechange = function() {
-                        if ( !done && ( !this.readyState || this.readyState === 'loaded' || this.readyState === 'complete' ) ) {
-                            done = true;
-                            // autoplay is handled via the players and plugins
-                            // /*global videojs*/
-                            // videojs( 'player-' + videoId ).play();
-                            window.define = _define; // reset previous state
-                            script.onload = script.onreadystatechange = null;
-                        }
-                    };
-                    document.body.appendChild( script );
-
-                    return defaults.elem;
+                    script.onload = script.onreadystatechange = null;
                 }
-            }
-        };
+            };
+
+            document.body.appendChild( script );
+        }
     }
+
 };
 
-module.exports = video.init();
+function loadAmdModule( videoId, scriptSrc ) {
+    // add encapsuled require config per videoId
+    // http://requirejs.org/docs/api.html#multiversion
+    var rjs = window.require.config({
+        'paths': { 'bc': scriptSrc },
+        'timeout': 30,
+        'context': videoId
+    });
+    // require the script
+    rjs([ 'require', 'bc' ]);
+}
+
+module.exports = video;
