27d56edf807bd9a806ae1a589b7fe2adeb2c4082
Wolfgang Schnerring <wolfgang.schnerring@zeit.de>
2018-07-18 11:15:34 +0200

    ZON-4791: Update tests to the current situation
    TMS/ES areas don't use the LazyProxy since e0f1128,
    instead add a test that we don't resolve ICMSContent at all

--- src/zeit/web/core/test/test_area.py
+++ src/zeit/web/core/test/test_area.py
@@ -1,7 +1,7 @@
-import pyramid.threadlocal
 import zope.component
 
 import zeit.content.cp.centerpage
+import zeit.retresco.content
 import zeit.solr.interfaces
 
 import zeit.web.core.centerpage
@@ -22,8 +22,6 @@ def test_ranking_area_with_automatic_false_has_zero_hits(application):
 
 def test_ranking_area_works_with_channel_query(
         application, dummy_request, monkeypatch):
-    monkeypatch.setattr(
-        pyramid.threadlocal, 'get_current_request', dummy_request)
     solr = zope.component.getUtility(zeit.solr.interfaces.ISolr)
     solr.results = [{'uniqueId': 'http://xml.zeit.de/zeit-online/article/01'}]
     cp = zeit.content.cp.centerpage.CenterPage()
@@ -36,3 +34,39 @@ def test_ranking_area_works_with_channel_query(
     area.automatic = True
     area = zeit.web.core.centerpage.get_area(area)
     assert len(area.values()) == 1
+
+
+def test_es_based_areas_use_results_directly_and_dont_resolve_content(
+        application, dummy_request):
+    es = zope.component.getUtility(zeit.retresco.interfaces.IElasticsearch)
+    es.results = [{'uniqueId': 'http://xml.zeit.de/zeit-online/article/01'}]
+    tms = zope.component.getUtility(zeit.retresco.interfaces.ITMS)
+    tms.results = [{'uniqueId': 'http://xml.zeit.de/zeit-online/article/01'}]
+
+    cp = zeit.content.cp.centerpage.CenterPage()
+    cp.uniqueId = 'http://xml.zeit.de/testcp'
+    area = cp.body.create_item('region').create_item('area')
+    area.kind = 'ranking'
+    area.count = 1
+    area.automatic_type = 'elasticsearch-query'
+    # actual value is irrelevant, results are mocked
+    area.elasticsearch_raw_query = '{"query": {"match_all": {}}}'
+    area.automatic = True
+
+    area = zeit.web.core.centerpage.get_area(area)
+    content = list(area.values()[0])[0]
+    assert isinstance(content, zeit.retresco.content.Content)
+
+    area.automatic_type = 'custom'
+    # actual value is irrelevant, results are mocked
+    area.query = (('channels', 'Wissen', None))
+    area = zeit.web.core.centerpage.get_area(area)
+    content = list(area.values()[0])[0]
+    assert isinstance(content, zeit.retresco.content.Content)
+
+    area.automatic_type = 'topicpage'
+    # actual value is irrelevant, results are mocked
+    area.referenced_topicpage = 'berlin'
+    area = zeit.web.core.centerpage.get_area(area)
+    content = list(area.values()[0])[0]
+    assert isinstance(content, zeit.retresco.content.Content)
