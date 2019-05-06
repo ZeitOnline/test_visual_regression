74a62a28d1b5eee00f33daa6b5278f6deae24088
thomaspuppe <info@thomaspuppe.de>
2019-05-06 12:30:19 +0200

    Add AMP pages to ZCO test

--- backstop_tests/zco/all.js
+++ backstop_tests/zco/all.js
@@ -2,9 +2,11 @@
 const base = require( `./base.js` );
 const embedheader = require( `./embedheader.js` );
 const paywall = require( `./paywall.js` );
+const amp = require( `./amp.js` );
 
 module.exports = [].concat(
   base,
   embedheader,
-  paywall
+  paywall,
+  amp
 );
