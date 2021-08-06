f25ed2a0996a5bbda1d7e07ce83f31aa135b6df4
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2021-08-06 09:13:55 +0200

    Update video config

--- backstop_tests/zon/video.js
+++ backstop_tests/zon/video.js
@@ -1,30 +1,10 @@
-const scenarios = []
-const urls = [
-  'http://localhost:9090/zeit-online/video/3537342483001',
-  'http://localhost:9090/zeit-online/video/3537342483002'
+const scenarios = [
+  {
+    url: 'http://localhost:9090/zeit-online/video/3537342483001',
+    delay: 300,
+    readySelector: '.vjs-poster',
+    selectors: ['main > article']
+  }
 ]
-
-urls.forEach(url => {
-  scenarios.push({
-    label: url.replace('http://localhost:9090/', '').replace(/\//g, '_'),
-    cookiePath: '',
-    url: url,
-    referenceUrl: '',
-    readyEvent: '',
-    readySelector: '',
-    delay: 100,
-    hideSelectors: [],
-    removeSelectors: [
-      '#pDebug'
-    ],
-    hoverSelector: '',
-    clickSelector: '',
-    postInteractionWait: '',
-    selectors: ['html'],
-    selectorExpansion: true,
-    misMatchThreshold: 0.1,
-    requireSameDimensions: true
-  })
-})
 
 module.exports = scenarios
