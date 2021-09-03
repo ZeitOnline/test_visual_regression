6b86440e082205bc04fd5e60416f035265e028ac
Nico Bruenjes <nico.bruenjes@gmail.com>
2021-09-03 09:11:53 +0200

    ZO-65: Add example scenario for dark mode

--- scenarios/zon/header.js
+++ scenarios/zon/header.js
@@ -10,6 +10,14 @@ const scenarios = [
     selectors: ['header.header'],
     viewports: ['tablet', 'desktop'],
   },
+  {
+    label: 'darkmode',
+    url: '/zeit-online/article/simple',
+    readySelector: '.nav__ressorts--fitted',
+    selectors: ['header.header'],
+    viewports: ['tablet', 'desktop'],
+    onBeforeScript: 'prefers-color-scheme-dark.js',
+  },
 ];
 
 module.exports = scenarios;
