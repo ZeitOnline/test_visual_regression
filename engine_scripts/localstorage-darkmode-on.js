ce6a7fce049e30ba42bceb22d5f54d6cea7a434e
Nico Bruenjes <nico.bruenjes@gmail.com>
2021-09-03 09:10:38 +0200

    ZO-65: Add example script for localstorage setting

---
+++ engine_scripts/localstorage-darkmode-on.js
@@ -0,0 +1,20 @@
+/**
+ * example engine script for setting localstorage value
+ * would be used as `onBeforeScript` in scenario
+ *
+ * @param {object}  page  the puppeteer page object
+ * @param {object}  scenario  currently running scenario config
+ * @see   https://github.com/garris/BackstopJS#running-custom-scripts
+ * @see   https://github.com/puppeteer/puppeteer/blob/v10.2.0/docs/api.md#class-page
+ */
+
+module.exports = async (page, scenario) => {
+  console.log('SCENARIO > ' + scenario.label);
+
+  await page.evaluateOnNewDocument(() => {
+    /*eslint-env browser*/
+    console.log('setting localstorage theme to dark');
+    localStorage.setItem('theme', 'dark');
+  });
+
+};
