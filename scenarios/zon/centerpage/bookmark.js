303e10dfe654497cdc67ac51df0cc618b903cbf5
Nico Bruenjes <nico.bruenjes@gmail.com>
2021-09-06 17:42:38 +0200

    ZO-65: Add test for bookmark icon click on cps

---
+++ scenarios/zon/centerpage/bookmark.js
@@ -0,0 +1,29 @@
+const scenarios = [
+  {
+    url: '/zeit-online/centerpage/zon-teaser-standard',
+    clickSelectors: ['.bookmark-icon'],
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zeit-online/centerpage/zon-teaser-wide',
+    clickSelectors: ['.bookmark-icon'],
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zeit-online/centerpage/zon-teaser-poster',
+    clickSelectors: ['.bookmark-icon'],
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zeit-online/centerpage/zon-teaser-podcast',
+    clickSelectors: ['.bookmark-icon'],
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zeit-online/centerpage/zon-teaser-podcast-lead',
+    clickSelectors: ['.bookmark-icon'],
+    selectors: ['viewport'],
+  },
+];
+
+module.exports = scenarios;
