95fa736e6ba7ca17edba30162d7b1524c2262dc8
Nico Bruenjes <nico.bruenjes@gmail.com>
2021-09-07 11:24:08 +0200

    ZO-65: Add hover screenshot example

---
+++ scenarios/zon/centerpage/hover.js
@@ -0,0 +1,9 @@
+const scenarios = [
+  {
+    url: '/zeit-online/centerpage/zon-teaser-lead',
+    hoverSelectors: ['.zon-teaser-leadtopiclink'],
+    selectors: ['document'],
+  },
+];
+
+module.exports = scenarios;
