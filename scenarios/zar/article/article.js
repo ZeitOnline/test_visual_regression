af1a592d3c26b29c76bad1b7506c6da7723b5f18
Moritz Stoltenburg <moritz.stoltenburg@zeit.de>
2021-09-08 21:45:52 +0200

    MAINT: Add tests for article pagination

--- scenarios/zar/article/article.js
+++ scenarios/zar/article/article.js
@@ -3,6 +3,10 @@ const scenarios = [
     url: '/arbeit/article/simple',
     selectors: ['main > article'],
   },
+  {
+    url: '/arbeit/article/paginated',
+    selectors: ['main > article'],
+  },
 ];
 
 module.exports = scenarios;
--- scenarios/zett/article/article.js
+++ scenarios/zett/article/article.js
@@ -3,6 +3,10 @@ const scenarios = [
     url: '/zett/article/simple',
     selectors: ['main > article'],
   },
+  {
+    url: '/zett/article/paginated',
+    selectors: ['main > article'],
+  },
 ];
 
 module.exports = scenarios;
