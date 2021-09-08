366dcd82b4861f317ef622844ccfac96e472cf40
Nico Bruenjes <nico.bruenjes@gmail.com>
2021-09-08 18:15:59 +0200

    ZO-65: Fix flaky image tests
    Our image script needs an absurd waiting time after scrolling to the node…
    6000ms is already to short making the test fail in random breakpoints
    Without image interception the delay needs to be even longer 🤦‍♂️

--- scenarios/zmo/article/blocks.js
+++ scenarios/zmo/article/blocks.js
@@ -1,14 +1,20 @@
+const waitTime = 6500;
 const scenarios = [
   {
     url: '/zeit-magazin/article/inline-gallery',
     selectors: ['.gallery'],
     scrollToSelector: '.gallery',
-    delay: 300,
+    onBeforeScript: 'intercept-image.js',
+    interceptImagePath: 'gallery-image.jpg',
+    postInteractionWait: 0,
   },
   {
     url: '/zeit-magazin/article/volumeteaser',
     selectors: ['.volume-teaser'],
     scrollToSelector: '.volume-teaser',
+    onBeforeScript: 'intercept-image.js',
+    interceptImagePath: 'zeit-wissen-cover.webp',
+    postInteractionWait: waitTime,
   },
 ];
 
--- scenarios/zon/article/blocks.js
+++ scenarios/zon/article/blocks.js
@@ -1,13 +1,20 @@
+const waitTime = 6500;
 const scenarios = [
   {
     url: '/zeit-online/article/inline-gallery',
     selectors: ['.gallery'],
     scrollToSelector: '.gallery',
-    delay: 300,
+    onBeforeScript: 'intercept-image.js',
+    interceptImagePath: 'gallery-image.jpg',
+    postInteractionWait: 0,
   },
   {
     url: '/zeit-online/article/volumeteaser',
     selectors: ['.volume-teaser'],
+    scrollToSelector: '.volume-teaser',
+    onBeforeScript: 'intercept-image.js',
+    interceptImagePath: 'printcover.webp',
+    postInteractionWait: waitTime,
   },
 ];
 
