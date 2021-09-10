648ca00abea3d534b9da5cd150845bb49b071edf
Nico Bruenjes <nico.bruenjes@gmail.com>
2021-09-10 15:53:10 +0200

    ZO-65: Add menu hover

--- scenarios/zon/actions/hover.js
+++ scenarios/zon/actions/hover.js
@@ -1,8 +1,15 @@
 const scenarios = [
   {
-    label: 'hover-more',
+    label: 'hover-nav-more',
     url: '/zeit-online/centerpage/zon-teaser-snapshot',
     hoverSelectors: ['.nav__ressorts-item--more', '#more-ressorts .nav__ressorts-item--zeitmagazin'],
+    selectors: ['document'],
+    viewports: ['tablet', 'desktop'],
+  },
+  {
+    label: 'hover-classified-more',
+    url: '/zeit-online/centerpage/zon-teaser-snapshot',
+    hoverSelectors: ['.nav__classifieds-item--has-dropdown', '#mehr .nav__dropdown-item--urlaubsziele'],
     selectors: ['document'],
     viewports: ['tablet', 'desktop'],
   },
