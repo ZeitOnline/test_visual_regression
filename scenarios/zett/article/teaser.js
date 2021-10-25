5120bd77eaf6ddb8c2d888f2dc11dc3953d0efb5
Daniel Kreitschmann <Daniel.Kreitschmann@zeit.de>
2021-10-25 11:52:06 +0200

    Add test for volume teasers in articles

---
+++ scenarios/zett/article/teaser.js
@@ -0,0 +1,8 @@
+const scenarios = [
+  {
+    url: '/zett/article/faq',
+    selectors: ['main > article'],
+  },
+];
+
+module.exports = scenarios;
---
+++ scenarios/zmo/article/teaser.js
@@ -0,0 +1,8 @@
+const scenarios = [
+  {
+    url: '/zeit-magazin/article/faq',
+    selectors: ['main > article'],
+  },
+];
+
+module.exports = scenarios;
