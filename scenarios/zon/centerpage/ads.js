731063490bdf20c725f88839d392d200c8d6a9a0
Nico Bruenjes <nico.bruenjes@gmail.com>
2022-04-26 17:30:18 +0200

    MAINT: Add a load of more testpages

--- scenarios/zon/centerpage/ads.js
+++ scenarios/zon/centerpage/ads.js
@@ -21,6 +21,102 @@ const scenarios = [
     onBeforeScript: 'appUserAgent.js',
     viewports: ['mobile', 'tablet']
   },
+  {
+    label: 'browser',
+    url: '/zeit-online/centerpage/index?ad-debug',
+    selectors: ['document']
+  },
+  {
+    label: 'app-webview',
+    url: '/zeit-online/centerpage/index?ad-debug',
+    onBeforeScript: 'appUserAgent.js',
+    selectors: ['document'],
+    viewports: ['mobile', 'tablet']
+  },
+  {
+    label: 'browser',
+    url: '/zeit-online/centerpage/index-with-classic?ad-debug',
+    selectors: ['document']
+  },
+  {
+    label: 'app-webview',
+    url: '/zeit-online/centerpage/index-with-classic?ad-debug',
+    onBeforeScript: 'appUserAgent.js',
+    selectors: ['document'],
+    viewports: ['mobile', 'tablet']
+  },
+  {
+    label: 'browser',
+    url: '/zeit-online/centerpage/centerpage?ad-debug',
+    selectors: ['document']
+  },
+  {
+    label: 'app-webview',
+    url: '/zeit-online/centerpage/centerpage?ad-debug',
+    onBeforeScript: 'appUserAgent.js',
+    selectors: ['document'],
+    viewports: ['mobile', 'tablet']
+  },
+  {
+    label: 'browser',
+    url: '/zeit-online/centerpage/teaser-to-wochenmarkt?ad-debug',
+    selectors: ['document']
+  },
+  {
+    label: 'app-webview',
+    url: '/zeit-online/centerpage/teaser-to-wochenmarkt?ad-debug',
+    onBeforeScript: 'appUserAgent.js',
+    selectors: ['document'],
+    viewports: ['mobile', 'tablet']
+  },
+  {
+    label: 'browser',
+    url: '/thema/index?ad-debug',
+    selectors: ['document']
+  },
+  {
+    label: 'app-webview',
+    url: '/thema/index?ad-debug',
+    onBeforeScript: 'appUserAgent.js',
+    selectors: ['document'],
+    viewports: ['mobile', 'tablet']
+  },
+  {
+    label: 'browser',
+    url: '/thema/autotopic?ad-debug',
+    selectors: ['document']
+  },
+  {
+    label: 'app-webview',
+    url: '/thema/autotopic?ad-debug',
+    onBeforeScript: 'appUserAgent.js',
+    selectors: ['document'],
+    viewports: ['mobile', 'tablet']
+  },
+  {
+    label: 'browser',
+    url: '/thema/manualtopic?ad-debug',
+    selectors: ['document']
+  },
+  {
+    label: 'app-webview',
+    url: '/thema/manualtopic?ad-debug',
+    onBeforeScript: 'appUserAgent.js',
+    selectors: ['document'],
+    viewports: ['mobile', 'tablet']
+  },
+  {
+    label: 'browser',
+    url: '/thema/jurastudium?ad-debug',
+    selectors: ['document']
+  },
+  {
+    label: 'app-webview',
+    url: '/thema/jurastudium?ad-debug',
+    onBeforeScript: 'appUserAgent.js',
+    selectors: ['document'],
+    viewports: ['mobile', 'tablet']
+  },
 ];
 
 module.exports = scenarios;
