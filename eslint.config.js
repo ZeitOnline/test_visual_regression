082e4136a15c1f0cb042cf3db2ce967110ecd268
thomaspuppe <info@thomaspuppe.de>
2024-08-20 16:54:12 +0200

    MAINT: Ignore backstopjs files at eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,6 @@ module.exports = [
     ...zonEslintConfig,
     {
         // Add project-specific overrides here, e.g. ignore certain files
+        ignores: ['backstop-settings.js', '**/index_bundle.js', 'data/html_report/*']
     }
 ];
