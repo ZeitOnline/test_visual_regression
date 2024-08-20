1c19deb51357d293f14e17df5b79fbd3ff5cb9af
thomaspuppe <info@thomaspuppe.de>
2024-08-20 16:38:26 +0200

    ZO-2784: Add scenario for fullwidth-header of all verticals

---
+++ scenarios/shared/article/fullwidth-header.js
@@ -0,0 +1,84 @@
+const scenarios = [
+  {
+    url: '/arbeit/article/header-fullwidth-alternative',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/arbeit/article/header-fullwidth-classic',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/arbeit/article/header-fullwidth-no-image',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/arbeit/article/header-fullwidth-split',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/campus/article/header-fullwidth-alternative',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/campus/article/header-fullwidth-classic',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/campus/article/header-fullwidth-no-image',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/campus/article/header-fullwidth-split',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zeit-magazin/article/header-fullwidth-alternative',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zeit-magazin/article/header-fullwidth-classic',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zeit-magazin/article/header-fullwidth-no-image',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zeit-magazin/article/header-fullwidth-split',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zeit-online/article/header-fullwidth-alternative',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zeit-online/article/header-fullwidth-classic',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zeit-online/article/header-fullwidth-no-image',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zeit-online/article/header-fullwidth-split',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zett/article/header-fullwidth-alternative',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zett/article/header-fullwidth-classic',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zett/article/header-fullwidth-no-image',
+    selectors: ['viewport'],
+  },
+  {
+    url: '/zett/article/header-fullwidth-split',
+    selectors: ['viewport'],
+  },
+];
+
+module.exports = scenarios;
